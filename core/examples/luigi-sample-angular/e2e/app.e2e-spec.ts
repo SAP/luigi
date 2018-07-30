fc28e2fa3d71174fbb44fa59c08a126b8e1e5cd7
Jesus Real <8238188+jesusreal@users.noreply.github.com>
2018-07-30 12:29:29 +0200

    update angular example from angular 5 to angular 6

--- core/examples/luigi-sample-angular/e2e/app.e2e-spec.ts
+++ core/examples/luigi-sample-angular/e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('luigisample App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('');
   });
 });
