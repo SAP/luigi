07b68b2ac80ae20f3e35bb3a8821f46386125660
Jesus Real <8238188+jesusreal@users.noreply.github.com>
2018-07-31 11:30:15 +0200

    Remove duplicated property in karma.conf.js

--- core/examples/luigi-sample-angular/karma.conf.js
+++ core/examples/luigi-sample-angular/karma.conf.js
@@ -16,10 +16,11 @@ module.exports = function(config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, 'coverage'), dir: require('path').join(__dirname, 'coverage'), reports: ['html', 'lcovonly'],
+      dir: require('path').join(__dirname, 'coverage'),
+      reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
-    
+
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
