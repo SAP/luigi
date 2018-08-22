870602947d9ae4f0315a446fc74d7c19d8d2148b
Markus <1720843+maxmarkus@users.noreply.github.com>
2018-08-22 12:29:40 +0200

    Authorization improvements (#19)
    * removed login button but instantly authenticating when auth is enabled, if access token is expired auth is also triggered. added logout page that currently references cdn
    
    * consolidated prependOrigin in helpers, added override for core post_logout_redirect_uri
    
    * moved logout.html from core to examples and adapted config
    
    * adjusted logout.html for openid connect provider
    
    * statically using fundamental-ui.min.css via assets, fix for prepend helper
    
    * fix for oauth2
    
    * changed library loader to use local dependency and therefore added webpack-copy for oidc-client, bugfixes for oidc-client in combination with instant-login
    
    * added timeout to give persistence
    
    * Optimize fundamental ui usage
    
    * Fix issue with backdrop modal width in content-view
    
    * only redirect if a state was present before, luigi top nav link is now dynamic link
    
    * luigi client es5 improvements
    
    * Remove font.css import after its deletion

--- core/src/providers/auth/oAuth2ImplicitGrant.js
+++ core/src/providers/auth/oAuth2ImplicitGrant.js
@@ -1,4 +1,4 @@
-import { deepMerge } from '../../utilities/helpers.js';
+import { deepMerge, prependOrigin } from '../../utilities/helpers.js';
 
 export class oAuth2ImplicitGrant {
   constructor(settings = {}) {
@@ -8,80 +8,72 @@ export class oAuth2ImplicitGrant {
           window.location.origin + '/luigi-core/auth/oauth2/callback.html',
         response_type: 'id_token token',
         scope: ''
-      }
+      },
+      post_logout_redirect_uri: window.location.origin + '/logout.html'
     };
     const mergedSettings = deepMerge(defaultSettings, settings);
 
-    // Prepend current url to redirect_uri, if it is a relative path
-    if (!mergedSettings.oAuthData.redirect_uri.startsWith('http')) {
-      const hasLeadingSlash = mergedSettings.oAuthData.redirect_uri.startsWith(
-        '/'
-      );
-      mergedSettings.oAuthData.redirect_uri =
-        window.location.origin +
-        (hasLeadingSlash ? '' : '/') +
-        mergedSettings.oAuthData.redirect_uri;
-    }
     this.settings = mergedSettings;
   }
 
   login() {
-    const settings = this.settings;
-    const generatedNonce =
-      (settings.nonceFn && settings.nonceFn()) || this._generateNonce();
+    return new Promise((resolve, reject) => {
+      const settings = this.settings;
+      const generatedNonce =
+        (settings.nonceFn && settings.nonceFn()) || this._generateNonce();
 
-    const createInputElement = (name, value) => {
-      const inputElem = document.createElement('input');
-      inputElem.name = name;
-      inputElem.id = name;
-      inputElem.value = value;
-      inputElem.type = 'hidden';
-      return inputElem;
-    };
+      const createInputElement = (name, value) => {
+        const inputElem = document.createElement('input');
+        inputElem.name = name;
+        inputElem.id = name;
+        inputElem.value = value;
+        inputElem.type = 'hidden';
+        return inputElem;
+      };
 
-    const formElem = document.createElement('form');
-    formElem.name = 'signIn';
-    formElem.id = 'signIn';
-    formElem.method = 'POST';
-    // formElem.target = 'signInTarget';
-    formElem.target = '_self';
-    formElem.action = settings.authorizeUrl;
+      const formElem = document.createElement('form');
+      formElem.name = 'signIn';
+      formElem.id = 'signIn';
+      formElem.method = 'POST';
+      // formElem.target = 'signInTarget';
+      formElem.target = '_self';
+      formElem.action = settings.authorizeUrl;
 
-    settings.oAuthData.state = window.location.href;
+      settings.oAuthData.redirect_uri = prependOrigin(
+        settings.oAuthData.redirect_uri
+      );
+      settings.oAuthData.state = window.location.href;
 
-    for (const name in settings.oAuthData) {
-      const node = createInputElement(name, settings.oAuthData[name]);
+      for (const name in settings.oAuthData) {
+        const node = createInputElement(name, settings.oAuthData[name]);
+        formElem.appendChild(node.cloneNode());
+      }
+
+      const node = createInputElement('nonce', generatedNonce);
       formElem.appendChild(node.cloneNode());
-    }
 
-    const node = createInputElement('nonce', generatedNonce);
-    formElem.appendChild(node.cloneNode());
-
-    document.getElementById('authorization').appendChild(formElem);
-    document.querySelector('form#signIn').submit();
+      document.getElementsByTagName('body')[0].appendChild(formElem);
+      document.querySelector('form#signIn').submit();
+      // TODO: We're not resolving the promise at any time,
+      // since oauth2 is redirecting off the page
+      // maybe it is possible to catch errors
+      document.querySelector('form#signIn').addEventListener('load', e => {
+        console.log('load, e', e, this);
+      });
+    });
   }
 
   logout(authData, logoutCallback) {
-    // yaas logout
     const settings = this.settings;
-    const logoutreq = `${settings.logoutUrl}?id_token_hint=${
+    const logouturl = `${settings.logoutUrl}?id_token_hint=${
       authData.idToken
-    }&client_id=${settings.oAuthData.client_id}&post_logout_redirect_uri=${
-      window.location.origin
-    }`;
-
-    const request = new XMLHttpRequest();
-    request.open('GET', logoutreq);
-    request.addEventListener('load', event => {
-      if (request.status >= 200 && request.status < 300) {
-        console.info(request.responseText);
-        return logoutCallback();
-      }
-
-      console.warn(request.statusText, request.responseText);
-      logoutCallback();
-    });
-    request.send();
+      }&client_id=${
+      settings.oAuthData.client_id
+      }&post_logout_redirect_uri=${prependOrigin(
+        settings.post_logout_redirect_uri
+      )}`;
+    logoutCallback && logoutCallback();
+    window.location.href = logouturl;
   }
 
   renewToken() {
