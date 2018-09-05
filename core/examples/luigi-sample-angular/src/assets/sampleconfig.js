78aac00b2fe85b16a37718e82e23dd15132c34dd
kwiatekus <krzysztof.kwiatosz@sap.com>
2018-09-05 15:27:25 +0200

    Update examples around luigi config (#30)
    * Update example apps around Luigi config changes

--- core/examples/luigi-sample-angular/src/assets/sampleconfig.js
+++ core/examples/luigi-sample-angular/src/assets/sampleconfig.js
@@ -188,7 +188,7 @@ Luigi.setConfig({
    *  - openIdConnect (eg. DEX)
    *  - oAuth2ImplicitGrant
    * custom:
-   *  - customIdpProvider (if you provide a class to LuigiConfig.auth.customIdpProvider)
+   *  - customIdpProvider (if you provide a class to Luigi.config.auth.customIdpProvider)
    *
    */
   auth: {
