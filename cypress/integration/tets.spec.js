25571cf88a3a715c264c55db74b9985282535d27
dariadomagala <daria.domagala@sap.com>
2018-09-20 11:04:41 +0200

    Add tets

--- cypress/integration/tets.spec.js
+++ cypress/integration/tets.spec.js
@@ -1,41 +1,102 @@
-context('Luigi Sample Application', () => {
+describe('Luigi Sample Application', () => {
   beforeEach(() => {
     cy.visit('http://localhost:4000');
   });
 
-  it('Login', () => {
-    cy.login('tets@email.com', 'tets');
-    cy.get('.fd-global-nav').contains('Overview');
-    cy.location().should(loc => {
-      expect(loc.hash).to.eq('#/overview');
-    });
-  });
-
-  it('Go to subpage with uxManager features', () => {
-    cy.login('tets', 'tets');
-    cy.location().should(loc => {
-      expect(loc.hash).to.eq('#/overview');
-    });
-    cy.wait(3000);
-    cy.get('iframe').then($iframe => {
-      const $iframeBody = $iframe.contents().find('body');
-      cy.wrap($iframeBody)
-        .contains('uxManager()')
-        .click();
-      cy.location().should(loc => {
-        expect(loc.hash).to.eq('#/projects/pr2');
-      });
-      cy.wait(2000);
-      cy.wrap($iframeBody).should('contain', 'LuigiClient uxManager methods:');
-      cy.wrap($iframeBody).should(
-        'contain',
-        'LuigiClient linkManager methods:'
-      );
-      cy.wrap($iframeBody)
-        .contains('absolute: to overview')
-        .click();
+  describe('Login', () => {
+    it('Login', () => {
+      cy.login('tets@email.com', 'tets');
+      cy.get('.fd-global-nav').contains('Overview');
       cy.location().should(loc => {
         expect(loc.hash).to.eq('#/overview');
+      });
+    });
+  });
+  describe('Luigi client features', () => {
+    it('linkManager features', () => {
+      cy.login('tets', 'tets');
+      cy.location().should(loc => {
+        expect(loc.hash).to.eq('#/overview');
+      });
+      cy.wait(3000);
+      cy.get('iframe').then($iframe => {
+        const $iframeBody = $iframe.contents().find('body');
+        //first
+        cy.goToFeaturesPage($iframeBody);
+        cy.wrap($iframeBody)
+          .contains('absolute: to overview')
+          .click();
+        cy.location().should(loc => {
+          expect(loc.hash).to.eq('#/overview');
+        });
+        //second
+        cy.goToFeaturesPage($iframeBody);
+        cy.wrap($iframeBody)
+          .contains('relative: to stakeholders')
+          .click();
+        cy.location().should(loc => {
+          expect(loc.hash).to.eq('#/projects/pr2/users/groups/stakeholders');
+        });
+        //third
+        cy.goToOverviewPage();
+        cy.goToFeaturesPage($iframeBody);
+        cy.wrap($iframeBody)
+          .contains('closest parent: to stakeholders')
+          .click();
+        cy.location().should(loc => {
+          expect(loc.hash).to.eq('#/projects/pr2/users/groups/stakeholders');
+        });
+        //forth
+        cy.goToOverviewPage();
+        cy.goToFeaturesPage($iframeBody);
+        cy.wrap($iframeBody)
+          .contains('parent by name: project to settings')
+          .click();
+        cy.location().should(loc => {
+          expect(loc.hash).to.eq('#/projects/pr2/settings');
+        });
+        cy.wrap($iframeBody).should('contain', 'Settings');
+        cy.wrap($iframeBody)
+          .contains('Click here')
+          .click();
+        cy.location().should(loc => {
+          expect(loc.hash).to.eq('#/projects/pr2');
+        });
+        //fifth
+        cy.wrap($iframeBody)
+          .contains('project to settings with params (foo=bar)')
+          .click();
+        cy.wrap($iframeBody).should('contain', 'Called with params:');
+        cy.wrap($iframeBody).should('contain', '"foo": "bar"');
+        cy.location().should(loc => {
+          expect(loc.hash).to.eq('#/projects/pr2/settings?~foo=bar&');
+        });
+        cy.wrap($iframeBody)
+          .contains('Click here')
+          .click();
+        cy.location().should(loc => {
+          expect(loc.hash).to.eq('#/projects/pr2');
+        });
+        //sixth
+        cy.wrap($iframeBody)
+          .contains('parent by name: with nonexisting context')
+          .click();
+        cy.location().should(loc => {
+          expect(loc.hash).to.eq('#/projects/pr2');
+        });
+        //seventh
+        cy.wrap($iframeBody)
+          .contains('with preserved view: project to settings and back')
+          .click();
+        cy.location().should(loc => {
+          expect(loc.hash).to.eq('#/projects/pr2/settings');
+        });
+        cy.wrap($iframeBody).should('contain', 'Settings');
+        //   .clear()
+        //   .type('test value');
+        // cy.wrap($iframeBody)
+        //   .contains('Click here')
+        //   .click();
       });
     });
   });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -39,3 +39,18 @@ Cypress.Commands.add('login', (email, password) => {
 
   cy.get('#login-button').click();
 });
+
+Cypress.Commands.add('goToFeaturesPage', iframe => {
+  cy.wrap(iframe)
+    .contains('linkManager()')
+    .click();
+  cy.location().should(loc => {
+    expect(loc.hash).to.eq('#/projects/pr2');
+  });
+  cy.wrap(iframe).should('contain', 'LuigiClient uxManager methods:');
+  cy.wrap(iframe).should('contain', 'LuigiClient linkManager methods:');
+});
+
+Cypress.Commands.add('goToOverviewPage', () => {
+  cy.get('.fd-global-nav__product-name').click();
+});
