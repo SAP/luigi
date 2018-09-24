49c8ce76ec72c67df19df6515fe70b59df6ab211
Markus Edenhauser <1720843+maxmarkus@users.noreply.github.com>
2018-09-24 16:47:44 +0200

    small fix for undefined param

--- core/examples/luigi-sample-angular/src/app/services/helpers.ts
+++ core/examples/luigi-sample-angular/src/app/services/helpers.ts
@@ -1,4 +1,4 @@
-export const toTitleCase = (str: string): string => {
+export const toTitleCase = (str: string = ''): string => {
   return str.replace(/\w\S*/g, function (txt) {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
   });
