b9e7f0f9a357fc3742cb8ca5e6b2d65bb804b515
dariadomagala <daria.domagala@sap.com>
2018-09-27 15:24:08 +0200

    Minor changes

--- core/examples/luigi-sample-angular/e2e/tests/login-flow.spec.js
+++ core/examples/luigi-sample-angular/e2e/tests/login-flow.spec.js
@@ -1,31 +1,29 @@
-describe('Luigi Sample Application', () => {
+describe('Login Flow', () => {
   beforeEach(() => {
     cy.visit('http://localhost:4200');
   });
 
-  describe('Login Flow', () => {
-    it('Login', () => {
-      cy.login('tets@email.com', 'tets');
-      cy.location().should(loc => {
-        expect(loc.hash).to.eq('#/overview');
-      });
+  it('Login', () => {
+    cy.login('tets@email.com', 'tets');
+    cy.location().should(loc => {
+      expect(loc.hash).to.eq('#/overview');
+    });
+  });
+
+  it('Logout and login again', () => {
+    cy.login('tets@email.com', 'tets');
+
+    //logout
+    cy.get('.sap-icon--customer').click();
+    cy.contains('Logout').click();
+    cy.get('body').should('contain', 'Logout successful');
+    cy.location().should(loc => {
+      expect(loc.pathname).to.eq('/logout.html');
     });
 
-    it('Logout and login again', () => {
-      cy.login('tets@email.com', 'tets');
-
-      //logout
-      cy.get('.sap-icon--customer').click();
-      cy.contains('Logout').click();
-      cy.get('body').should('contain', 'Logout successful');
-      cy.location().should(loc => {
-        expect(loc.pathname).to.eq('/logout.html');
-      });
-
-      //login again
-      cy.contains('Login again').click();
-      cy.get('body').should('contain', 'Login to Luigi sample app');
-      cy.login('tets@email.com', 'tets');
-    });
+    //login again
+    cy.contains('Login again').click();
+    cy.get('body').should('contain', 'Login to Luigi sample app');
+    cy.login('tets@email.com', 'tets');
   });
 });
