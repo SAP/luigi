1385ab47eca43011951e8fbeeec71ba29ae6de1f
Sudczak, Jan <jan.sudczak@sap.com>
2018-10-25 10:29:41 +0200

    Test the containsAllSegments function

--- core/test/utilities/helpers.spec.js
+++ core/test/utilities/helpers.spec.js
@@ -1,7 +1,13 @@
+const chai = require('chai');
+
+const assert = chai.assert;
+
+import { containsAllSegments } from '../../src/utilities/helpers';
+
 describe('Helpers()', () => {
   describe('#containsAllSegments()', () => {
-    it('should return true ', async () => {
-      const sourceUrl = '#/mas/ko/pa/tol';
+    it('should return true when proper data provided', async () => {
+      const sourceUrl = 'mas/ko/pa/tol';
       const targetPathSegments = [
         {
           //doesn't matter, it's omitted anyway
@@ -19,6 +25,56 @@ describe('Helpers()', () => {
           pathSegment: 'tol'
         }
       ];
+      assert.equal(containsAllSegments(sourceUrl, targetPathSegments), true);
+    });
+
+    it('should return false when totally wrong data provided', async () => {
+      const sourceUrl = 'mas/ko/pa/tol';
+      const targetPathSegments = [
+        {
+          //doesn't matter, it's omitted anyway
+        },
+        {
+          pathSegment: 'Sinon'
+        },
+        {
+          pathSegment: 'is'
+        },
+        {
+          pathSegment: 'totally'
+        },
+        {
+          pathSegment: 'unusable'
+        }
+      ];
+      assert.equal(containsAllSegments(sourceUrl, targetPathSegments), false);
+    });
+
+    it("should return false when pathSegments numbers don't match", async () => {
+      const tooShortSourceUrl = 'one/two';
+      const tooLongSourceUrl = 'lets/change/Sinon/to/Jest/someday';
+      const targetPathSegments = [
+        {
+          //doesn't matter, it's omitted anyway
+        },
+        {
+          pathSegment: 'one'
+        },
+        {
+          pathSegment: 'two'
+        },
+        {
+          pathSegment: 'three'
+        }
+      ];
+      assert.equal(
+        containsAllSegments(tooShortSourceUrl, targetPathSegments),
+        false
+      );
+      assert.equal(
+        containsAllSegments(tooLongSourceUrl, targetPathSegments),
+        false
+      );
     });
   });
 });
