c3b70795f8b106e7d09d8089f851c0a451d5bdf9
y-kkamil <>
2018-11-06 10:00:34 +0100

    change bug in library-loaders

--- core/src/providers/auth/libraryLoaders.js
+++ core/src/providers/auth/libraryLoaders.js
@@ -2,7 +2,7 @@ export const authLibraries = {
   openIdConnect: () => {
     const oidc = document.createElement('script');
     oidc.setAttribute('type', 'text/javascript');
-    oidc.setAttribute('src', '/luigi-core/public/auth/oidc/oidc-client.min.js');
+    oidc.setAttribute('src', '/luigi-core/auth/oidc/oidc-client.min.js');
     document.body.appendChild(oidc);
   }
 };
