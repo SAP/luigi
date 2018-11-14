fe125273798fcdb79563e602338dc8bcb3647a21
Jesus Real <8238188+jesusreal@users.noreply.github.com>
2018-11-14 15:35:58 +0100

    cleanup public folder on each webpack build

--- core/webpack.config.js
+++ core/webpack.config.js
@@ -3,6 +3,7 @@ const babelSettings = JSON.parse(readFileSync('.babelrc'));
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const sass = require('node-sass');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -85,11 +86,23 @@ module.exports = {
     ]
   },
   plugins: [
+    new CleanWebpackPlugin(['public'], {
+      exclude: ['package.json'],
+      verbose: true
+    }),
     new ExtractTextPlugin('luigi.css'),
     new CopyWebpackPlugin([
       {
         from: 'node_modules/oidc-client/dist/oidc-client.min.js',
         to: 'auth/oidc/'
+      },
+      {
+        from: 'src/auth/oauth2/callback.html',
+        to: 'auth/oauth2/callback.html'
+      },
+      {
+        from: 'src/auth/oidc/silent-callback.html',
+        to: 'auth/oidc/silent-callback.html'
       }
     ])
   ],
