a25af8411dce0fa50217c216d8a168b4bed8e29d
Sudczak, Jan <jan.sudczak@sap.com>
2018-11-19 13:56:12 +0100

    Make Angular running natively on both routing modes

--- core/examples/luigi-sample-angular/src/app/app-routing.module.ts
+++ core/examples/luigi-sample-angular/src/app/app-routing.module.ts
@@ -60,11 +60,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, {
-      useHash: true
-    })
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
