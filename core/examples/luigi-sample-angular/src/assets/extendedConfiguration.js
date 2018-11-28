0b761a8becdd1fc5205deb43c1395c09542dc856
Markus <1720843+maxmarkus@users.noreply.github.com>
2018-11-28 13:04:35 +0100

    Improvements for oidc auth (#251)
    * added possibility for root redirect_uris by disabling routing if access_token or id_token exist in the url, small contextswitcher bug, refactored backdrop since inline if-elses are breaking in svelte oncreate,
    * iframe resiliency for logout
    * added logout.html and changed logoutUrl, fixed small bugs causing errors
    * added skipRoutingForUrlPatterns configuration setting for skipping route handling
    * Update docs/navigation-configuration.md

--- core/examples/luigi-sample-angular/src/assets/extendedConfiguration.js
+++ core/examples/luigi-sample-angular/src/assets/extendedConfiguration.js
@@ -533,11 +533,25 @@ Luigi.setConfig({
   },
   routing: {
     /**
+     * useHashRouting
+     *
      * Development:
      * For hash routing, set to true and run `npm run start`
      * For path routing, set to false and run `npm run startWebpack`
      */
-    useHashRouting: true
+    useHashRouting: true,
+    /**
+     * Prefix for reflecting params in the url, which is used when navigating .withParams() function.
+     */
+    nodeParamPrefix: '~',
+    /**
+     * skipRoutingForUrlPatterns
+     * Define regex patterns that prevent the router from handling path changes.
+     * Used to exclude authentication or other callbacks.
+     *
+     * Default: /access_token=/, /id_token=/
+     */
+    skipRoutingForUrlPatterns: [/access_token=/, /id_token=/]
   },
   settings: {
     header: () => ({
