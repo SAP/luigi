a856400f993fcd296250009f26df2d8224981dad
Markus Edenhauser <1720843+maxmarkus@users.noreply.github.com>
2018-12-05 19:19:45 +0100

    added babel and cleaned up webpack config

--- core/examples/luigi-sample-angular/config/extendedConfiguration.js
+++ core/examples/luigi-sample-angular/config/extendedConfiguration.js
@@ -1,7 +1,7 @@
 import { navigationPermissionChecker } from './config.helpers.js';
 
-var getAllProjects = function () {
-  return new Promise(function (resolve) {
+const getAllProjects = () => {
+  return new Promise(function(resolve) {
     resolve([
       {
         id: 'pr1',
@@ -15,8 +15,8 @@ var getAllProjects = function () {
   });
 };
 
-var getProjectPlugins = function (projectId) {
-  return new Promise(function (resolve) {
+var getProjectPlugins = function(projectId) {
+  return new Promise(function(resolve) {
     if (projectId === 'pr2') {
       resolve([
         {
@@ -54,8 +54,8 @@ var getProjectPlugins = function (projectId) {
   });
 };
 
-var projectDetailNavProviderFn = function (context) {
-  return new Promise(function (resolve) {
+var projectDetailNavProviderFn = function(context) {
+  return new Promise(function(resolve) {
     var projectId = context.currentProject;
     var children = [
       {
@@ -250,8 +250,8 @@ var projectDetailNavProviderFn = function (context) {
         }
       }
     ];
-    getProjectPlugins(projectId).then(function (result) {
-      result.forEach(function (plugin) {
+    getProjectPlugins(projectId).then(function(result) {
+      result.forEach(function(plugin) {
         children.push({
           category: plugin.category,
           pathSegment: plugin.viewId,
@@ -265,11 +265,11 @@ var projectDetailNavProviderFn = function (context) {
   });
 };
 
-var projectsNavProviderFn = function (context) {
-  return new Promise(function (resolve) {
-    getAllProjects().then(function (result) {
+var projectsNavProviderFn = function(context) {
+  return new Promise(function(resolve) {
+    getAllProjects().then(function(result) {
       var children = [];
-      result.forEach(function (project) {
+      result.forEach(function(project) {
         children.push({
           /**
            * navigationContext:
@@ -313,7 +313,7 @@ Luigi.setConfig({
     mockAuth: {
       authorizeUrl: `${
         window.location.origin
-        }/assets/auth-mock/login-mock.html`,
+      }/assets/auth-mock/login-mock.html`,
       logoutUrl: `${window.location.origin}/assets/auth-mock/logout-mock.html`,
       post_logout_redirect_uri: '/logout.html',
       authorizeMethod: 'GET',
@@ -379,24 +379,24 @@ Luigi.setConfig({
     },
 
     events: {
-      onLogout: function () {
+      onLogout: function() {
         console.log('onLogout');
       },
-      onAuthSuccessful: function (data) {
+      onAuthSuccessful: function(data) {
         console.log('onAuthSuccessful', data);
       },
-      onAuthExpired: function () {
+      onAuthExpired: function() {
         console.log('onAuthExpired');
       },
       // TODO: define luigi-client api for getting errors
-      onAuthError: function (err) {
+      onAuthError: function(err) {
         console.log('authErrorHandler 1', err);
       }
     }
   },
   navigation: {
     nodeAccessibilityResolver: navigationPermissionChecker,
-    nodes: function () {
+    nodes: function() {
       return [
         {
           pathSegment: 'overview',
--- core/examples/luigi-sample-angular/webpack-config.config.js
+++ core/examples/luigi-sample-angular/webpack-config.config.js
@@ -1,162 +1,4 @@
 const path = require('path');
-const ProgressPlugin = require('webpack/lib/ProgressPlugin');
-const CircularDependencyPlugin = require('circular-dependency-plugin');
-
-const {
-  NoEmitOnErrorsPlugin,
-  SourceMapDevToolPlugin,
-  NamedModulesPlugin
-} = require('webpack');
-
-const entryPoints = ['extendedConfiguration'];
-
-// const projectRoot = process.cwd() + '/config';
-const projectRoot = path.resolve(__dirname, 'config');
-
-// module.exports = {
-//   resolve: {
-//     extensions: ['.js'],
-//     symlinks: true,
-//     modules: ['./config', './node_modules'],
-//     // alias: rxPaths(),
-//     mainFields: ['browser', 'module', 'main']
-//   },
-//   resolveLoader: {
-//     modules: ['./node_modules'],
-//     // alias: rxPaths()
-//   },
-//   entry: {
-//     main: ['./config/extendedConfiguration.ts'],
-//     // polyfills: ['./src/polyfills.ts'],
-//     styles: cssFiles
-//   },
-//   output: {
-//     path: path.join(process.cwd(), 'dist'),
-//     filename: '[name].extended.js',
-//     chunkFilename: '[id].chunk.js',
-//     crossOriginLoading: false
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.(jpg|png|webp|gif|otf|ttf|woff|woff2|ani)$/,
-//         loader: 'url-loader',
-//         options: {
-//           name: '[name].[hash:20].[ext]',
-//           limit: 10000
-//         }
-//       }
-//     ]
-//   },
-//   plugins: [
-//     new NoEmitOnErrorsPlugin(),
-//     // new CopyWebpackPlugin(
-//     //   [
-//     //     {
-//     //       context: 'src',
-//     //       to: '',
-//     //       from: {
-//     //         glob: 'assets/**/*',
-//     //         dot: true
-//     //       }
-//     //     }
-//     //   ],
-//     //   {
-//     //     ignore: ['.gitkeep', '**/.DS_Store', '**/Thumbs.db'],
-//     //     debug: 'warning'
-//     //   }
-//     // ),
-//     new ProgressPlugin(),
-//     new CircularDependencyPlugin({
-//       exclude: /(\\|\/)node_modules(\\|\/)/,
-//       failOnError: false,
-//       onDetected: false,
-//       cwd: projectRoot
-//     }),
-//     new SourceMapDevToolPlugin({
-//       filename: '[file].map[query]',
-//       moduleFilenameTemplate: '[resource-path]',
-//       fallbackModuleFilenameTemplate: '[resource-path]?[hash]',
-//       sourceRoot: 'webpack:///'
-//     }),
-//     new NamedModulesPlugin({})
-//   ],
-//   optimization: {
-//     splitChunks: {
-//       chunks: 'all'
-//     }
-//   },
-//   node: {
-//     fs: 'empty',
-//     global: true,
-//     crypto: 'empty',
-//     tls: 'empty',
-//     net: 'empty',
-//     process: true,
-//     module: false,
-//     clearImmediate: false,
-//     setImmediate: false
-//   }
-// };
-
-// const path = require('path');
-
-// module.exports = {
-//   target: 'es5',
-//   entry: {
-//     app: [
-//       'babel-polyfill',
-//       './config/extendedConfiguration.ts'
-//     ]
-//   },
-//   output: {
-//     path: path.resolve(__dirname, 'dist'),
-//     filename: 'extendedConfig.bundle.js'
-//   },
-//   plugins: [
-//     new NoEmitOnErrorsPlugin(),
-//     new ProgressPlugin(),
-//     new CircularDependencyPlugin({
-//       exclude: /(\\|\/)node_modules(\\|\/)/,
-//       failOnError: false,
-//       onDetected: false,
-//       cwd: projectRoot
-//     }),
-//     new SourceMapDevToolPlugin({
-//       filename: '[file].map[query]',
-//       moduleFilenameTemplate: '[resource-path]',
-//       fallbackModuleFilenameTemplate: '[resource-path]?[hash]',
-//       sourceRoot: 'webpack:///'
-//     }),
-//     new NamedModulesPlugin({})
-//   ],
-//   module: {
-//     loaders: [{
-//       test: /\.js?$/,
-//       exclude: /node_modules/,
-//       loader: 'babel-loader',
-//       query: {
-//         presets: ['env']
-//       }
-//     }]
-//   },
-//   optimization: {
-//     splitChunks: {
-//       chunks: 'all'
-//     }
-//   },
-//   node: {
-//     fs: 'empty',
-//     global: true,
-//     crypto: 'empty',
-//     tls: 'empty',
-//     net: 'empty',
-//     process: true,
-//     module: false,
-//     clearImmediate: false,
-//     setImmediate: false
-//   }
-// };
 
 module.exports = {
   target: 'web',
@@ -166,8 +8,26 @@ module.exports = {
   output: {
     filename: 'extendedConfiguration.bundle.js',
     path: path.resolve(__dirname, 'src', 'assets')
-  }
-  // plugins: [
-  //   new NoEmitOnErrorsPlugin()
-  // ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              // "es2015",
+              '@babel/preset-env'
+            ]
+          }
+        }
+      }
+    ]
+  },
+  stats: {
+    colors: true
+  },
+  devtool: 'source-map'
 };
