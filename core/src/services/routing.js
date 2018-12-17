f5f7772b7454ccf8b2c353032eb06ddc2097b395
Sudczak, Jan <jan.sudczak@sap.com>
2018-12-17 09:37:43 +0100

    xd44

--- core/src/services/routing.js
+++ core/src/services/routing.js
@@ -2,6 +2,7 @@ import * as Navigation from '../navigation/services/navigation';
 import * as RoutingHelpers from '../utilities/helpers/routing-helpers';
 import { LuigiConfig } from './config';
 import * as GenericHelpers from '../utilities/helpers/generic-helpers';
+import * as Iframe from './iframe';
 
 export const getNodePath = node => {
   return node
@@ -128,3 +129,119 @@ export const getCurrentPath = () =>
   LuigiConfig.getConfigValue('routing.useHashRouting')
     ? window.location.hash.replace('#', '') // TODO: GenericHelpers.getPathWithoutHash(window.location.hash) fails in ContextSwitcher
     : GenericHelpers.trimLeadingSlash(window.location.pathname);
+
+export const handleRouteChange = async (path, component, node, config) => {
+  const defaultPattern = [/access_token=/, /id_token=/];
+  const patterns =
+    LuigiConfig.getConfigValue('routing.skipRoutingForUrlPatterns') ||
+    defaultPattern;
+  const hasSkipMatches =
+    patterns.filter(p => window.location.href.match(p)).length !== 0;
+  if (hasSkipMatches) {
+    return;
+  }
+  try {
+    const pathUrl =
+      path && path.length ? GenericHelpers.getPathWithoutHash(path) : '';
+    const pathData = await Navigation.getNavigationPath(
+      LuigiConfig.getConfigValueAsync('navigation.nodes'),
+      pathUrl.split('?')[0]
+    );
+
+    const hideNav = LuigiConfig.getConfigBooleanValue(
+      'settings.hideNavigation'
+    );
+
+    const {
+      viewUrl = '',
+      isolateView = false,
+      hideSideNav = false
+    } = RoutingHelpers.getLastNodeObject(pathData);
+    const params = RoutingHelpers.parseParams(pathUrl.split('?')[1]);
+    const nodeParams = RoutingHelpers.getNodeParams(params);
+    const pathParams = RoutingHelpers.getPathParams(pathData.navigationPath);
+    const viewGroup = RoutingHelpers.findViewGroup(
+      RoutingHelpers.getLastNodeObject(pathData)
+    );
+
+    if (!viewUrl) {
+      const routeExists = RoutingHelpers.isExistingRoute(path, pathData);
+
+      if (routeExists) {
+        const defaultChildNode = await RoutingHelpers.getDefaultChildNode(
+          pathData
+        );
+        navigateTo(`${pathUrl ? `/${pathUrl}` : ''}/${defaultChildNode}`);
+      } else {
+        const alert = {
+          message: 'Could not find the requested route',
+          link: pathUrl
+        };
+
+        component.set({ alert });
+        navigateTo('/');
+        //error 404
+      }
+      return;
+    }
+
+    if (!GenericHelpers.containsAllSegments(pathUrl, pathData.navigationPath)) {
+      const matchedPath = await matchPath(pathUrl);
+
+      const alert = {
+        message: 'Could not map the exact target node for the requested route',
+        link: pathUrl
+      };
+
+      component.set({ alert });
+      navigateTo(matchedPath);
+    }
+
+    const previousCompData = component.get();
+    component.set({
+      hideNav,
+      hideSideNav,
+      viewUrl,
+      navigationPath: pathData.navigationPath,
+      currentNode:
+        pathData.navigationPath && pathData.navigationPath.length > 0
+          ? pathData.navigationPath[pathData.navigationPath.length - 1]
+          : null,
+      context: pathData.context,
+      nodeParams,
+      pathParams,
+      isolateView,
+      viewGroup,
+      previousNodeValues: previousCompData
+        ? {
+            viewUrl: previousCompData.viewUrl,
+            isolateView: previousCompData.isolateView,
+            viewGroup: previousCompData.viewGroup
+          }
+        : {}
+    });
+
+    Iframe.navigateIframe(config, component, node);
+  } catch (err) {
+    console.info('Could not handle route change', err);
+  }
+};
+
+export const handleRouteClick = node => {
+  if (node.externalLink && node.externalLink.url) {
+    node.externalLink.sameWindow
+      ? (window.location.href = node.externalLink.url)
+      : window.open(node.externalLink.url).focus();
+    // externalLinkUrl property is provided so there's no need to trigger routing mechanizm
+    return;
+  } else if (node.link) {
+    const link = node.link.startsWith('/')
+      ? node.link
+      : buildFromRelativePath(node);
+    navigateTo(link);
+    return;
+  } else {
+    const route = RoutingHelpers.buildRoute(node, `/${node.pathSegment}`);
+    navigateTo(route);
+  }
+};
