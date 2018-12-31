712c8c5e366c2b8def5b961c6dae52d2579cd823
y-kkamil <>
2018-12-31 10:05:30 +0100

    change api documentation

--- client/luigi-client.js
+++ client/luigi-client.js
@@ -210,6 +210,7 @@
      * Returns the node parameters of the active URL.
      * Node parameters are defined like URL query parameters but with a specific prefix allowing Luigi to pass them to the micro front-end view.  The default prefix is **~** and you can use it in the following way: `https://my.luigi.app/home/products?~sort=asc~page=3`.
      * @returns {Object} node parameters, where the object property name is the node parameter name without the prefix, and its value is the value of the node parameter. For example `{sort: 'asc', page: 3}`.
+     * **Note:** some special characters (`<`, `>`, `"`, `'`, `/`) in node parameters are HTML-encoded.
      * @memberof lifecycle
      */
     getNodeParams: function getNodeParams() {
@@ -220,6 +221,7 @@
      * Path parameters are defined by navigation nodes with a dynamic **pathSegment** value starting with **:**, such as **productId**.
      * All path parameters in the current navigation path (as defined by the active URL) are returned.
      * @returns {Object} path parameters, where the object property name is the path parameter name without the prefix, and its value is the actual value of the path parameter. For example ` {productId: 1234, ...}`.
+     * **Note:** some special characters (`<`, `>`, `"`, `'`, `/`) in path parameters are HTML-encoded.
      * @memberof lifecycle
      */
     getPathParams: function getPathParams() {
