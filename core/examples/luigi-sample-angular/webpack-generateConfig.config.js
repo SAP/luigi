8dc399512c743aba37e6d610334daf5d210f364f
Sudczak, Jan <jan.sudczak@sap.com>
2019-01-03 15:48:43 +0100

    Rename bundled config

--- core/examples/luigi-sample-angular/webpack-generateConfig.config.js
+++ core/examples/luigi-sample-angular/webpack-generateConfig.config.js
@@ -5,20 +5,19 @@ module.exports = {
   watch: true,
   mode: 'production',
   entry: {
-    generatedExtendedConfig: './src/assets/luigi-config/extended/main.js',
-    generatedBasicConfig:
-      './src/assets/luigi-config/basic/basicConfiguration.js'
+    extendedConfiguration: './src/assets/luigi-config/extended/main.js',
+    basicConfiguration: './src/assets/luigi-config/basic/basicConfiguration.js'
   },
   output: {
-    filename: '[name].js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'src/assets')
   },
   plugins: [
     new webpack.BannerPlugin(
       `
       Don't be afraid!
-      This file was automatically generated and you should not modify it.
-      Go to 'luigi-config' folder and modify Luigi configuration there with a pleasure.
+      This file was generated automatically and you should not modify it.
+      The documentation will tell you how to modify Luigi configuration with pleasure.
       `
     )
   ],
