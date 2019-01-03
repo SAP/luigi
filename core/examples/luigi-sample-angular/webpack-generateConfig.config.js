b6e9676124284ace9a322be0664485cac6e68d73
Sudczak, Jan <jan.sudczak@sap.com>
2019-01-03 15:27:23 +0100

    Use class-properties feature & add description to generated file

--- core/examples/luigi-sample-angular/webpack-generateConfig.config.js
+++ core/examples/luigi-sample-angular/webpack-generateConfig.config.js
@@ -1,10 +1,33 @@
 const path = require('path');
+const webpack = require('webpack');
+
 module.exports = {
   watch: true,
   mode: 'production',
-  entry: { generatedLuigiConfig: './src/assets/luigi-config/main.js' },
+  entry: {
+    generatedLuigiConfig: './src/assets/luigi-config/main.js'
+  },
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'src/assets')
+  },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: `
+      Don't be afraid!
+      This file was automatically generated and you should not modify it.
+      Go to 'luigi-config' folder and modify Luigi configuration there with a pleasure.
+      `
+    })
+  ],
+  module: {
+    rules: [
+      {
+        loader: 'babel-loader',
+        options: {
+          rootMode: 'root'
+        }
+      }
+    ]
   }
 };
