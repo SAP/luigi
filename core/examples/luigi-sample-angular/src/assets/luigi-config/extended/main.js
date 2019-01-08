eddd9eadde6a9da9465bac47cb4b4297ad182f7b
Sudczak, Jan <jan.sudczak@sap.com>
2019-01-08 12:20:53 +0100

    Remove obsolete lines

--- core/examples/luigi-sample-angular/src/assets/luigi-config/extended/main.js
+++ core/examples/luigi-sample-angular/src/assets/luigi-config/extended/main.js
@@ -2,9 +2,6 @@ import { auth } from './auth';
 import { navigation } from './navigation';
 import { routing } from './routing';
 import { settings } from './settings';
-// require('webpack').BannerPlugin({
-//   banner: 'hello world'
-// });
 
 Luigi.setConfig({
   auth,
