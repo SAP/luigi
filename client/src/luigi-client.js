7373a54b3cf292d6a1b5e920f6ec778addf71e6b
Sudczak, Jan <jan.sudczak@sap.com>
2019-01-11 11:23:35 +0100

    remove object.assign polyfill

--- client/src/luigi-client.js
+++ client/src/luigi-client.js
@@ -1,32 +1,3 @@
-// Object.assign polyfill
-if (typeof Object.assign != 'function') {
-  Object.defineProperty(Object, 'assign', {
-    value: function assign(target, varArgs) {
-      'use strict';
-      if (target == null) {
-        throw new TypeError('Cannot convert undefined or null to object');
-      }
-
-      var to = Object(target);
-
-      for (var index = 1; index < arguments.length; index++) {
-        var nextSource = arguments[index];
-
-        if (nextSource != null) {
-          for (var nextKey in nextSource) {
-            if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
-              to[nextKey] = nextSource[nextKey];
-            }
-          }
-        }
-      }
-      return to;
-    },
-    writable: true,
-    configurable: true
-  });
-}
-
 // actual Luigi code
 var luigiInitialized = false;
 var defaultContextKeys = ['context', 'internal', 'nodeParams', 'pathParams'];
