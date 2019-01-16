9020b75d33227e52f73f3d6ee644586c4afc9e32
Philipp Pracht <philipp.pracht@sap.com>
2019-01-16 18:10:21 +0100

    Add collapsible navigation node groups (#357)
    Add collapsible navigation node groups

--- core/src/utilities/helpers/navigation-helpers.js
+++ core/src/utilities/helpers/navigation-helpers.js
@@ -1,6 +1,9 @@
 // Helper methods for 'navigation.js' file. They don't require any method from 'navigation.js` but are required by them.
 import { LuigiConfig } from '../../services/config';
 import * as AuthHelpers from './auth-helpers';
+
+const EXP_CAT_KEY = 'luigi.preferences.navigation.expandedCategories';
+
 export const isNodeAccessPermitted = (
   nodeToCheckPermissionFor,
   parentNode,
@@ -42,6 +45,15 @@ export const applyContext = (context, addition, navigationContext) => {
   return context;
 };
 
+// TODO: use one extracted generic helper function here and in routing.js
+export const getNodePath = node => {
+  if (node.parent) {
+    return getNodePath(node.parent) + '/' + node.pathSegment;
+  } else {
+    return node.pathSegment;
+  }
+};
+
 export const groupNodesBy = (nodes, property) => {
   const result = {};
   nodes.forEach(node => {
@@ -66,9 +78,43 @@ export const groupNodesBy = (nodes, property) => {
     if (!arr.metaInfo) {
       arr.metaInfo = metaInfo;
     }
+    if (!arr.metaInfo.categoryUid && key && arr.metaInfo.collapsible) {
+      arr.metaInfo.categoryUid = node.parent
+        ? getNodePath(node.parent) + ':' + key
+        : key;
+    }
     arr.push(node);
   });
   return result;
+};
+
+export const loadExpandedCategories = () => {
+  let expandedList = [];
+  const expString = localStorage.getItem(EXP_CAT_KEY);
+  if (expString) {
+    try {
+      expandedList = JSON.parse(expString);
+    } catch (e) {
+      console.warn('Preference data corrupted, using default');
+    }
+  }
+  return expandedList;
+};
+
+export const storeExpandedState = (key, value) => {
+  const expandedList = loadExpandedCategories();
+  if (value) {
+    if (expandedList.indexOf(key) < 0) {
+      expandedList.push(key);
+    }
+  } else {
+    let index = expandedList.indexOf(key);
+    if (index >= 0) {
+      expandedList.splice(index, 1);
+    }
+  }
+  localStorage.setItem(EXP_CAT_KEY, JSON.stringify(expandedList));
+  return expandedList;
 };
 
 export const isOpenUIiconName = string => /^[a-z0-9\-]+$/i.test(string);
