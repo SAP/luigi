2bfad55c04374ee205be9444c3bf5296cfa8a1d5
Peter Kurajsky <pekura@users.noreply.github.com>
2019-01-22 10:47:56 +0100

    Relative navigation from client does not work with dynamic nodes (#362)
    Relative navigation from client does not work with dynamic nodes in the parent path - fixed.

--- core/src/services/routing.js
+++ core/src/services/routing.js
@@ -108,9 +108,9 @@ export const getCurrentPath = () =>
   LuigiConfig.getConfigValue('routing.useHashRouting')
     ? window.location.hash.replace('#', '') // TODO: GenericHelpers.getPathWithoutHash(window.location.hash) fails in ContextSwitcher
     : window.location.search
-      ? GenericHelpers.trimLeadingSlash(window.location.pathname) +
-        window.location.search
-      : GenericHelpers.trimLeadingSlash(window.location.pathname);
+    ? GenericHelpers.trimLeadingSlash(window.location.pathname) +
+      window.location.search
+    : GenericHelpers.trimLeadingSlash(window.location.pathname);
 
 export const handleRouteChange = async (
   path,
