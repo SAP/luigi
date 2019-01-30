e9eca1ce38b18d61432fe8a48f005a149638172f
Jesus Real <8238188+jesusreal@users.noreply.github.com>
2019-01-30 14:06:18 +0100

    text changes

--- core/examples/luigi-sample-angular/src/app/project/project.component.ts
+++ core/examples/luigi-sample-angular/src/app/project/project.component.ts
@@ -97,7 +97,7 @@ export class ProjectComponent implements OnInit, OnDestroy {
   showConfirmationModal() {
     this.confirmationModalResult.currentKey = '';
     const content = {
-      title: 'Modal Header',
+      title: 'Modal Header - Luigi modal',
       text: `Lorem tipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
         aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`,
