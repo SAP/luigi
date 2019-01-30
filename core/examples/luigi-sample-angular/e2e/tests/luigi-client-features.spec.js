748583b1bfd3b73d75204db3c5461d3b3af103ab
Jesus Real <8238188+jesusreal@users.noreply.github.com>
2019-01-30 14:25:13 +0100

    Uncomment e2e tests

--- core/examples/luigi-sample-angular/e2e/tests/luigi-client-features.spec.js
+++ core/examples/luigi-sample-angular/e2e/tests/luigi-client-features.spec.js
@@ -13,87 +13,87 @@ describe('Luigi client features', () => {
       const $iframeBody = $iframe.contents().find('body');
       cy.goToLinkManagerMethods($iframeBody);
 
-    //   //navigate using absolute path
-    //   cy.wrap($iframeBody)
-    //     .contains('absolute: to overview')
-    //     .click();
-    //   cy.expectPathToBe('/overview');
+      //navigate using absolute path
+      cy.wrap($iframeBody)
+        .contains('absolute: to overview')
+        .click();
+      cy.expectPathToBe('/overview');
 
-    //   cy.goToLinkManagerMethods($iframeBody);
+      cy.goToLinkManagerMethods($iframeBody);
 
-    //   //navigate using relative path
-    //   cy.wrap($iframeBody)
-    //     .contains('relative: to stakeholders')
-    //     .click();
-    //   cy.expectPathToBe('/projects/pr2/users/groups/stakeholders');
+      //navigate using relative path
+      cy.wrap($iframeBody)
+        .contains('relative: to stakeholders')
+        .click();
+      cy.expectPathToBe('/projects/pr2/users/groups/stakeholders');
 
-    //   cy.goToOverviewPage();
-    //   cy.goToLinkManagerMethods($iframeBody);
+      cy.goToOverviewPage();
+      cy.goToLinkManagerMethods($iframeBody);
 
-    //   //navigate using closest context
-    //   cy.wrap($iframeBody)
-    //     .contains('closest parent: to stakeholders')
-    //     .click();
-    //   cy.expectPathToBe('/projects/pr2/users/groups/stakeholders');
+      //navigate using closest context
+      cy.wrap($iframeBody)
+        .contains('closest parent: to stakeholders')
+        .click();
+      cy.expectPathToBe('/projects/pr2/users/groups/stakeholders');
 
-    //   cy.goToOverviewPage();
-    //   cy.goToLinkManagerMethods($iframeBody);
+      cy.goToOverviewPage();
+      cy.goToLinkManagerMethods($iframeBody);
 
-    //   //navigate using context
-    //   cy.wrap($iframeBody)
-    //     .contains('parent by name: project to settings')
-    //     .click();
-    //   cy.expectPathToBe('/projects/pr2/settings');
+      //navigate using context
+      cy.wrap($iframeBody)
+        .contains('parent by name: project to settings')
+        .click();
+      cy.expectPathToBe('/projects/pr2/settings');
 
-    //   cy.wrap($iframeBody).should('contain', 'Settings');
-    //   cy.wrap($iframeBody)
-    //     .contains('Click here')
-    //     .click();
-    //   cy.expectPathToBe('/projects/pr2');
+      cy.wrap($iframeBody).should('contain', 'Settings');
+      cy.wrap($iframeBody)
+        .contains('Click here')
+        .click();
+      cy.expectPathToBe('/projects/pr2');
 
-    //   //navigate with params
-    //   cy.wrap($iframeBody)
-    //     .contains('project to settings with params (foo=bar)')
-    //     .click();
-    //   cy.wrap($iframeBody).should('contain', 'Called with params:');
-    //   cy.wrap($iframeBody).should('contain', '"foo": "bar"');
+      //navigate with params
+      cy.wrap($iframeBody)
+        .contains('project to settings with params (foo=bar)')
+        .click();
+      cy.wrap($iframeBody).should('contain', 'Called with params:');
+      cy.wrap($iframeBody).should('contain', '"foo": "bar"');
 
-    //   cy.expectSearchToBe('?~foo=bar&');
+      cy.expectSearchToBe('?~foo=bar&');
 
-    //   cy.wrap($iframeBody)
-    //     .contains('Click here')
-    //     .click();
-    //   cy.expectPathToBe('/projects/pr2');
+      cy.wrap($iframeBody)
+        .contains('Click here')
+        .click();
+      cy.expectPathToBe('/projects/pr2');
 
-    //   //don't navigate
-    //   cy.wrap($iframeBody)
-    //     .contains('parent by name: with nonexisting context')
-    //     .click();
-    //   cy.expectPathToBe('/projects/pr2');
+      //don't navigate
+      cy.wrap($iframeBody)
+        .contains('parent by name: with nonexisting context')
+        .click();
+      cy.expectPathToBe('/projects/pr2');
 
-    //   //navigate with preserve view functionality
-    //   cy.wrap($iframeBody)
-    //     .contains('with preserved view: project to global settings and back')
-    //     .click();
-    //   cy.expectPathToBe('/settings');
+      //navigate with preserve view functionality
+      cy.wrap($iframeBody)
+        .contains('with preserved view: project to global settings and back')
+        .click();
+      cy.expectPathToBe('/settings');
 
-    //   //wait for the second iFrame to be loaded
-    //   cy.wait(500);
-    //   cy.get('iframe')
-    //     .first()
-    //     .then($preserveViewiFrame => {
-    //       const $preserveViewiFrameBody = $preserveViewiFrame
-    //         .contents()
-    //         .find('body');
-    //       cy.wrap($preserveViewiFrameBody)
-    //         .find('input')
-    //         .clear()
-    //         .type('tets');
-    //       cy.wrap($preserveViewiFrameBody)
-    //         .find('button')
-    //         .click();
-    //       cy.expectPathToBe('/projects/pr2');
-    //     });
+      //wait for the second iFrame to be loaded
+      cy.wait(500);
+      cy.get('iframe')
+        .first()
+        .then($preserveViewiFrame => {
+          const $preserveViewiFrameBody = $preserveViewiFrame
+            .contents()
+            .find('body');
+          cy.wrap($preserveViewiFrameBody)
+            .find('input')
+            .clear()
+            .type('tets');
+          cy.wrap($preserveViewiFrameBody)
+            .find('button')
+            .click();
+          cy.expectPathToBe('/projects/pr2');
+        });
 
       // check if path exists
       cy.goToLinkManagerMethods($iframeBody);
