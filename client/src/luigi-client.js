1ea90343219566047b5e2e78c472cb253b2d5b29
Barbara Szwarc <barbara.m.szwarc@gmail.com>
2019-01-31 15:31:39 +0100

    Update client/src/luigi-client.js
    Co-Authored-By: jesusreal <8238188+jesusreal@users.noreply.github.com>

--- client/src/luigi-client.js
+++ client/src/luigi-client.js
@@ -423,7 +423,7 @@ const LuigiClient = {
       /**
        * Shows a confirmation modal.
        * @param {Object} content the content of the confirmation modal
-       * @param {string} content.header text for modal header
+       * @param {string} content.header the content of the modal header
        * @param {string} content.body text for modal body
        * @param {string} content.buttonConfirm text for modal confirm button
        * @param {string} content.buttonDismiss text for modal dismiss button
