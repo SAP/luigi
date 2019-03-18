8efade0f1a9ade4ed38ab90c35b4edc013e2b8b6
Sudczak, Jan <jan.sudczak@sap.com>
2019-03-18 13:34:05 +0100

    Add e2e tests

--- core/examples/luigi-sample-angular/e2e/tests/product-switcher.spec.js
+++ core/examples/luigi-sample-angular/e2e/tests/product-switcher.spec.js
@@ -3,18 +3,77 @@ describe('ProductSwitcher', () => {
     cy.visit('/');
     cy.login('tets@email.com', 'tets');
   });
+  context('Desktop', () => {
+    beforeEach(() => {
+      // run these tests as if in a desktop
+      cy.viewport('macbook-15');
+    })
 
-  it('Clicking around the product switcher', () => {
-    //check if hybris is there
-    cy.get('.fd-product-switcher')
-      .click()
-      .contains('hybris');
+    it('Clicking around the product switcher', () => {
+      //check if hybris is there
+      cy.get('.fd-product-switcher')
+        .click()
+        .contains('hybris');
 
-    //check if internal link is there
-    cy.get('.fd-product-switcher .fd-product-switcher__body')
-      .contains('Project 1')
-      .click();
+      //check if internal link is there
+      cy.get('.fd-product-switcher .fd-product-switcher__body')
+        .contains('Project 1')
+        .click();
 
-    cy.expectPathToBe('/projects/pr1');
+      cy.expectPathToBe('/projects/pr1');
+    });
+
+    it('Mobile Product Switcher is not visible', () => {
+      cy.get('[data-cy="mobile-product-switcher"]')
+      .should('not.be.visible')
+    });
+  });
+
+  context('Mobile', () => {
+    beforeEach(() =>{
+      cy.viewport('iphone-6');
+    });
+
+    it('Desktop Product Switcher is not visible', () => {
+      cy.get('[data-cy="desktop-product-switcher"]')
+      .should('not.be.visible')
+    });
+
+    it('Should be able to select product', () => {
+      cy.get('[data-cy="mobile-menu"]')
+        .click();
+
+      //open mobile product switcher
+      cy.get('[data-cy="mobile-product-switcher"]')
+        .click();
+
+      //check if internal link is there
+      cy.get('.y-full-width-list__title')
+        .contains('Project 1')
+        .click();
+
+      cy.expectPathToBe('/projects/pr1');
+    });
+
+    it('Should be able to close', () => {
+      cy.get('[data-cy="mobile-menu"]')
+        .click();
+
+      //open mobile product switcher
+      cy.get('[data-cy="mobile-product-switcher"]')
+        .click();
+      
+      //close mobile product switcher
+      cy.get('[data-cy="mobile-product-switcher-close"]')
+        .click();
+      
+      //no product switcher is visible
+      cy.get('.fd-product-switcher')
+        .should('not.be.visible')
+
+      //the path wasn't changed
+      cy.expectPathToBe('/overview');
+    });
+
   });
 });
