92ccb239c187b66a495e0cdc390f26844b96c032
Johannes Doberer <johannes.doberer@sap.com>
2019-04-02 16:05:14 +0200

    fix test

--- core/examples/luigi-sample-angular/e2e/tests/login-flow.spec.js
+++ core/examples/luigi-sample-angular/e2e/tests/login-flow.spec.js
@@ -13,12 +13,12 @@ describe('Login Flow', () => {
     //logout
     cy.get('.sap-icon--customer').click();
     cy.contains('Logout').click();
-    cy.get('body').should('contain', 'Logout successful');
+    cy.get('title').should('contain', 'Logout successful');
     cy.expectPathToBe('/logout.html');
 
     //login again
-    cy.contains('Login again').click();
-    cy.get('body').should('contain', 'Login to Luigi sample app');
+    cy.contains('Re-Login').click();
+    cy.get('h1').should('contain', 'Login to Luigi sample app');
     cy.login('tets@email.com', 'tets');
   });
 });
