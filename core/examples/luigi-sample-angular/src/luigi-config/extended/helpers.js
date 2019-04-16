9153f95c6d0ba3240e72d6aad3ff4eb5046444d3
Jesús Real Serrano <8238188+jesusreal@users.noreply.github.com>
2019-04-16 11:07:45 +0200

    Document responsive navigation setting (#475)

--- core/examples/luigi-sample-angular/src/luigi-config/extended/helpers.js
+++ core/examples/luigi-sample-angular/src/luigi-config/extended/helpers.js
@@ -109,14 +109,9 @@ export const navigationPermissionChecker = (
   // depending on the current path and context returns true or false
   // true means the current node is accessible, false the opposite
   const mockCurrentUserGroups = ['admins'];
-  if (nodeToCheckPermissionFor.constraints) {
-    // check if user has required groups
-    return (
-      nodeToCheckPermissionFor.constraints.filter(
-        c => mockCurrentUserGroups.indexOf(c) !== -1
-      ).length !== 0
-    );
-  }
-
-  return true;
+  if (!nodeToCheckPermissionFor.constraints) return true;
+  // check if user has required groups
+  return nodeToCheckPermissionFor.constraints.some(c =>
+    mockCurrentUserGroups.includes(c)
+  );
 };
