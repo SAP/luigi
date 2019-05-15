a17490a4efcb42057266fc19c930ca02feef53cb
Markus Edenhauser <1720843+maxmarkus@users.noreply.github.com>
2019-05-15 14:51:32 +0200

    fix for history api in dev server

--- core/examples/luigi-sample-angular/webpack-extra.config.js
+++ core/examples/luigi-sample-angular/webpack-extra.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   devServer: {
     // because of 404 issue, which redirected us to sampleapp.html instead of index.html
-    // "historyApiFallback": true
-    historyApiFallback: {
-      rewrites: [{ from: /./, to: './src/index.html' }]
-    }
+    historyApiFallback: true
   }
 };
