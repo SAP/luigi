63b9f2ad1a212b6c7d910474155e334b7bb3024a
Kurajsky, Peter <peter.kurajsky@sap.com>
2019-05-24 17:20:59 +0200

    code improvements

--- core/src/utilities/helpers/generic-helpers.js
+++ core/src/utilities/helpers/generic-helpers.js
@@ -203,14 +203,16 @@ export const replaceVars = (
   return processedString;
 };
 
-export const doOnStoreChange = (store, fn, events) => {
-  const evs = events || ['state'];
+export const doOnStoreChange = (store, fn, events = []) => {
+  const evs = events;
   if (evs.indexOf('state') === -1) {
     evs.push('state');
   }
+  // register listener for store event(s)
   evs.forEach(e => {
     store.on(e, fn);
   });
+  // and call the listener once specifically, immediately:
   fn({
     current: store.get(),
     changed: { config: true },
