edf510a9afc02591830f250074ef803d1324bca7
Markus Edenhauser <1720843+maxmarkus@users.noreply.github.com>
2019-05-29 15:47:07 +0200

    fix path issue

--- scripts/npmPublish.sh
+++ scripts/npmPublish.sh
@@ -35,38 +35,53 @@ function setNpmToken {
 function checkGithubRelease {
   cd $BASE_DIR/../client
   NAME=$(node -p "require('./package.json').name")
-  VERSION=$(node -p "require('./package.json').version")
+  CLIENT_VERSION=$(node -p "require('./package.json').version")
+  cd $BASE_DIR/../core/public
+  CORE_VERSION=$(node -p "require('./package.json').version")
+
+  if [ "CORE_VERSION" != "CLIENT_VERSION" ]; then
+    echoe "Version mismatch between Client and Core."
+    exit 1
+  fi
 
   # Check if it can be published (github release must exist)
-  TAGS_GREP=`git ls-remote --tags origin | grep "v$VERSION$" | wc -l`
+  TAGS_GREP=`git ls-remote --tags origin | grep "v$CORE_VERSION$" | wc -l`
   if [[ "$TAGS_GREP" =~ "0" ]]; then
-    echo "Tag (github release) does not exist, not going to publish $VERSION to npm"
-    exit 0;
+    echo "Tag (github release) does not exist, not going to publish $CORE_VERSION to npm"
+    exit 0
   fi
 }
 
 function publishPackage {
-  FOLDER=$1
+  BASE_FOLDER=$1
+  PUBLISH_FOLDER=$2
 
-  cd $BASE_DIR/../$FOLDER
+  cd $BASE_DIR/../$PUBLISH_FOLDER
+  NAME=$(node -p "require('./package.json').name")
+  VERSION=$(node -p "require('./package.json').version")
   # Check if was published already
   NPM_GREP=`npm info $NAME versions | grep "'$VERSION'" | wc -l`
   if [[ "$NPM_GREP" =~ "1" ]]; then
     echo "$NAME@$VERSION already published, skipping until next release."
   else
 
-    echoe "Installing ..."
+    cd $BASE_DIR/../$BASE_FOLDER
+    echoe "Installing $NAME@$VERSION ..."
     npm ci
-    echoe "Bundling ..."
+    echoe "Bundling $NAME@$VERSION ..."
     npm run bundle
 
-    echoe "Publishing ..."
+    echoe "Publishing $NAME@$VERSION ..."
+    cd $BASE_DIR/../$PUBLISH_FOLDER
     npm publish --access public
     if [[ $VERSION != *"rc."* ]]; then
       echo "Tag $NAME@$VERSION with latest on npm"
       npm dist-tag add $NAME@$VERSION latest
+    else 
+      echo "Release candidate $NAME@$VERSION NOT tagged as latest"
     fi
 
+    echoe "Published $NAME@$VERSION"
   fi # end NPM_GREP
 }
 
@@ -80,6 +95,6 @@ function removeNpmToken {
 
 checkGithubRelease
 setNpmToken
-publishPackage "core/public"
-publishPackage "client"
+publishPackage "core" "core/public"
+publishPackage "client" "client"
 removeNpmToken
