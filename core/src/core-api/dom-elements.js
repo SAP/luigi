95f737ec9a833090c418d162f56d5b1f5d3bb7a6
marynaKhromova <46446373+marynaKhromova@users.noreply.github.com>
2019-06-03 09:55:00 +0200

    Update core/src/core-api/dom-elements.js
    Co-Authored-By: Barbara Szwarc <barbara.m.szwarc@gmail.com>

--- core/src/core-api/dom-elements.js
+++ core/src/core-api/dom-elements.js
@@ -34,7 +34,7 @@ class LuigiElementsManager {
   }
 
   /**
-   * Returns the active microfrontend iframe.
+   * Returns the active micro frontend iframe.
    * If there is a modal, which includes the microfrontend iframe, the function will return this iframe.
    * @returns {Object} the active microfrontend iframe DOM element.
    * @memberof LuigiElements
