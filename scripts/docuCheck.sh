9415763247d64a1b60498f7c7b02ecd5a791756a
Markus Edenhauser <1720843+maxmarkus@users.noreply.github.com>
2019-06-18 11:12:02 +0200

    changes based on pr comments

--- scripts/docuCheck.sh
+++ scripts/docuCheck.sh
@@ -12,23 +12,30 @@ declare -a LUIGI_FOLDERS=(
 source $BASE_DIR/shared/bashHelpers.sh
 
 installPrerequisites() {
-  echoe "Install documentation prerequisites"
-  mkdir -p $BASE_DIR/tmp-docu
-
-  cd $BASE_DIR/tmp-docu
-  DOCU_VERSION=`cat $BASE_DIR/../client/package.json | jq --raw-output ".devDependencies.documentation"`
-  # create local package json to make it cachable
-  echo "{\"dependencies\": {\"documentation\": \"$DOCU_VERSION\"}}" > package.json
-  npm i
-
   # link documentation binary if it does not exist
-  NODE_BIN_FOLDER="node_modules/.bin/"
+  local NODE_BIN_FOLDER="node_modules/.bin/"
+    # link documentation binary if it does not exist
   for FOLDER in "${LUIGI_FOLDERS[@]}"
   do
-    if [ ! -e $BASE_DIR/../$FOLDER$NODE_BIN_FOLDER/documentation ]; then
+    echoe "Install documentation prerequisites for $FOLDER"
+    local DOCU_TMP_FOLDER=$BASE_DIR/tmp-docu/$FOLDER
+    mkdir -p $DOCU_TMP_FOLDER
+
+    cd $DOCU_TMP_FOLDER
+    local NODE_BIN_FOLDER=$BASE_DIR/../$FOLDER/node_modules/.bin
+    if ! [ -L $NODE_BIN_FOLDER/documentation ]; then
+      local DOCU_VERSION=`cat $BASE_DIR/../$FOLDER/package.json | jq --raw-output ".devDependencies.documentation"`
+      # create local package json to make it cachable
+      echo "{\"dependencies\": {\"documentation\": \"$DOCU_VERSION\"}}" > package.json
+      echo "Installing documentation dependency for $FOLDER"
+      npm i
+    
       echo "Linking documentation binary for $FOLDER"
-      mkdir -p $BASE_DIR/../$FOLDER$NODE_BIN_FOLDER
-      ln -s $BASE_DIR/tmp-docu/$NODE_BIN_FOLDER/documentation $BASE_DIR/../$FOLDER$NODE_BIN_FOLDER/documentation
+      echo "$NODE_BIN_FOLDER/documentation"
+      mkdir -p $NODE_BIN_FOLDER
+      ln -s $DOCU_TMP_FOLDER/node_modules/.bin/documentation $NODE_BIN_FOLDER/documentation
+    else
+      echo "Documentation binary for $FOLDER already linked"
     fi
   done
 }
@@ -37,7 +44,7 @@ installPrerequisites() {
 checkDocu() {
   cd $BASE_DIR/../$1
 
-  DOCU_STEP=`cat package.json | jq --raw-output ".scripts.docu"`
+  local DOCU_STEP=`cat package.json | jq --raw-output ".scripts.docu"`
   if [[ $DOCU_STEP == *"npm"* ]]; then
     echoe "Validating and generating documentation for $1"
     eval $DOCU_STEP
@@ -50,7 +57,7 @@ checkDocu() {
 validateMdChanges() {
   echoe "Validate .md changes"
   # verify that there are no changes in md files
-  MD_FILE_CHANGES=`git status | grep '.md' | wc -l`
+  local MD_FILE_CHANGES=`git status | grep '.md' | wc -l`
   if [[ $MD_FILE_CHANGES != *"0"* ]]; then
     echoe "Unsaved documentation changes found"
     echo "The following files need to be commited and pushed again:"
--- scripts/npmPublish.sh
+++ scripts/npmPublish.sh
@@ -6,22 +6,7 @@ set -e # exit on errors
 
 BASE_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
-echoe() {
-  # find all colors here: https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux#5947802
-  # Reset
-  Color_Off='\033[0m'       # Text Reset
-
-  # Bold High Intensity
-  BIBlack='\033[1;90m'      # Black
-  
-  # High Intensity backgrounds
-  On_IYellow='\033[0;103m'  # Yellow
-
-  echo ""
-  echo ""
-  echo -e "${On_IYellow}${BIBlack}${1}${Color_Off}"
-  echo ""
-}
+source $BASE_DIR/shared/bashHelpers.sh
 
 function setNpmToken {
   if [ "$TRAVIS" = "true" ]; then
