4ed788de4d3c68061dc472abbb0a89edaf6d54f8
Jesús Real Serrano <8238188+jesusreal@users.noreply.github.com>
2019-07-02 11:57:14 +0200

    Add IE11 support (#601)

---
+++ core/webpack-common-rules.js
@@ -0,0 +1,57 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const sass = require('node-sass');
+
+module.exports = {
+  svelte: {
+    test: /\.html$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'svelte-loader',
+      options: {
+        emitCss: true,
+        name: 'Luigi',
+        preprocess: {
+          style: ({ content, attributes }) => {
+            if (attributes.type !== 'text/scss') return;
+            return new Promise((fulfil, reject) => {
+              sass.render(
+                {
+                  data: content,
+                  includePaths: ['src'],
+                  sourceMap: true,
+                  outFile: 'x' // this is necessary, but is ignored
+                },
+                (err, result) => {
+                  if (err) return reject(err);
+
+                  fulfil({
+                    code: result.css.toString(),
+                    map: result.map.toString()
+                  });
+                }
+              );
+            });
+          }
+        }
+      }
+    }
+  },
+  css: {
+    test: /\.(sa|sc|c)ss$/,
+    use: [
+      {
+        loader: MiniCssExtractPlugin.loader
+      },
+      {
+        loader: 'css-loader'
+      }
+    ]
+  },
+  urls: {
+    test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+    loader: 'url-loader',
+    options: {
+      limit: 50000
+    }
+  }
+};
