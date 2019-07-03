bc4e15d58d8cc4e0f4cb24337bca7b3fd28dd40d
Johannes Doberer <johannes.doberer@sap.com>
2019-07-03 15:06:25 +0200

    add e2e test

--- core/examples/luigi-sample-angular/e2e/tests/navigation.spec.js
+++ core/examples/luigi-sample-angular/e2e/tests/navigation.spec.js
@@ -199,5 +199,43 @@ describe('Navigation', () => {
       cy.get('.no-side-nav').should('exist');
       cy.get('.fd-app__sidebar').should('not.be.visible');
     });
+
+    it('Open navigation node in a modal', () => {
+      let iframeBody;
+      cy.get('iframe', { timeout: 1000 }).then(ifr => {
+        iframeBody = ifr.contents().find('body');
+      });
+      // projects page
+      cy.get('.fd-shellbar')
+        .contains('Projects')
+        .click();
+
+      //projects page
+      cy.get('.fd-app__sidebar').should('contain', 'Project One');
+      cy.get('.fd-app__sidebar').should('contain', 'Project Two');
+      cy.get('.fd-app__sidebar')
+        .contains('Project Two')
+        .click();
+
+      //project two page
+      cy.expectPathToBe('/projects/pr2');
+
+      cy.get('.fd-app__sidebar').should('not.contain', 'Project Two');
+      cy.get('.fd-app__sidebar')
+        .contains('Miscellaneous2')
+        .click();
+
+      cy.wrap(iframeBody)
+        .get('[data-e2e=modal-mf]')
+        .should('be.visible');
+
+      cy.wrap(iframeBody)
+        .get('[data-e2e=modal-mf] [aria-label=close]')
+        .click();
+
+      cy.wrap(iframeBody)
+        .get('[data-e2e=modal-mf]')
+        .should('not.be.visible');
+    });
   });
 });
