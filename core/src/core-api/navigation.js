a2a171f6ef212a5414f7beb918cd9ed0118e93fe
Markus <1720843+maxmarkus@users.noreply.github.com>
2019-07-16 11:10:38 +0200

    Apply suggestions from code review
    Co-Authored-By: Barbara Szwarc <barbara.m.szwarc@gmail.com>

--- core/src/core-api/navigation.js
+++ core/src/core-api/navigation.js
@@ -1,13 +1,13 @@
 class LuigiNavigationManager {
   /**
-   * Use these functions for navigation related features
+   * Use these functions for navigation-related features.
    * @name LuigiNavigation
    */
   constructor() {}
 
   /**
-   * Refreshes top navigation badge counters
-   * Basically triggeres a re-render of the navigation.
+   * Refreshes top navigation badge counters by rendering the navigation again.
+  
    * @memberof LuigiNavigation
    * @example
    * Luigi.navigation().updateTopNavigation();
