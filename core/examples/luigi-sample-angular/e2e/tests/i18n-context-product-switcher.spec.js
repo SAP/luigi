840081def5bf4cf303a943996c088048ef143ecf
Markus Edenhauser <1720843+maxmarkus@users.noreply.github.com>
2019-08-16 12:28:41 +0200

    grouped small e2e tets, split up ux manager tests. every test file is currently around 30 secs

---
+++ core/examples/luigi-sample-angular/e2e/tests/i18n-context-product-switcher.spec.js
@@ -0,0 +1,259 @@
+describe('Context switcher', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    cy.login('tets@email.com', 'tets');
+  });
+
+  it('Clicking around the context switcher', () => {
+    cy.selectContextSwitcherItem('New Environment (top)');
+
+    cy.expectPathToBe('/create-environment');
+
+    cy.selectContextSwitcherItem('Environment 1');
+
+    cy.expectPathToBe('/environments/env1');
+
+    // check label
+    cy.get('.fd-product-menu .fd-popover__control button').should(
+      'contain',
+      'Environment 1'
+    );
+  });
+
+  it('Add and remove project with context switcher', () => {
+    cy.goToProjectsPage();
+
+    cy.get('.fd-app__sidebar').should('contain', 'Project One');
+    cy.get('.fd-app__sidebar').should('contain', 'Project Two');
+    cy.get('.fd-app__sidebar').should('not.contain', 'Project 3');
+
+    cy.goToOverviewPage();
+    cy.expectPathToBe('/overview');
+
+    cy.get('[data-cy=luigi-alert]').should('not.exist');
+
+    // add project
+
+    cy.selectContextSwitcherItem('New Project');
+
+    cy.expectPathToBe('/projects');
+
+    cy.get('.fd-app__sidebar').should('contain', 'Project One');
+    cy.get('.fd-app__sidebar').should('contain', 'Project Two');
+    cy.get('.fd-app__sidebar').should('contain', 'Project 3');
+
+    cy.get('[data-cy=luigi-alert]').should(
+      'have.class',
+      'fd-alert--information'
+    );
+
+    cy.get('[data-cy=luigi-alert]').should('contain', 'Project 3 created.');
+
+    cy.goToOverviewPage();
+    cy.expectPathToBe('/overview');
+
+    // remove project
+
+    cy.selectContextSwitcherItem('Remove Project');
+
+    cy.expectPathToBe('/projects');
+
+    cy.get('.fd-app__sidebar').should('contain', 'Project One');
+    cy.get('.fd-app__sidebar').should('contain', 'Project Two');
+    cy.get('.fd-app__sidebar').should('not.contain', 'Project 3');
+
+    // remove all projects
+
+    cy.selectContextSwitcherItem('Remove Project');
+
+    cy.expectPathToBe('/projects');
+
+    cy.selectContextSwitcherItem('Remove Project');
+
+    cy.expectPathToBe('/projects');
+
+    cy.get('.fd-app__sidebar').should('not.contain', 'Project One');
+    cy.get('.fd-app__sidebar').should('not.contain', 'Project Two');
+    cy.get('.fd-app__sidebar').should('not.contain', 'Project 3');
+
+    cy.get('.fd-product-menu .fd-popover__body').should(
+      'not.contain',
+      'Remove Project'
+    );
+
+    // add projects again
+
+    cy.selectContextSwitcherItem('New Project');
+
+    cy.expectPathToBe('/projects');
+
+    cy.get('.fd-app__sidebar').should('contain', 'Project 1');
+    cy.get('.fd-app__sidebar').should('not.contain', 'Project 2');
+    cy.get('.fd-app__sidebar').should('not.contain', 'Project 3');
+
+    cy.get('.fd-product-menu .fd-popover__body').should(
+      'contain',
+      'Remove Project'
+    );
+
+    cy.selectContextSwitcherItem('New Project');
+
+    cy.expectPathToBe('/projects');
+
+    cy.get('.fd-app__sidebar').should('contain', 'Project 1');
+    cy.get('.fd-app__sidebar').should('contain', 'Project 2');
+    cy.get('.fd-app__sidebar').should('not.contain', 'Project 3');
+
+    cy.get('.fd-product-menu .fd-popover__body').should(
+      'contain',
+      'Remove Project'
+    );
+
+    cy.expectPathToBe('/projects');
+  });
+});
+
+describe('ProductSwitcher', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    cy.login('tets@email.com', 'tets');
+  });
+  context('Desktop', () => {
+    beforeEach(() => {
+      // run these tests as if in a desktop
+      cy.viewport('macbook-15');
+    });
+
+    it('Clicking around the product switcher', () => {
+      //check if hybris is there
+      cy.get('.fd-product-switcher')
+        .click()
+        .contains('hybris');
+
+      //check if internal link is there
+      cy.get('.fd-product-switcher__product-title')
+        .contains('Project 1')
+        .click();
+
+      cy.expectPathToBe('/projects/pr1');
+
+      cy.get('.fd-product-switcher__product-title').should(
+        'not.contain',
+        'Project 3'
+      );
+
+      cy.goToOverviewPage();
+      cy.expectPathToBe('/overview');
+
+      // add project
+      cy.selectContextSwitcherItem('New Project');
+      cy.expectPathToBe('/projects');
+
+      cy.get('.fd-product-switcher__product-title').should(
+        'contain',
+        'Project 3'
+      );
+
+      cy.get('.fd-product-switcher').click();
+
+      cy.get('.fd-product-switcher__product-title')
+        .contains('Project 3')
+        .click();
+
+      cy.expectPathToBe('/projects/pr3');
+    });
+
+    it('Mobile Product Switcher is not visible', () => {
+      cy.get('[data-cy="mobile-product-switcher"]').should('not.be.visible');
+    });
+  });
+
+  context('Mobile', () => {
+    beforeEach(() => {
+      cy.viewport('iphone-6');
+    });
+
+    it('Desktop Product Switcher is not visible', () => {
+      cy.get('[data-cy="desktop-product-switcher"]').should('not.be.visible');
+    });
+
+    it('Should be able to select product', () => {
+      cy.get('[data-cy="mobile-menu"]').click();
+
+      //open mobile product switcher
+      cy.get('[data-cy="mobile-product-switcher"]').click();
+
+      //check if internal link is there
+      cy.get('.y-full-width-list__title')
+        .contains('Project 1')
+        .click();
+
+      cy.expectPathToBe('/projects/pr1');
+    });
+
+    it('Should be able to close', () => {
+      cy.get('[data-cy="mobile-menu"]').click();
+
+      //open mobile product switcher
+      cy.get('[data-cy="mobile-product-switcher"]').click();
+
+      //close mobile product switcher
+      cy.get('[data-cy="mobile-product-switcher-close"]').click();
+
+      //no product switcher is visible
+      cy.get('.fd-product-switcher').should('not.be.visible');
+
+      //the path wasn't changed
+      cy.expectPathToBe('/overview');
+    });
+  });
+});
+
+describe('I18n', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    cy.login('tets@email.com', 'tets');
+  });
+
+  it('custom translation test', () => {
+    cy.get('.fd-shellbar')
+      .contains('Projects')
+      .click();
+
+    //projects page
+    cy.get('.fd-app__sidebar').should('contain', 'Project One');
+    cy.get('.fd-app__sidebar').should('contain', 'Project Two');
+    cy.get('.fd-app__sidebar')
+      .contains('Project One')
+      .click();
+
+    //project one page
+    cy.expectPathToBe('/projects/pr1');
+
+    cy.window().then(win => {
+      let config = win.Luigi.getConfig();
+      const mySettings = {
+        customTranslationImplementation: () => {
+          return {
+            getTranslation: (key, interpolations, locale) => {
+              return (
+                '*' +
+                key +
+                '* ' +
+                (locale || win.Luigi.i18n().getCurrentLocale())
+              );
+            }
+          };
+        }
+      };
+      config.settings = mySettings;
+      win.Luigi.setConfig(config);
+    });
+    cy.get('.fd-app__sidebar').should('contain', '*Developers* en');
+
+    cy.get('.fd-shellbar')
+      .contains('Projects')
+      .click();
+    cy.get('.fd-app__sidebar').should('contain', '*Project One* en');
+  });
+});
--- core/examples/luigi-sample-angular/e2e/tests/microfrontends.spec.js
+++ core/examples/luigi-sample-angular/e2e/tests/microfrontends.spec.js
@@ -1,4 +1,87 @@
-describe('SplitView', () => {
+describe('Modal Microfrontend', () => {
+  let $iframeBody;
+  beforeEach(() => {
+    cy.visit('/');
+    cy.login('tets', 'tets');
+
+    cy.getIframeBody().then(result => {
+      $iframeBody = result;
+    });
+  });
+
+  describe('Behaviour when used in LinkManager', () => {
+    beforeEach(() => {
+      cy.goToLinkManagerMethods($iframeBody);
+    });
+
+    it(`doesn't change URL when opened`, () => {
+      cy.expectPathToBe('/projects/pr2');
+
+      cy.wrap($iframeBody)
+        .contains('rendered in a modal')
+        .click();
+
+      cy.expectPathToBe('/projects/pr2');
+    });
+
+    it(`can be closed by Close Button`, () => {
+      cy.wrap($iframeBody)
+        .contains('rendered in a modal')
+        .click();
+
+      cy.wrap($iframeBody)
+        .get('[data-e2e=modal-mf]')
+        .should('be.visible');
+
+      cy.wrap($iframeBody)
+        .get('[data-e2e=modal-mf] [aria-label=close]')
+        .click();
+
+      cy.wrap($iframeBody)
+        .get('[data-e2e=modal-mf]')
+        .should('not.be.visible');
+    });
+
+    it(`sets proper URL inside iframe`, () => {
+      cy.wrap($iframeBody)
+        .contains('rendered in a modal')
+        .click();
+
+      cy.get('[data-e2e=modal-mf] iframe').then(ifr => {
+        expect(ifr.attr('src')).to.equal('/sampleapp.html#/settings');
+      });
+    });
+
+    it(`has the size set directly`, () => {
+      cy.wrap($iframeBody)
+        .contains('rendered in a modal')
+        .click();
+
+      cy.get('[data-e2e=modal-mf]').then(modal => {
+        expect(modal.attr('style')).to.contain('width:');
+        expect(modal.attr('style')).to.contain('height:');
+      });
+    });
+
+    it(`go back closes the modal`, () => {
+      cy.wrap($iframeBody)
+        .contains('rendered in a modal')
+        .click();
+
+      cy.get('[data-e2e=modal-mf] iframe')
+        .iframe()
+        .then(modal => {
+          cy.wrap(modal)
+            .contains('Go back')
+            .click();
+        });
+
+      cy.expectPathToBe('/projects/pr2');
+    });
+  });
+});
+
+describe('SplitView Microfrontend', () => {
   beforeEach(() => {
     cy.visit('/');
     cy.login('tets@email.com', 'tets', true);
