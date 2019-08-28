2e813b69d966ca0483e36fe406f2c2a31fa0d67a
Markus Edenhauser <1720843+maxmarkus@users.noreply.github.com>
2019-08-28 15:19:26 +0200

    check webserver availablility every second, update cypress key

--- test/e2e.sh
+++ test/e2e.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e # exit on errors
 BASE_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+
 # We assume, Angular example is ran with `npm run build`
 # and root dependencies are installed
 # npm i -g cypress@^3.4.1 cypress-plugin-retries sirv-cli
@@ -19,20 +20,21 @@ echo "Starting webserver"
 sirv start dist --single --cors --port 4200 &
 WS_PID=$!
 
-# # wait until example is built and running
-# sleep 60
+# wait until example is built and running
+SLEEPSECS=1 # sleep time between webserver availability check
 WAITCOUNT=0
 until $(curl --output /dev/null --silent --head --fail http://localhost:4200); do
   printf '.'
-  sleep 5
-  WAITCOUNT=$(($WAITCOUNT + 5))
+  sleep $SLEEPSECS
+  WAITCOUNT=$(($WAITCOUNT + $SLEEPSECS))
 done
 echo "Webserver was ready after $WAITCOUNT seconds."
 
 
 if [ "$USE_CYPRESS_DASHBOARD" == "true" ]; then
   echo "Running tests in parallel with recording"
-  npm run e2e:run -- --record --parallel --key 8c693a90-b747-48af-b3dc-2bab84927657
+  # obtain the key here: https://dashboard.cypress.io/#/projects/czq7qc/settings
+  npm run e2e:run -- --record --parallel --key 4bf20f87-8352-47d5-aefa-1e684fab69cf
 else
   echo "Running tests without parallelization"
   npm run e2e:run
