958f32171d1415bd304f2cfa0bd74b1efc9095fe
Maryna Khromova <46446373+marynaKhromova@users.noreply.github.com>
2019-08-29 16:46:45 +0200

    check if env is valid

--- scripts/whiteSource.sh
+++ scripts/whiteSource.sh
@@ -5,6 +5,7 @@ set -e # exit on errors
 BASE_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
 cd $BASE_DIR/..
+echo "product token: $WHITESOURCE_PRODUCT_TOKEN"
 
 echo $(printf '{"apiKey": "%s","userKey": "%s","productName": "%s","devDep": "false","forceUpdate": true,"checkPolicies": true}' "$WHITESOURCE_APIKEY" "$WHITESOURCE_USER_TOKEN" "$WHITESOURCE_PRODUCT_TOKEN" ) > $BASE_DIR/../whitesource.config.json
 whitesource run
