92dd1c6eae32c5f51d73335cbe7a7e0b6fcfdffa
Maryna Khromova <46446373+marynaKhromova@users.noreply.github.com>
2019-08-29 17:32:27 +0200

    exit based on exit code

--- scripts/whiteSource.sh
+++ scripts/whiteSource.sh
@@ -7,4 +7,36 @@ BASE_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 cd $BASE_DIR/..
 
 echo $(printf '{"apiKey": "%s","userKey": "%s","productName": "%s","devDep": "false","forceUpdate": true,"checkPolicies": true}' "$WHITESOURCE_APIKEY" "$WHITESOURCE_USER_TOKEN" "$WHITESOURCE_PRODUCT_TOKEN" ) > $BASE_DIR/../whitesource.config.json
+
+# WHITE SOURCE
+# Exit Code
+# Starting version 1.1.1 and later, the following exit codes are displayed upon scan completion:
+
+# SUCCESS: 0,
+# ERROR: -1,
+# POLICY_VIOLATION: -2,
+# CLIENT_FAILURE: -3,
+# CONNECTION_FAILURE: -4,
+# SERVER_FAILURE: -5
+
+# Exit Codes in Bash
+# The exit codes WhiteSource returns in the Bash command language should be treated as 'x' modulo 256: 
+
+# Exit code 0 is equivalent to code 0 (0 mod 256 = 0)
+# Exit code -1 is equivalent to code 255 (-1 mod 256 = 255)
+# Exit code -2 is equivalent to code 254 (-2 mod 256 = 254)
+# Exit code -3 is equivalent to code 253 (-3 mod 256 = 253)
+# Exit code -4 is equivalent to code 252 (-4 mod 256 = 252)
+# Exit code -5 is equivalent to code 251 (-5 mod 256 = 251)
+
 whitesource run
+
+RV=$?
+echo "Exit code: $RV"
+
+if [ $RV == 254 ]; then 
+  echo "Policy Violation";
+  exit $RV
+fi
+
+exit 0
