b0f904a3a424bdac857f16aa5c324ef12fba706d
Maryna Khromova <46446373+marynaKhromova@users.noreply.github.com>
2019-08-29 16:10:19 +0200

    change whitesource config location

--- scripts/whiteSource.sh
+++ scripts/whiteSource.sh
@@ -1,4 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-echo $(printf '{"apiKey": "%s","userKey": "%s","productName": "%s","devDep": "false","forceUpdate": true,"checkPolicies": true}' "$WHITESOURCE_APIKEY" "$WHITESOURCE_USER_TOKEN" "$WHITESOURCE_PRODUCT_TOKEN" ) > whitesource.config.json
+set -e # exit on errors
+
+BASE_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+
+cd $BASE_DIR/..
+
+echo $(printf '{"apiKey": "%s","userKey": "%s","productName": "%s","devDep": "false","forceUpdate": true,"checkPolicies": true}' "$WHITESOURCE_APIKEY" "$WHITESOURCE_USER_TOKEN" "$WHITESOURCE_PRODUCT_TOKEN" ) > $BASE_DIR/../whitesource.config.json
 whitesource run
