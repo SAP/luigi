8ef39d5947e8ccc4908695867ffba241f63a3dba
Jesús Real Serrano <8238188+jesusreal@users.noreply.github.com>
2019-08-30 12:34:04 +0200

    Leftovers from custom messages feature (#786)

--- core/src/core-api/custom-messages.js
+++ core/src/core-api/custom-messages.js
@@ -23,7 +23,7 @@ class CustomMessagesManager {
     const internalMessage = MessagesListeners.convertCustomMessageUserToInternal(
       message
     );
-    IframeHelpers.getMicrofrontends()
+    IframeHelpers.getMicrofrontendsInDom()
       .map(microfrontendObj => microfrontendObj.container)
       .map(mfContainer =>
         IframeHelpers.sendMessageToIframe(mfContainer, internalMessage)
@@ -33,7 +33,7 @@ class CustomMessagesManager {
   /**
    * Sends a message to specific micro frontend identified with an id.
    * Use Luigi.elements().getMicrofrontends() to get the iframe id.
-   * @param {microfrontendId} number the id of the micro frontend
+   * @param {number} microfrontendId the id of the micro frontend
    * @param {Object} message an object containing data to be sent to the micro frontend to process it further. This object is set as an input parameter of the custom message listener on the micro frontend side.
    * @param {string} message.id the id of the message
    * @param {*} message.MY_DATA_FIELD any other message data field
