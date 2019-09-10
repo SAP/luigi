edeb20aae0db6af784661cf1df38cc4cb15b097c
Maryna Khromova <46446373+marynaKhromova@users.noreply.github.com>
2019-09-10 18:09:07 +0200

    proper comments

--- scripts/whiteSource.sh
+++ scripts/whiteSource.sh
@@ -4,9 +4,13 @@ set -e # exit on errors
 
 BASE_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
-declare -a FOLDERS=("client" "core" "website/landingpage/dev")
+# Folders in which whitesource has to be run
+declare -a FOLDERS=("client"
+                    "core"
+                    "website/landingpage/dev"
+                   )
 
-# Project tokens for client, core and landingpage
+# Project tokens for corresponding folders
 declare -a PROJECT_TOKENS=("a63fd5aaaa2343199327aac6d3e2b5346e930927d66441bf92111f111ecfc8ad"
                            "dd9b33e88b684129b325ef1c3510f52712f26167f06c432ab84fd4452cd353d9"
                            "0bc00859083a4cf98e1246c91cabc56a4623fb69f1f24cdd8ca10f9f0d3dfbb4"
