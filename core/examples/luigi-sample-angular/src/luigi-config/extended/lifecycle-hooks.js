06debc2e52fd1b4c1b8ca94c5d5bfaa164df4c96
Markus Edenhauser <1720843+maxmarkus@users.noreply.github.com>
2019-09-11 16:57:21 +0200

    refactored magic number

--- core/examples/luigi-sample-angular/src/luigi-config/extended/lifecycle-hooks.js
+++ core/examples/luigi-sample-angular/src/luigi-config/extended/lifecycle-hooks.js
@@ -2,9 +2,10 @@ class LifecycleHooks {
   luigiAfterInit() {
     // fallback in case some micro frontend did not send 'my-microfrontend-is-ready' custom message
     // (settings.appLoadingIndicator.hideAutomatically is set to false)
+    const fallbackHideTimeout = 5000;
     setTimeout(() => {
       Luigi.ux().hideAppLoadingIndicator();
-    }, 5000);
+    }, fallbackHideTimeout);
   }
 }
 
