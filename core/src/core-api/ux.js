30f9d48b7c2b72e48282e7887df55e43925d1044
Zarko Simic <42171300+zarkosimic@users.noreply.github.com>
2019-09-13 11:26:48 +0200

    Move showAlert to ux section as part of Luigi Core API

--- core/src/core-api/ux.js
+++ core/src/core-api/ux.js
@@ -21,6 +21,44 @@ class LuigiUX {
       }, 500);
     }
   }
+
+  /**
+   * Shows an alert.
+   * @memberof UX
+   * @param {Object} settings the settings for the alert
+   * @param {string} settings.text the content of the alert. To add a link to the content, you have to set up the link in the `links` object. The key(s) in the `links` object must be used in the text to reference the links, wrapped in curly brackets with no spaces. If you don't specify any text, the alert is not displayed
+   * @param {('info'|'success'|'warning'|'error')} settings.type sets the type of alert
+   * @param {Object} settings.links provides links data
+   * @param {Object} settings.links.LINK_KEY object containing the data for a particular link. To properly render the link in the alert message refer to the description of the **settings.text** parameter
+   * @param {string} settings.links.LINK_KEY.text text which replaces the link identifier in the alert content
+   * @param {string} settings.links.LINK_KEY.url url to navigate when you click the link. Currently, only internal links are supported in the form of relative or absolute paths.
+   * @param {number} settings.closeAfter (optional) time in milliseconds that tells Luigi when to close the Alert automatically. If not provided, the Alert will stay on until closed manually. It has to be greater than `100`.
+   * @returns {promise} which is resolved when the alert is dismissed.
+   * @since 0.6.4
+   * @example
+   * const settings = {
+   *  text: Ut enim ad minim veniam, {goToHome} quis nostrud exercitation ullamco {relativePath} laboris nisi ut aliquip ex ea commodo consequat.
+   *    Duis aute irure dolor {goToOtherProject},
+   *  type: 'info',
+   *  links: {
+   *    goToHome: { text: 'homepage', url: '/overview' },
+   *    goToOtherProject: { text: 'other project', url: '/projects/pr2' },
+   *    relativePath: { text: 'relative hide side nav', url: 'hideSideNav' }
+   *  },
+   * closeAfter: 3000
+   * }
+   * Luigi
+   *  .ux()
+   *  .showAlert(settings)
+   *  .then(() => {
+   *     // Logic to execute when the alert is dismissed
+   * });
+
+    */
+
+  showAlert(settings) {
+    return Luigi.showAlert(settings);
+  }
 }
 
 export const ux = new LuigiUX();
