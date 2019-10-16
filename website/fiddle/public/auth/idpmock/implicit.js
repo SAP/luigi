adeb5ddef578ebe64ed2726d7e5faa754f3a7cea
Philipp Pracht <philipp.pracht@sap.com>
2019-10-16 11:52:08 +0200

    Update implicit.js

--- website/fiddle/public/auth/idpmock/implicit.js
+++ website/fiddle/public/auth/idpmock/implicit.js
@@ -13,6 +13,6 @@ window.onload = function () {
 
     loginButton.addEventListener('click', function (ev) {
         ev.preventDefault();
-        window.location.replace(goTo);
+        window.location.replace(goTo.replace(/\s\s+/g, ''));
     });
 };
