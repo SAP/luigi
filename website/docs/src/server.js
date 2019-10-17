9f3bcf50e3afb179e95b8f04d7cdd4bb44880233
Zarko Simic <42171300+zarkosimic@users.noreply.github.com>
2019-10-17 17:59:37 +0200

    Path changes fix

--- website/docs/src/server.js
+++ website/docs/src/server.js
@@ -8,6 +8,7 @@ const dev = NODE_ENV === 'development';
 
 polka() // You can also use Express
 	.use(
+		'/docu-microfrontend',
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		sapper.middleware()
