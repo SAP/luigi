91221a8701cc7781fac6f161409c023b027b871e
Johannes Doberer <johannes.doberer@sap.com>
2019-10-29 12:11:07 +0100

    tab nav tets

--- core/examples/luigi-sample-angular/e2e/tests/navigation.spec.js
+++ core/examples/luigi-sample-angular/e2e/tests/navigation.spec.js
@@ -309,41 +309,126 @@ describe('Navigation', () => {
     });
   });
   describe('Horizontal Navigation', () => {
-    it('Open horizontal navigation', () => {
-      cy.get('.fd-shellbar')
-        .contains('Projects')
-        .click();
+    context('Desktop', () => {
+      it('Open horizontal navigation', () => {
+        cy.get('.fd-shellbar')
+          .contains('Projects')
+          .click();
 
-      cy.get('.fd-app__sidebar')
-        .contains('Horizontal Navigation Example')
-        .click();
+        cy.get('.fd-app__sidebar')
+          .contains('Horizontal Navigation Example')
+          .click();
 
-      cy.expectPathToBe('/projects/tabNav');
+        cy.expectPathToBe('/projects/tabNav');
 
-      cy.get('.fd-tabs')
-        .contains('Node with node activation hook')
-        .click();
-      cy.expectPathToBe('/projects/tabNav/on-node-activation');
+        cy.get('.fd-tabs')
+          .contains('Node with node activation hook')
+          .click();
+        cy.expectPathToBe('/projects/tabNav/on-node-activation');
 
-      cy.get('.fd-tabs')
-        .contains('Settings')
-        .click();
-      cy.get('.fd-menu__item')
-        .contains('Project Settings')
-        .click();
+        cy.get('.fd-tabs')
+          .contains('Settings')
+          .click();
+        cy.get('.fd-menu__item')
+          .contains('Project Settings')
+          .click();
 
-      cy.get('.fd-tabs')
-        .contains('More')
-        .click();
+        cy.get('.fd-tabs')
+          .contains('More')
+          .click();
 
-      cy.get('.fd-menu')
-        .contains('Default Child node Example')
-        .click();
+        cy.get('.fd-menu')
+          .contains('Default Child node Example')
+          .click();
 
-      cy.get('.fd-side-nav__subitem')
-        .contains('First Child')
-        .click();
-      cy.expectPathToBe('/projects/tabNav/dps1');
+        cy.get('.fd-side-nav__subitem')
+          .contains('First Child')
+          .click();
+        cy.expectPathToBe('/projects/tabNav/dps1');
+      });
+    });
+
+    context('Mobile', () => {
+      beforeEach(() => {
+        cy.viewport('iphone-6');
+      });
+      it('Horizontal Navigation on mobile', () => {
+        cy.get('[data-testid="mobile-menu"]').click();
+        cy.get('.fd-popover__body').within(() => {
+          cy.get('[data-testid="projects_projects"]').click();
+        });
+        cy.get('.lui-burger').click();
+        cy.get('.fd-side-nav').contains('Horizontal Navigation Example');
+        cy.get('[data-testid="tabnav_horizontalnavigationexample"]').click();
+        cy.get('[data-testid="tabnav_horizontalnavigationexample"]').should(
+          'have.class',
+          'is-selected'
+        );
+        cy.get('.fd-tabs').contains('User Management');
+        cy.get('.fd-tabs__item')
+          .contains('Node with node activation hook')
+          .should('not.visible');
+        cy.get('.fd-tabs').contains('More');
+      });
+
+      it('Test activated node on moible with keep selected context', () => {
+        cy.visit('/projects/tabNav/avengers/captain-america/super-power');
+        cy.get('.luigi__more').should('have.attr', 'aria-selected', 'true');
+        cy.get('.luigi__more').click();
+        cy.get('.fd-menu__item')
+          .contains('Keep Selected Example')
+          .should('have.attr', 'aria-selected', 'true');
+      });
+
+      it('recalc of tab nav by using resizing', () => {
+        cy.visit('/projects/tabNav');
+        cy.get('.luigi-tabsContainer').within(() => {
+          cy.get('.fd-tabs__item').contains('User Management');
+          cy.get('.fd-tabs__item').contains('Node with node activation hook');
+          cy.get('.fd-tabs__item')
+            .contains('Settings')
+            .should('not.visible');
+        });
+        cy.viewport(900, 750);
+        cy.get('.luigi-tabsContainer').within(() => {
+          cy.get('.fd-tabs__item')
+            .contains('Settings')
+            .should('visible');
+          cy.get('.fd-tabs__item')
+            .contains('Settings')
+            .click();
+          cy.get('.fd-popover')
+            .contains('Project Settings')
+            .click();
+          cy.expectPathToBe('/projects/tabNav/settings');
+          cy.get('.fd-tabs__item')
+            .contains('Settings')
+            .should('have.attr', 'aria-selected', 'true');
+        });
+      });
+
+      it('ResponsiveNavigation Semicollapsed', () => {
+        cy.viewport(1000, 600);
+        cy.window().then(win => {
+          const config = win.Luigi.getConfig();
+          config.settings.responsiveNavigation = 'semiCollapsible';
+          win.Luigi.configChanged('settings');
+          cy.get('[data-testid="mobile-menu"]').click();
+          cy.get('.fd-popover__body').within(() => {
+            cy.get('[data-testid="projects_projects"]').click();
+          });
+          cy.get('.fd-side-nav').contains('Horizontal Navigation Example');
+          cy.get('[data-testid="tabnav_horizontalnavigationexample"]').click();
+          cy.get('.fd-tabs__item')
+            .contains('Miscellaneous 2')
+            .should('not.visible');
+          cy.get('.sap-icon--close-command-field').click();
+          cy.wait(1000);
+          cy.get('.fd-tabs__item')
+            .contains('Miscellaneous2')
+            .should('visible');
+        });
+      });
     });
   });
 });
