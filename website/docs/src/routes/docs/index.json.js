32077d26c729a19850b4cfc9fb6e3053e1b3df6d
Johannes Doberer <johannes.doberer@sap.com>
2019-11-07 07:31:17 +0100

    no message

--- website/docs/src/routes/docs/index.json.js
+++ website/docs/src/routes/docs/index.json.js
@@ -2,13 +2,14 @@
 import { getParsedDocs } from './_parser';
 
 export function get(req, response) {
-  res.setHeader("Strict-Transport-Security", "max-age=31536000");
+  response.setHeader("Strict-Transport-Security", "max-age=31536000");
   response.writeHead(200, {
     'Content-Type': 'application/json',
     'Strict-Transport-Security': 'max-age=31536000'
   });
 
   getParsedDocs().then((contents) => {
+    // console.log('response', response.getHeader('Strict-Transport-Security'));
     response.end(contents);
   });
 }
