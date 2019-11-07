ae68517bb17ad4c4071fc7947f3f3a1579a8b7be
Johannes Doberer <johannes.doberer@sap.com>
2019-11-07 07:13:42 +0100

    no message

--- website/docs/src/routes/docs/index.json.js
+++ website/docs/src/routes/docs/index.json.js
@@ -2,7 +2,7 @@
 import { getParsedDocs } from './_parser';
 
 export function get(req, response) {
-  response.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
+  res.setHeader("Strict-Transport-Security", "max-age=31536000");
   response.writeHead(200, {
     'Content-Type': 'application/json',
     'Strict-Transport-Security': 'max-age=31536000; includeSubDomains; preload'
