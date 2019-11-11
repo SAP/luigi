516fae9b6f8324d4587d970704f96f9cc341a3b2
Zarko Simic <42171300+zarkosimic@users.noreply.github.com>
2019-11-11 13:21:49 +0100

    Linking to pages fix for prod

--- website/docs/src/routes/docs/_parser.js
+++ website/docs/src/routes/docs/_parser.js
@@ -11,22 +11,22 @@ export function getParsedDocs() {
     return Promise.resolve(JSON.stringify(parsedDocs));
   });
 }
-let parsedReadMeDoc;
-export function getParsedReadMeDoc() {
-  if (parsedReadMeDoc) {
-    return Promise.resolve(JSON.stringify(parsedReadMeDoc));
-  }
-  return setParsedReadMeDoc().then((doc) => {
-    parsedReadMeDoc = doc;
-    return Promise.resolve(JSON.stringify(parsedReadMeDoc));
-  });
-}
+// let parsedReadMeDoc;
+// export function getParsedReadMeDoc() {
+//   if (parsedReadMeDoc) {
+//     return Promise.resolve(JSON.stringify(parsedReadMeDoc));
+//   }
+//   return setParsedReadMeDoc().then((doc) => {
+//     parsedReadMeDoc = doc;
+//     return Promise.resolve(JSON.stringify(parsedReadMeDoc));
+//   });
+// }
 
 function setParsedDocs() {
   const dir = './../../docs';
   const parsingArr = [];
   readdirSync(dir)
-    .filter(name => name !== 'README.md')
+    // .filter(name => name !== 'README.md')
     .filter(name => name.endsWith('.md'))
     .map(name => {
       const mdContent = readFileSync(dir + '/' + name);
@@ -64,26 +64,26 @@ function setParsedDocs() {
     });
 }
 
-function setParsedReadMeDoc() {
-  const dir = './../../docs';
-  let parsingArr;
-  readdirSync(dir)
-    .find(name => {
-      if(name == 'README.md') {
-        const mdContent = readFileSync(dir + '/' + name);
-        parsingArr = new Promise((resolve) => {
-          MarkdownSvc.process(mdContent).then((contents) => {
-            resolve({
-              contents
-            });
-          })
-        });
-      }
-    });
+// function setParsedReadMeDoc() {
+//   const dir = './../../docs';
+//   let parsingArr;
+//   readdirSync(dir)
+//     .find(name => {
+//       if(name == 'README.md') {
+//         const mdContent = readFileSync(dir + '/' + name);
+//         parsingArr = new Promise((resolve) => {
+//           MarkdownSvc.process(mdContent).then((contents) => {
+//             resolve({
+//               contents
+//             });
+//           })
+//         });
+//       }
+//     });
     
-  return parsingArr
-    .then((file) => {
-      // return for sapper
-      return file;
-    });
-}
+//   return parsingArr
+//     .then((file) => {
+//       // return for sapper
+//       return file;
+//     });
+// }
--- website/docs/src/routes/docs/index.svelte
+++ website/docs/src/routes/docs/index.svelte
@@ -1,14 +1,14 @@
 <script context="module">
 	export function preload({ params, query }) {
-		return this.fetch(`docs.json`).then(r => r.json()).then(doc => {
-			return { doc };
+		return this.fetch(`docs.json`).then(r => r.json()).then(docs => {
+			return { docs };
 		});
 	}
 	
 </script>
 
 <script>
-	export let doc;
+	export let docs;
 </script>
 
 <style>
@@ -19,4 +19,14 @@
 	<title>Documentation</title>
 </svelte:head>
 
-{@html doc.contents}
+{#each docs as doc}
+		<!-- we're using the non-standard `rel=prefetch` attribute to
+				tell Sapper to load the data for the page as soon as
+				the user hovers over the link or taps it, instead of
+				waiting for the 'click' event -->
+		{#if doc.shortName == "README"}
+			{@html doc.contents}
+		{:else}
+			<li><a rel='prefetch' href='docs/{doc.shortName}'>{doc.name}</a></li>
+		{/if}
+{/each}
