89d51a40787d5679e1420f1aa87d0f333ae6d865
Markus Edenhauser <1720843+maxmarkus@users.noreply.github.com>
2019-11-14 16:47:55 +0100

    parent container for copybutton

--- website/docs/scripts/markdown.unified-preview.js
+++ website/docs/scripts/markdown.unified-preview.js
@@ -10,9 +10,10 @@ const filesToProcess = [
 
 filesToProcess.forEach(async (name) => {
   const fileContent = readFileSync(__dirname + '/mocks/' + name);
-  const result = await MarkdownSvc.process(String(fileContent), { shortName: name.replace('.md', '') });
-  console.log(`============== ${name} =================`);
-  console.log(result)
-  console.log(`============ end: ${name} ==============`);
-  console.log('');
+  MarkdownSvc.process(String(fileContent), { shortName: name.replace('.md', '') }).then(result => {
+    console.log(`============== ${name} =================`);
+    console.log(result)
+    console.log(`============ end: ${name} ==============`);
+    console.log('');
+  }).catch(e => console.error);
 });
--- website/docs/src/unified-plugins/rehype-copy-to-clipboard.js
+++ website/docs/src/unified-plugins/rehype-copy-to-clipboard.js
@@ -1,16 +1,5 @@
 import h from 'hastscript';
-import has from 'hast-util-has-property';
 import visit from 'unist-util-visit';
-import fs from 'fs';
-
-let log = () => {}
-if (process.env.NODE_ENV === 'debug') {
-  const debugFile =  __dirname + '/debug.log';
-  fs.writeFileSync(debugFile, '');
-  log = (text = '') => {
-    fs.appendFileSync(debugFile, text);
-  }
-}
 
 export default function addCopyToClipboard() {
   return function transformer(tree) {
@@ -20,17 +9,21 @@ export default function addCopyToClipboard() {
   }
 
   function modify(node, prop) {
-    if (node.tagName === 'pre') {
+    const notYetProcessed = !node.properties.className || node.properties.className.indexOf('canCopyCode') === -1; // prevent infinite loop
+    if (node.tagName === 'pre' && notYetProcessed) {
       // Docu: https://github.com/syntax-tree/hastscript#use
-      const copyCode = h('a.copyCode', { onclick: 'copyCode(event, this)' }, [
-        h('div', [
-          h('img', { src: '/images/copy-clipboard-default.svg' }),
-          h('.popoverCopy', [
-            'Click to copy'
-          ]),
-        ]),
+      const newNodeData = h('div.copyCodeContainer', [
+        h('a.copyCode', { onclick: 'copyCode(event, this)' }, 
+          [h('div', [
+            h('img', { src: '/images/copy-clipboard-default.svg' }),
+            h('.popoverCopy', [
+              'Click to copy'
+            ])]
+          )]
+        ),
+        h('pre.canCopyCode', node.children)
       ]);
-      node.children.unshift(copyCode);
+      Object.assign(node, newNodeData);
     }
   }
 }
