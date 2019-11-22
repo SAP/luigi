7ad40677b650907bb7cdcfc198666891b1388253
Zarko Simic <42171300+zarkosimic@users.noreply.github.com>
2019-11-22 16:14:19 +0100

    Add legal to footer

--- website/docs/static/luigi/privacy-policy.js
+++ website/docs/static/luigi/privacy-policy.js
@@ -1,15 +1,21 @@
 (function() {
   setTimeout(function(){
     try {
-      let a = document.createElement('a');
-      const linkText = document.createTextNode("Privacy Policy");
-      a.appendChild(linkText);
-      a.href = "https://www.sap.com/about/legal/privacy.html";
-      a.target = "_blank";
+      let aPrivacyPolicy = document.createElement('a');
+      const linkTextPrivacyPolicy = document.createTextNode("Privacy Policy");
+      aPrivacyPolicy.appendChild(linkTextPrivacyPolicy);
+      aPrivacyPolicy.href = "https://www.sap.com/about/legal/privacy.html";
+      aPrivacyPolicy.target = "_blank";
       const parent = document.getElementsByClassName("lui-side-nav__footer--text")[0];
       const child = document.getElementsByClassName("lui-side-nav__footer--text")[0].firstChild;
       parent.removeChild(child);
-      document.getElementsByClassName("lui-side-nav__footer--text")[0].appendChild(a);
+      document.getElementsByClassName("lui-side-nav__footer--text")[0].appendChild(aPrivacyPolicy);
+      let aLegal = document.createElement('a');
+      const linkTextLegal = document.createTextNode("Legal");
+      aLegal.appendChild(linkTextLegal);
+      aLegal.href = "https://www.sap.com/about/legal/impressum.html";
+      aLegal.target = "_blank";
+      document.getElementsByClassName("lui-side-nav__footer--text")[0].appendChild(aLegal);
     } catch(e) {
       console.error('Something went wrong!', e);
     }
