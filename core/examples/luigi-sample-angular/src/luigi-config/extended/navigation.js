3e9ec01976591d3cfa040abd0727ccccfa758bdf
Johannes Doberer <johannes.doberer@sap.com>
2019-12-05 14:40:33 +0100

    no message

--- core/examples/luigi-sample-angular/src/luigi-config/extended/navigation.js
+++ core/examples/luigi-sample-angular/src/luigi-config/extended/navigation.js
@@ -240,10 +240,8 @@ class Navigation {
           label: 'Environment ' + n, // (i.e mapping between what the user sees and what is taken to replace the dynamic part for the dynamic node)
           pathValue: 'env' + n // will be used to replace dynamic part
         })),
-    customListRenderer: (node, selectedOption, optionLink) => {
-      return `<li onclick="window.Luigi.navigation().navigate('${optionLink}')">
-        <a class="fd-menu__item">${node.label} Pete</a>
-              </li>`;
+    customOptionsRenderer: option => {
+      return `<a class="fd-menu__item">${option.label}</a>`;
     },
     actions: this.getContextSwitcherActions,
 
--- core/src/core-api/navigation.js
+++ core/src/core-api/navigation.js
@@ -28,30 +28,30 @@ class LuigiNavigationManager {
     window.postMessage({ msg: 'luigi.navigation.update-badge-counters' }, '*');
   }
 
-  navigate(path, preserveView, modalSettings, splitViewSettings) {
-    if (this.options.errorSkipNavigation) {
-      this.options.errorSkipNavigation = false;
-      return;
-    }
-    if (modalSettings && splitViewSettings) {
-      console.warn(
-        'modalSettings and splitViewSettings cannot be used together. Only modal setting will be taken into account.'
-      );
-    }
+  // navigate(path, preserveView, modalSettings, splitViewSettings) {
+  //   if (this.options.errorSkipNavigation) {
+  //     this.options.errorSkipNavigation = false;
+  //     return;
+  //   }
+  //   if (modalSettings && splitViewSettings) {
+  //     console.warn(
+  //       'modalSettings and splitViewSettings cannot be used together. Only modal setting will be taken into account.'
+  //     );
+  //   }
 
-    this.options.preserveView = preserveView;
-    const relativePath = path[0] !== '/';
-    const navigationOpenMsg = {
-      msg: 'luigi.navigation.open',
-      params: Object.assign(this.options, {
-        link: path,
-        relative: relativePath,
-        modal: modalSettings,
-        splitView: splitViewSettings
-      })
-    };
-    window.postMessage(navigationOpenMsg, '*');
-  }
+  //   this.options.preserveView = preserveView;
+  //   const relativePath = path[0] !== '/';
+  //   const navigationOpenMsg = {
+  //     msg: 'luigi.navigation.open',
+  //     params: Object.assign(this.options, {
+  //       link: path,
+  //       relative: relativePath,
+  //       modal: modalSettings,
+  //       splitView: splitViewSettings
+  //     })
+  //   };
+  //   window.postMessage(navigationOpenMsg, '*');
+  // }
 }
 
 export const navigation = new LuigiNavigationManager();
