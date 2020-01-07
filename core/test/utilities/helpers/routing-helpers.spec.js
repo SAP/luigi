9c4e98a9e3625abff5970f61640acd70d75d71b9
Markus Edenhauser <1720843+maxmarkus@users.noreply.github.com>
2020-01-07 14:53:23 +0100

    unit tets

--- core/test/utilities/helpers/routing-helpers.spec.js
+++ core/test/utilities/helpers/routing-helpers.spec.js
@@ -1,7 +1,10 @@
+const sinon = require('sinon');
 const chai = require('chai');
 const expect = chai.expect;
 const assert = chai.assert;
-import { RoutingHelpers } from '../../../src/utilities/helpers';
+import { GenericHelpers, RoutingHelpers } from '../../../src/utilities/helpers';
+import { LuigiConfig } from '../../../src/core-api';
+import { Routing } from '../../../src/services/routing';
 
 describe('Routing-helpers', () => {
   describe('substituteDynamicParamsInObject', () => {
@@ -173,6 +176,131 @@ describe('Routing-helpers', () => {
           entry: 'e23'
         })
       ).to.equal('/projects/pr1/details/e23');
+    });
+  });
+  describe('buildRoute', () => {
+    const node = {
+      pathSegment: 'one',
+      parent: {
+        pathSegment: 'projects',
+        parent: {
+          pathSegment: 'home'
+        }
+      }
+    };
+    it('without params', () => {
+      assert.equal(
+        RoutingHelpers.buildRoute(node, '/' + node.pathSegment),
+        '/home/projects/one'
+      );
+    });
+    it('with params', () => {
+      const params = 'sort=desc&filter=false';
+      assert.equal(
+        RoutingHelpers.buildRoute(node, '/' + node.pathSegment, params),
+        '/home/projects/one?' + params
+      );
+    });
+  });
+  describe('getRouteLink', () => {
+    beforeEach(() => {
+      sinon.stub(LuigiConfig, 'getConfigBooleanValue');
+      sinon.stub(Routing, 'buildFromRelativePath');
+      sinon.stub(RoutingHelpers, 'buildRoute');
+      sinon.stub(GenericHelpers, 'replaceVars');
+    });
+    afterEach(() => {
+      sinon.restore();
+    });
+    it('externalLink', () => {
+      const expected = { url: 'https://luigi-project.io' };
+      const given = {
+        externalLink: expected,
+        link: 'something',
+        pathSegment: 'something-else'
+      };
+
+      assert.equal(RoutingHelpers.getRouteLink(given), expected);
+    });
+    it('when it starts with /', () => {
+      const expected = '/projects';
+      const given = {
+        link: expected,
+        pathSegment: 'something-else'
+      };
+
+      assert.equal(RoutingHelpers.getRouteLink(given), expected);
+      sinon.assert.notCalled(Routing.buildFromRelativePath);
+    });
+    it('calls build function for relative links', () => {
+      const given = {
+        link: 'projects',
+        pathSegment: 'something-else'
+      };
+      Routing.buildFromRelativePath.returns(`/${given.link}`);
+
+      assert.equal(RoutingHelpers.getRouteLink(given), '/projects');
+      sinon.assert.calledWith(Routing.buildFromRelativePath, given);
+    });
+    it('on pathSegment calls getRoute and replaceVars', () => {
+      const expected = '/projects/something-else';
+      const given = {
+        pathSegment: 'something-else'
+      };
+
+      RoutingHelpers.buildRoute.returns(expected);
+      GenericHelpers.replaceVars.returns(expected);
+
+      assert.equal(RoutingHelpers.getRouteLink(given), expected);
+
+      sinon.assert.notCalled(Routing.buildFromRelativePath);
+      sinon.assert.calledWith(
+        RoutingHelpers.buildRoute,
+        given,
+        '/' + given.pathSegment
+      );
+      sinon.assert.calledWith(
+        GenericHelpers.replaceVars,
+        expected,
+        undefined,
+        ':',
+        false
+      );
+    });
+  });
+  describe('getNodeHref', () => {
+    beforeEach(() => {
+      sinon.stub(LuigiConfig, 'getConfigBooleanValue');
+      sinon.stub(RoutingHelpers, 'getRouteLink');
+    });
+    afterEach(() => {
+      sinon.restore();
+    });
+    it('js:void on falsy config value', () => {
+      LuigiConfig.getConfigBooleanValue.returns(false);
+
+      expect(RoutingHelpers.getNodeHref({}, {})).to.equal('javascript:void(0)');
+      sinon.assert.notCalled(RoutingHelpers.getRouteLink);
+      sinon.assert.calledWith(
+        LuigiConfig.getConfigBooleanValue,
+        'navigation.addNavHrefs'
+      );
+    });
+    it('returns valid link on url object', () => {
+      LuigiConfig.getConfigBooleanValue.returns(true);
+      RoutingHelpers.getRouteLink.returns({ url: '/test' });
+      expect(RoutingHelpers.getNodeHref({ pathSegment: 'test' }, {})).to.equal(
+        '/test'
+      );
+      sinon.assert.calledOnce(RoutingHelpers.getRouteLink);
+    });
+    it('returns valid link on string received', () => {
+      LuigiConfig.getConfigBooleanValue.returns(true);
+      RoutingHelpers.getRouteLink.returns('/test');
+      expect(RoutingHelpers.getNodeHref({ pathSegment: 'test' }, {})).to.equal(
+        '/test'
+      );
+      sinon.assert.calledOnce(RoutingHelpers.getRouteLink);
     });
   });
 });
