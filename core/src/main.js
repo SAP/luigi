98bbbf603c445aadbf93924b7e3cb9d3dfd4c3cf
Markus Edenhauser <1720843+maxmarkus@users.noreply.github.com>
2020-01-17 18:22:45 +0100

    moved business logic to auth-layer, added writable store for userinfo and loggedIn state

--- core/src/main.js
+++ core/src/main.js
@@ -1,8 +1,7 @@
 import App from './App.html';
 import { LuigiConfig, LuigiI18N, LuigiElements } from './core-api';
 import { writable, readable } from 'svelte/store';
-import { version } from '../package.json';
-
+import { AuthLayerSvc } from './services';
 const createConfigStore = () => {
   const { subscribe, update, reset } = writable({});
   const scopeSubscribers = {};
@@ -39,40 +38,44 @@ Luigi._store = store;
 const configReadyCallback = () => {
   return new Promise(resolve => {
     LuigiI18N._init();
-    // setTimeout needed so that luigi container is rendered when we retrieve it
-    let app;
-    setTimeout(() => {
-      if (LuigiElements.isCustomLuigiContainer()) {
-        document
-          .getElementsByTagName('html')[0]
-          .classList.add('luigi-app-in-custom-container');
-      }
 
-      app = new App({
-        target: LuigiElements.getLuigiContainer(),
-        props: {
-          store,
-          getTranslation
+    AuthLayerSvc.init().then(() => {
+      // setTimeout needed so that luigi container is rendered when we retrieve it
+      setTimeout(() => {
+        let app;
+
+        if (LuigiElements.isCustomLuigiContainer()) {
+          document
+            .getElementsByTagName('html')[0]
+            .classList.add('luigi-app-in-custom-container');
         }
+
+        app = new App({
+          target: LuigiElements.getLuigiContainer(),
+          props: {
+            store,
+            getTranslation
+          }
+        });
+
+        Luigi.showAlert = settings => {
+          return app.$$.ctx.showAlert(settings);
+        };
+
+        Luigi.showConfirmationModal = settings => {
+          return app.$$.ctx.showModal(settings);
+        };
+
+        Luigi.pathExists = path => {
+          return app.$$.ctx.pathExists(path);
+        };
+
+        Luigi.hasBack = () => {
+          return app.$$.ctx.hasBack();
+        };
+
+        resolve();
       });
-
-      Luigi.showAlert = settings => {
-        return app.$$.ctx.showAlert(settings);
-      };
-
-      Luigi.showConfirmationModal = settings => {
-        return app.$$.ctx.showModal(settings);
-      };
-
-      Luigi.pathExists = path => {
-        return app.$$.ctx.pathExists(path);
-      };
-
-      Luigi.hasBack = () => {
-        return app.$$.ctx.hasBack();
-      };
-
-      resolve();
     });
   });
 };
---
+++ core/src/services/auth-layer.js
@@ -0,0 +1,236 @@
+import { LuigiAuth, LuigiConfig } from '../core-api';
+import {
+  AuthHelpers,
+  GenericHelpers,
+  StateHelpers
+} from '../utilities/helpers';
+import { AuthStoreSvc } from './';
+import { get, writable } from 'svelte/store';
+
+class AuthLayerSvcClass {
+  constructor() {
+    this._userInfoStore = writable({});
+    this._loggedInStore = writable(false);
+    return this;
+  }
+
+  setUserInfo(uInfo) {
+    this._userInfoStore.set(uInfo);
+  }
+  setLoggedIn(loggedIn) {
+    this._loggedInStore.set(loggedIn);
+  }
+
+  getUserInfoStore() {
+    return this._userInfoStore;
+  }
+
+  getLoggedInStore() {
+    return this._loggedInStore;
+  }
+
+  async init() {
+    const idpProviderName = LuigiConfig.getConfigValue('auth.use');
+    const idpProviderSettings = LuigiConfig.getConfigValue(
+      `auth.${idpProviderName}`
+    );
+
+    /**
+     * Prevent IDP Provider initialization, if an error is present
+     * in the url params and onAuthError is defined in the user config.
+     * errors are represented by `error` and `errorDescription` param.
+     */
+    const uaError = AuthHelpers.parseUrlAuthErrors() || {};
+    const noError = await AuthHelpers.handleUrlAuthErrors(
+      idpProviderSettings,
+      uaError.error,
+      uaError.errorDescription
+    );
+    if (!noError) {
+      return;
+    }
+
+    this.idpProviderInstance = this.getIdpProviderInstance(
+      idpProviderName,
+      idpProviderSettings
+    );
+    if (GenericHelpers.isPromise(this.idpProviderInstance)) {
+      return this.idpProviderInstance
+        .then(resolved => {
+          this.idpProviderInstance = resolved;
+          return this.checkAuth(idpProviderSettings);
+        })
+        .catch(err => {
+          const errorMsg = `Error: ${err.message || err}`;
+          console.error(errorMsg, err.message && err);
+          LuigiConfig.setErrorMessage(errorMsg);
+        });
+    }
+    return this.checkAuth(idpProviderSettings);
+  }
+
+  isAuthValid() {
+    return (
+      AuthHelpers.getStoredAuthData().accessTokenExpirationDate >
+      Number(new Date())
+    );
+  }
+
+  async checkAuth(idpProviderSettings) {
+    const authData = AuthHelpers.getStoredAuthData();
+    if (!authData || !this.isAuthValid()) {
+      if (LuigiConfig.getConfigValue('auth.disableAutoLogin')) {
+        return;
+      }
+
+      /**
+       * onAuthExpired
+       * If onAuthExpired exists, it will be evaluated.
+       * Continues with the standard authorization flow,
+       * if `onAuthExpired` it returns undefined or truthy value.
+       */
+      let startAuth = true;
+      if (authData) {
+        startAuth = await LuigiAuth.handleAuthEvent(
+          'onAuthExpired',
+          idpProviderSettings
+        );
+      }
+      if (startAuth) {
+        return this.startAuthorization();
+      }
+      return;
+    }
+
+    if (
+      this.idpProviderInstance.settings &&
+      GenericHelpers.isFunction(this.idpProviderInstance.settings.userInfoFn)
+    ) {
+      this.idpProviderInstance.settings
+        .userInfoFn(this.idpProviderInstance.settings, authData)
+        .then(userInfo => {
+          this.setUserInfo(userInfo);
+          this.setLoggedIn(true);
+        });
+    } else {
+      if (GenericHelpers.isFunction(this.idpProviderInstance.userInfo)) {
+        this.idpProviderInstance
+          .userInfo(idpProviderSettings)
+          .then(userInfo => {
+            this.setLoggedIn(true);
+          });
+      } else {
+        this.setLoggedIn(true);
+        this.setUserInfo(get(this._userInfoStore));
+      }
+    }
+
+    const hasAuthSuccessFulFn = GenericHelpers.isFunction(
+      LuigiConfig.getConfigValue('auth.events.onAuthSuccessful')
+    );
+    if (hasAuthSuccessFulFn && AuthStoreSvc.isNewlyAuthorized) {
+      AuthStoreSvc.removeNewlyAuthorized();
+      await LuigiAuth.handleAuthEvent(
+        'onAuthSuccessful',
+        idpProviderSettings,
+        authData
+      );
+    }
+
+    if (
+      GenericHelpers.isFunction(
+        this.idpProviderInstance.setTokenExpirationAction
+      )
+    ) {
+      this.idpProviderInstance.setTokenExpirationAction();
+    }
+    if (
+      GenericHelpers.isFunction(
+        this.idpProviderInstance.setTokenExpireSoonAction
+      )
+    ) {
+      this.idpProviderInstance.setTokenExpireSoonAction();
+    }
+  }
+  async startAuthorization() {
+    if (this.idpProviderInstance) {
+      return this.idpProviderInstance.login().then(res => {
+        AuthStoreSvc.setNewlyAuthorized();
+        if (res) {
+          alert(res);
+        }
+        return;
+      });
+    }
+  }
+
+  logout() {
+    const authData = AuthHelpers.getStoredAuthData();
+    const logoutCallback = async redirectUrl => {
+      await LuigiAuth.handleAuthEvent(
+        'onLogout',
+        this.idpProviderInstance.settings,
+        undefined,
+        redirectUrl
+      );
+      AuthStoreSvc.removeAuthData();
+    };
+    const customLogoutFn = LuigiConfig.getConfigValue(
+      `auth.${LuigiConfig.getConfigValue('auth.use')}.logoutFn`
+    );
+    if (GenericHelpers.isFunction(customLogoutFn)) {
+      customLogoutFn(
+        this.idpProviderInstance.settings,
+        authData,
+        logoutCallback
+      );
+    } else if (GenericHelpers.isFunction(this.idpProviderInstance.logout)) {
+      this.idpProviderInstance.logout(authData, logoutCallback);
+    } else if (isProfileLogoutItem && profileLogoutfnDefinded) {
+      profileNav.logout.customLogoutFn();
+    } else {
+      logoutCallback(this.idpProviderInstance.settings.logoutUrl);
+    }
+  }
+
+  IdpProviderException(message) {
+    return { message, name: 'IdpProviderException' };
+  }
+
+  async getIdpProviderInstance(idpProviderName, idpProviderSettings) {
+    // custom provider provided via config:
+    const idpProvider = GenericHelpers.getConfigValueFromObject(
+      idpProviderSettings,
+      'idpProvider'
+    );
+    if (idpProvider) {
+      const customIdpInstance = await new idpProvider(idpProviderSettings);
+      ['login'].forEach(requiredFnName => {
+        if (!GenericHelpers.isFunction(customIdpInstance[requiredFnName])) {
+          throw this.IdpProviderException(
+            `${requiredFnName} function does not exist in custom IDP Provider ${idpProviderName}`
+          );
+        }
+      });
+
+      return customIdpInstance;
+    }
+
+    // handle non-existing providers
+    const onAuthConfigError = GenericHelpers.isFunction(
+      LuigiConfig.getConfigValue('auth.events.onAuthConfigError')
+    );
+    if (onAuthConfigError) {
+      await LuigiAuth.handleAuthEvent('onAuthConfigError', {
+        idpProviderName: idpProviderName,
+        type: 'IdpProviderException'
+      });
+    } else {
+      throw IdpProviderException(
+        `IDP Provider ${idpProviderName} does not exist.`
+      );
+    }
+  }
+}
+
+export const AuthLayerSvc = new AuthLayerSvcClass();
