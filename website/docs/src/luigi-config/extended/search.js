7f2e92bba534be79bdd16517333afeae0b118376
ndricim <ndricim.rrapi@sap.com>
2020-03-25 12:59:30 +0100

    Prettier

--- website/docs/src/luigi-config/extended/search.js
+++ website/docs/src/luigi-config/extended/search.js
@@ -19,12 +19,12 @@ class DocSearch {
   }
 
   addSearchField() {
-    const createElementFromHTML = (htmlString) => {
+    const createElementFromHTML = htmlString => {
       var div = document.createElement('div');
       div.innerHTML = htmlString.trim();
       // Change this to div.childNodes to support multiple top-level nodes
-      return div.firstChild; 
-    }
+      return div.firstChild;
+    };
     const searchElement = createElementFromHTML(`
       <div class="fd-shellbar__action">
         <div class="fd-search-input fd-search-input--closed">
@@ -47,16 +47,19 @@ class DocSearch {
   }
 
   initDocSearch() {
-    const transformData = (suggestions) => {
-      return suggestions.map((sg) => {
+    const transformData = suggestions => {
+      return suggestions.map(sg => {
         if (this.isDevelop) {
-          sg.url = sg.url.replace('https://docs.luigi-project.io', this.coreBaseUrl);
+          sg.url = sg.url.replace(
+            'https://docs.luigi-project.io',
+            this.coreBaseUrl
+          );
         }
         sg.url = sg.url.replace('/docu-microfrontend', '');
         return sg;
       });
     };
-    
+
     const handleSelected = (_, event) => {
       if (
         !event ||
@@ -69,9 +72,14 @@ class DocSearch {
         return;
       }
       const url = new URL(event._args[0].url);
-      const urlWithPath = url.pathname.replace(this.coreBaseUrl, '').replace('.md', '').replace('/docu-microfrontend', '');
+      const urlWithPath = url.pathname
+        .replace(this.coreBaseUrl, '')
+        .replace('.md', '')
+        .replace('/docu-microfrontend', '');
       if (url.hash) {
-        Luigi.navigation().withParams({'section': url.hash.substring(1).toLowerCase()}).navigate(urlWithPath);
+        Luigi.navigation()
+          .withParams({ section: url.hash.substring(1).toLowerCase() })
+          .navigate(urlWithPath);
       } else {
         Luigi.navigation().navigate(urlWithPath);
       }
@@ -79,7 +87,7 @@ class DocSearch {
 
     const createAlgoliaOptions = () => {
       const algoliaOptions = {
-        hitsPerPage: 8,
+        hitsPerPage: 8
       };
 
       return {
@@ -91,10 +99,11 @@ class DocSearch {
           openOnFocus: false,
           autoselect: true,
           hint: true,
+          clearOnSelected: true
         },
         algoliaOptions,
         transformData,
-        handleSelected,
+        handleSelected
       };
     };
     docsearch(createAlgoliaOptions());
@@ -102,7 +111,7 @@ class DocSearch {
 
   attachHandlers() {
     const inputEl = document.getElementById('lui-search-field');
-  
+
     const focusSearch = () => {
       let inputField = document.getElementById('docsearch');
       if (this.inputActive) {
@@ -110,23 +119,25 @@ class DocSearch {
           inputField.focus();
         }, 200);
       } else {
-        inputField.value = "";
+        inputField.value = '';
       }
     };
-    
+
     const toggleInputActive = () => {
       this.inputActive = !this.inputActive;
-      const searchButton = document.getElementById('lui-search-button');    
+      const searchButton = document.getElementById('lui-search-button');
       searchButton.setAttribute('aria-hidden', this.inputActive);
       searchButton.setAttribute('aria-expanded', !this.inputActive);
       inputEl.setAttribute('aria-hidden', !this.inputActive);
-    }
+    };
 
-    document.getElementById('lui-search-button').addEventListener('click', (e) => {
-      e.preventDefault();
-      toggleInputActive();
-      focusSearch();
-    });
+    document
+      .getElementById('lui-search-button')
+      .addEventListener('click', e => {
+        e.preventDefault();
+        toggleInputActive();
+        focusSearch();
+      });
   }
 }
 
