ee6b011825e526486b715fa97188d233e699e340
Markus Edenhauser <1720843+maxmarkus@users.noreply.github.com>
2020-04-01 19:43:33 +0200

    check if it works with higher timeout

--- test/e2e-test-application/e2e/support/commands.js
+++ test/e2e-test-application/e2e/support/commands.js
@@ -249,7 +249,7 @@ function onIframeReady($iframe, successFn, errorFn) {
  * returns the window instance of an iframe
  */
 Cypress.Commands.add('getIframeWindow', (num = 0) => {
-  cy.wait(0); // Just wait for next tick
+  cy.wait(100);
   return cy
     .get('iframe')
     .eq(num)
