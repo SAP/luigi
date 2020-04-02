7703d245912c7573ce562f04f148c6492f2127a4
Ndricim Rrapi <ndricim.rrapi@sap.com>
2020-04-02 15:25:22 +0200

    Update luigi-client.d.ts

--- client/luigi-client.d.ts
+++ client/luigi-client.d.ts
@@ -224,6 +224,7 @@ export declare interface LinkManager {
    * Enables navigating to sibling nodes without knowing the absolute path
    * @memberof linkManager
    * @returns {linkManager} link manager instance
+   * @since NEXTRELASE
    * @example
    * LuigiClient.linkManager().fromParent().navigate('/sibling')
    */
