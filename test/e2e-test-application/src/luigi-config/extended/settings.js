4005f3fe2789397cf2c2e77aa0d20696ac6bb297
Maryna Khromova <46446373+marynaKhromova@users.noreply.github.com>
2020-04-07 17:53:31 +0200

    adjust modal with iframe

--- test/e2e-test-application/src/luigi-config/extended/settings.js
+++ test/e2e-test-application/src/luigi-config/extended/settings.js
@@ -11,7 +11,7 @@ class Settings {
   appLoadingIndicator = {
     hideAutomatically: false
   };
-  responsiveNavigation = 'simpleMobileOnly'; // Options: simple | simpleMobileOnly | semiCollapsible | Fiori3
+  responsiveNavigation = 'simple'; // Options: simple | simpleMobileOnly | semiCollapsible | Fiori3
   sideNavFooterText = `Luigi Client: ${version || 'unknown'}`;
   thirdPartyCookieCheck = {
     // thirdPartyCookieScriptLocation: 'https://domain/init.html',
