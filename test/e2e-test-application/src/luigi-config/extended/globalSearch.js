d7332a96bc6dbe47166ef18f8df5b57237e072b0
JohannesDoberer <johannes.doberer@sap.com>
2020-04-24 10:43:58 +0200

    no message

--- test/e2e-test-application/src/luigi-config/extended/globalSearch.js
+++ test/e2e-test-application/src/luigi-config/extended/globalSearch.js
@@ -1,4 +1,13 @@
 class GlobalSearch {
   enabled = true;
+  search = {
+    customSearch: e => {
+      return new Promise((resolve, reject) => {
+        if (e.keyCode === 13) {
+          return resolve(['search something crazy', 'more crazy stuff']);
+        }
+      });
+    }
+  };
 }
 export const globalSearch = new GlobalSearch();
