75f3fdeeff8e1839f5f401572f72033a7c590b7c
Markus Edenhauser <1720843+maxmarkus@users.noreply.github.com>
2020-05-06 14:34:17 +0200

    remove spinner from index, simplified vue setup

--- scripts/setup/vue.sh
+++ scripts/setup/vue.sh
@@ -15,23 +15,41 @@ else
   echo "Luigi project folder name: $folder"
 fi
 # steps to execute line by line
-echo ""
+# echo "Creating Vue base with Luigi Core and Config"
+# vue create -d $folder && cd $folder
+# echo "Creating Vue Micro-Frontend"
 vue create -d $folder && cd $folder
+npm i vue-router vuex @luigi-project/core @luigi-project/client fundamental-styles @sap-theming/theming-base-content
+npm i -D sass-loader node-sass webpack webpack-cli @babel/core @babel/preset-env babel-loader
 
-vue create -d my-original-vue-app && cd my-original-vue-app
-npm i @luigi-project/core @luigi-project/client fundamental-styles
-npm i -D @sap-theming/theming-base-content webpack webpack-cli @babel/core @babel/preset-env babel-loader
-mkdir -p public/assets
-mkdir -p src/luigi-config
-mv public/index.html public/vue.html
+mkdir -p src/luigi-config src/assets/scss src/views public/assets
+rm public/index.html
 
-# download assets
-curl http://localhost:8000/assets/index.html > public/index.html
-curl http://localhost:8000/assets/luigi-config.es6.js > src/luigi-config/luigi-config.es6.js
-curl http://localhost:8000/assets/basicMicroFrontend.html > public/assets/basicMicroFrontend.html
-# curl https://raw.githubusercontent.com/SAP/luigi/master/scripts/setup/assets/index.html > public/index.html
-# curl https://raw.githubusercontent.com/SAP/luigi/master/scripts/setup/assets/luigi-config.es6.js > src/luigi-config/luigi-config.es6.js
-# curl https://raw.githubusercontent.com/SAP/luigi/master/scripts/setup/assets/basicMicroFrontend.html > public/assets/basicMicroFrontend.html
+sed 's/"scripts": {/"scripts": {\
+  \  "buildConfig":"webpack --entry .\/src\/luigi-config\/luigi-config.es6.js -o .\/public\/assets\/luigi-config.js --mode production",/1' package.json > p.tmp.json && mv p.tmp.json package.json
+
+
+echo "@import '~fundamental-styles/dist/fundamental-styles.css';
+" > src/assets/scss/style.scss
+
+# cleanup defaults
+rm -f src/app.vue
+rm -rf src/components
+
+# fetch assets from example
+curl https://raw.githubusercontent.com/SAP/luigi/master/core/examples/luigi-example-vue/public/sampleapp.html > public/sampleapp.html
+curl https://raw.githubusercontent.com/SAP/luigi/master/core/examples/luigi-example-vue/src/app.vue > src/app.vue
+curl https://raw.githubusercontent.com/SAP/luigi/master/core/examples/luigi-example-vue/src/main.js > src/main.js
+curl https://raw.githubusercontent.com/SAP/luigi/master/core/examples/luigi-example-vue/src/router.js > src/router.js
+curl https://raw.githubusercontent.com/SAP/luigi/master/core/examples/luigi-example-vue/src/store.js > src/store.js
+curl https://raw.githubusercontent.com/SAP/luigi/master/core/examples/luigi-example-vue/src/views/home.vue > src/views/home.vue
+curl https://raw.githubusercontent.com/SAP/luigi/master/core/examples/luigi-example-vue/src/views/sample1.vue > src/views/sample1.vue
+curl https://raw.githubusercontent.com/SAP/luigi/master/core/examples/luigi-example-vue/src/views/sample2.vue > src/views/sample2.vue
+
+# generic assets
+curl https://raw.githubusercontent.com/SAP/luigi/master/scripts/setup/assets/index.html > public/index.html
+curl https://raw.githubusercontent.com/SAP/luigi/master/core/examples/luigi-example-vue/public/luigi-config.js > src/luigi-config/luigi-config.es6.js
+
 
 echo "const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
@@ -39,24 +57,47 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
   pages: {
     sampleapp: {
-      entry:'src/main.js',
-      template:'public/vue.html',filename:'vue.html'
+      entry: 'src/main.js',
+      template: 'public/sampleapp.html',
+      filename: 'sampleapp.html'
     }
   },
   lintOnSave:true,
-  runtimeCompiler:true,
-  outputDir:'dist',
+  runtimeCompiler: true,
+  outputDir: 'dist',
   configureWebpack: {
-    plugins:[
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ['css-loader']
+        },
+        {
+          test: /\.scss$/,
+          use: ['sass-loader']
+        }
+      ]
+    },
+    plugins: [
       new CopyWebpackPlugin(
         [
           {context:'public',to:'index.html',from:'index.html'},
           {context:'node_modules/@luigi-project/core',to:'./luigi-core',from:{glob:'**',dot:true}},
-          {context:'node_modules/@luigi-project/client',to:'./luigi-client',from:{glob:'**',dot:true}}
+          {context:'node_modules/@luigi-project/client',to:'./luigi-client',from:{glob:'**',dot:true}},
+          {
+            from: 'node_modules/fundamental-styles/dist',
+            to: './fundamental-styles'
+          },
+          {
+            from: 'node_modules/@sap-theming/theming-base-content',
+            to: './fonts'
+          }
         ],
         {ignore:['.gitkeep','**/.DS_Store','**/Thumbs.db'],debug:'warning'}
       )]
     }
   };" > vue.config.js
+
+npm run buildConfig
 
 npm run serve
