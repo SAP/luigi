9e7d1f567ad88840548cfe77458188053376e5a8
JohannesDoberer <johannes.doberer@sap.com>
2020-05-20 09:25:14 +0200

    no message

--- test/e2e-test-application/src/luigi-config/extended/globalSearch.js
+++ test/e2e-test-application/src/luigi-config/extended/globalSearch.js
@@ -1,25 +1,20 @@
 class GlobalSearch {
+  // searchResultItem = {
+  //   pathObject: {
+  //     path: 'projects/pr1',
+  //     params: {} // can be used by linkmanager.navigate(path).withParams(params)
+  //   },
+  //   label: 'Project 1',
+  //   description: 'You can find info about project one',
+  //   onActivate() { }
+  // }
   searchProvider = {
-    searchProvider: {
-      onInput: () => {
-        //Luigi.globalSearch().getSearchString();
-        let searchResultItem = {
-          pathObject: {
-            path,
-            params: {} // can be used by linkmanager.navigate(path).withParams(params)
-          },
-          label,
-          description,
-          onActivate() {}
-        };
-        //Luigi.globalSearch().showSearchResult([searchResultItem]);
-      }
-    },
-    onEnter: () => {},
-    onEscape: () => {},
-    customResultRenderer: searchResultItem => {},
-    onSearchResultItemSelected: searchResultItem => {}
+    onInput: () => {}
   };
+  onEnter = () => {};
+  onEscape = () => {};
+  customResultRenderer = searchResultItem => {};
+  onSearchResultItemSelected = searchResultItem => {};
 }
 
 export const globalSearch = new GlobalSearch();
