e80635cb7986998d311160ab43a22d009176c9b0
JohannesDoberer <johannes.doberer@sap.com>
2020-06-19 13:43:48 +0200

    customSearchResultItemRenderer

--- test/e2e-test-application/src/luigi-config/extended/globalSearch.js
+++ test/e2e-test-application/src/luigi-config/extended/globalSearch.js
@@ -126,42 +126,52 @@ class GlobalSearch {
       Luigi.globalSearch().clearSearchField();
       this.searchResult = [];
     },
-    customSearchResultRenderer: (searchResults, slot, searchApiObj) => {
-      let div = document.createElement('div');
-      div.setAttribute(
-        'class',
-        'fd-popover__body fd-popover__body--right luigi-search__popover__body'
-      );
-      let nav = document.createElement('nav');
-      nav.setAttribute('class', 'fd-menu');
-      let ul = document.createElement('ul');
-      ul.setAttribute('class', 'fd-menu__list fd-menu__list--top');
-      for (let i = 0; i < searchResults.length; i++) {
-        let li = document.createElement('li');
-        li.setAttribute('class', 'fd-menu__item');
-        li.setAttribute(
-          'style',
-          'background-color:green;border:1px solid red;'
-        );
-        let a = document.createElement('a');
-        a.addEventListener('click', e => {
-          console.log('e', e);
-          searchApiObj.fireItemSelected(searchResults[i]);
-        });
-        a.setAttribute('class', 'fd-menu__link');
-        let span = document.createElement('span');
-        span.setAttribute('class', 'fd-menu__title');
-        span.innerHTML = searchResults[i].label;
-        a.appendChild(span);
-        li.appendChild(a);
-        ul.appendChild(li);
-      }
-      nav.appendChild(ul);
-      div.appendChild(nav);
-      slot.appendChild(div);
-    },
-    // customSearchResultItemRenderer(searchResultItem) {
-    //   return `<a class="fd-menu__link" style="background-color:yellow;border:1px solid blue;" on:click|preventDefault="{() => {}}"><span class="fd-menu__title">${searchResultItem.label}</span></a>`;
+    // customSearchResultRenderer: (searchResults, slot, searchApiObj) => {
+    //   let div = document.createElement('div');
+    //   div.setAttribute(
+    //     'class',
+    //     'fd-popover__body fd-popover__body--right luigi-search__popover__body'
+    //   );
+    //   let nav = document.createElement('nav');
+    //   nav.setAttribute('class', 'fd-menu');
+    //   let ul = document.createElement('ul');
+    //   ul.setAttribute('class', 'fd-menu__list fd-menu__list--top');
+    //   for (let i = 0; i < searchResults.length; i++) {
+    //     let li = document.createElement('li');
+    //     li.setAttribute('class', 'fd-menu__item');
+    //     li.setAttribute(
+    //       'style',
+    //       'background-color:green;border:1px solid red;'
+    //     );
+    //     let a = document.createElement('a');
+    //     a.addEventListener('click', e => {
+    //       console.log('e', e);
+    //       searchApiObj.fireItemSelected(searchResults[i]);
+    //     });
+    //     a.setAttribute('class', 'fd-menu__link');
+    //     let span = document.createElement('span');
+    //     span.setAttribute('class', 'fd-menu__title');
+    //     span.innerHTML = searchResults[i].label;
+    //     a.appendChild(span);
+    //     li.appendChild(a);
+    //     ul.appendChild(li);
+    //   }
+    //   nav.appendChild(ul);
+    //   div.appendChild(nav);
+    //   slot.appendChild(div);
+    // },
+    // customSearchResultItemRenderer(searchResultItem, slot, searchApiObj) {
+    //   let a = document.createElement('a');
+    //   a.setAttribute('class', 'fd-menu__link');
+    //   a.setAttribute('style', 'fd-menu__link" style="background-color:yellow;border:1px solid blue;');
+    //   a.addEventListener('click', () => {
+    //     searchApiObj.fireItemSelected(searchResultItem);
+    //   })
+    //   let span = document.createElement('span');
+    //   span.setAttribute('class', "fd-menu__title");
+    //   span.innerHTML = searchResultItem.label;
+    //   a.appendChild(span);
+    //   slot.appendChild(a);
     // },
     onSearchResultItemSelected: searchResultItem => {
       if (searchResultItem.pathObject.externalLink) {
