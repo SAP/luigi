34a0c10c0ca15c7c6009d3bdb8951404da3b3549
JohannesDoberer <johannes.doberer@sap.com>
2020-07-14 11:25:58 +0200

    no message

--- test/e2e-test-application/e2e/test2/1-angular/external-micro-frontend.spec.js
+++ test/e2e-test-application/e2e/test2/1-angular/external-micro-frontend.spec.js
@@ -6,7 +6,7 @@ describe('Navigation', () => {
   describe('Normal navigating', () => {
     it('Check if external micro frontend is available and an alert will be shown.', () => {
       cy.visit('/projects/pr1/externalmf');
-      cy.wait(1000);
+      cy.wait(5000);
       cy.get('[data-testid=luigi-alert]').should(
         'have.class',
         'fd-message-strip--success'
