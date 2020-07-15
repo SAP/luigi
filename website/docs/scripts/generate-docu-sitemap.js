b4430d2ec8118316cf2752d224a24ddaf494a4a7
UlianaMunich <like-me1@yandex.ru>
2020-07-15 15:55:14 +0200

    generate a nicely aligned sitemap.xml for documentation web site

--- website/docs/scripts/generate-docu-sitemap.js
+++ website/docs/scripts/generate-docu-sitemap.js
@@ -3,26 +3,23 @@ var fs = require('fs')
 
 getParsedDocs().then(raw => {
   const docs = JSON.parse(raw);
-  var header = '<?xml version="1.0" encoding="utf-8"?>'
-  + '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">';
+  var header = '<?xml version="1.0" encoding="utf-8"?>\n'
+  + '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">\n';
   var footer = '</urlset>';
   var sitemapString = "";
 
   sitemapString = header;
   for(var i=0; i<docs.length; i++) {
-    sitemapString = sitemapString + '<url>\n'
-    + '<loc>https://docs.luigi-project.io/docs/' + docs[i].shortName + '/</loc>\n'
-    + '<changefreq>monthly</changefreq>\n<priority>1.0</priority>\n'
-    + '</url>\n'
-
-    //console.log("TEST", docs[i].shortName);
+    sitemapString = sitemapString + '\xa0\xa0<url>\n'
+    + '\xa0\xa0\xa0\xa0<loc>https://docs.luigi-project.io/docs/' + docs[i].shortName + '</loc>\n'
+    + '\xa0\xa0\xa0\xa0<changefreq>monthly</changefreq>\n\xa0\xa0\xa0\xa0<priority>1.0</priority>\n'
+    + '\xa0\xa0</url>\n'
   }
   sitemapString = sitemapString + footer;
 
-  fs.writeFile('sitemap123.xml', sitemapString,  function (err,data) {
+  fs.writeFile('sitemap.xml', sitemapString,  function (err,data) {
     if (err) {
       return console.log(err);
     }
-    console.log(data);
   });
 });
