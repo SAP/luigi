b3eab094b48a7196b1624e72c77b52eca5206726
Philipp Pracht <philipp.pracht@sap.com>
2020-07-20 21:12:17 +0200

    added scope to idpmock

--- website/fiddle/public/auth/idpmock/implicit.js
+++ website/fiddle/public/auth/idpmock/implicit.js
@@ -2,18 +2,22 @@ window.onload = function () {
     const loginButton = document.getElementById('login-button');
     const expiresIn = 3600;
     const state = encodeURIComponent(new URLSearchParams(window.location.search).get("state"));
-    const goTo = `/auth/callback.html#
-      expires_in=${expiresIn}&
-      state=${state}&
-      access_token=thisisanaccesstokenthatisnotreallyneeded&
-      id_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.
-      eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjQyMDAiLCJzdWIiOiJtYXNrb3BhdG9sIiwiZXhwIjoxNjQzNzY0OTIwLCJhe
-      nAiOiJtYXNrb3BhdG9sIiwibm9uY2UiOiJidE5rWVZzc1FldVlWNmEyR1RVZm1wWVFFelBRN3c1ZENCbU54SG54IiwiZW
-      1haWwiOiJsdWlnaXVzZXJAa3ltYS5jeCJ9.80GanA3z-Rl67VQAb-lvMNpDZKcVI32CQZIoM9imF5w&
-      token_type=bearer`;
+    const goTo = (scope) => {
+        return `/auth/callback.html#
+          expires_in=${expiresIn}&
+          state=${state}&
+          scope=${scope}&
+          access_token=thisisanaccesstokenthatisnotreallyneeded&
+          id_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.
+          eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjQyMDAiLCJzdWIiOiJtYXNrb3BhdG9sIiwiZXhwIjoxNjQzNzY0OTIwLCJhe
+          nAiOiJtYXNrb3BhdG9sIiwibm9uY2UiOiJidE5rWVZzc1FldVlWNmEyR1RVZm1wWVFFelBRN3c1ZENCbU54SG54IiwiZW
+          1haWwiOiJsdWlnaXVzZXJAa3ltYS5jeCJ9.80GanA3z-Rl67VQAb-lvMNpDZKcVI32CQZIoM9imF5w&
+          token_type=bearer`;
+    }
 
     loginButton.addEventListener('click', function (ev) {
         ev.preventDefault();
-        window.location.replace(goTo.replace(/\s\s+/g, ''));
+        const scope = 'admin';
+        window.location.replace(goTo(scope).replace(/\s\s+/g, ''));
     });
 };
