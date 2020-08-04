e9e4c6b1b26c70ad3dea25a6f37d3ed372198944
Philipp Pracht <philipp.pracht@sap.com>
2020-08-04 16:50:56 +0200

    comments

--- client/src/luigi-element.js
+++ client/src/luigi-element.js
@@ -1,7 +1,6 @@
-export function html(literal) {
-    return literal;
-}
-
+/**
+ * Base class for Luigi web component micro frontends.
+ */
 export class LuigiElement extends HTMLElement {
     constructor() {
         super();        
@@ -11,10 +10,20 @@ export class LuigiElement extends HTMLElement {
         this._shadowRoot.appendChild(template.content.cloneNode(true));
     }
 
+    /**
+     * Query selector operating on shadow root.
+     *
+     * @see ParentNode.querySelector 
+     */
     querySelector(selector) {
         return this._shadowRoot.querySelector(selector);
     }
 
+    /**
+     * Handles changes on the context property.
+     *
+     * @private
+     */
     set context(ctx) {
         if(this.onContextUpdate) {
             this.onContextUpdate(ctx);
@@ -22,9 +31,25 @@ export class LuigiElement extends HTMLElement {
         this.attributeChangedCallback();
     }
 
+    /**
+     * Handles changes on attributes.
+     *
+     * @private
+     */
     attributeChangedCallback(name, oldVal, newVal) {
         if (this.update) {
             this.update();
         }
     }
+}
+
+/**
+ * Html string processing according to luigi functionality.
+ * Also useful in combination with LitElement VS Code plugins.
+ *
+ * @param {String} literal The literal to process.
+ * @returns {String} Returns the processed literal.
+ */
+export function html(literal) {
+    return literal;
 }
