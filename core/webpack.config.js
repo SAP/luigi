752aa673b847ca33268910900c258c13da7826ba
Markus Edenhauser <1720843+maxmarkus@users.noreply.github.com>
2020-08-14 14:42:22 +0200

    remove unlinking

--- core/webpack.config.js
+++ core/webpack.config.js
@@ -32,16 +32,23 @@ class PatchLuigiPlugin {
   apply(compiler) {
     if (compiler.hooks) {
       compiler.hooks.afterEmit.tap('Luigi Patch babel + terser', () => {
-        console.log('\x1b[33mWebpack [' + new Date().toLocaleTimeString() +']: ', '\x1b[0m', 'Post-processing babel and terser...');
+        console.log(
+          '\x1b[33mWebpack [' + new Date().toLocaleTimeString() + ']: ',
+          '\x1b[0m',
+          'Post-processing babel and terser...'
+        );
         execSync(
           [
             `replace '__luigi_dyn_import' 'import' public/luigi.js`,
-            'echo "' + (new Date()) + '" > dev-tools/latest_build.log'
+            'echo "' + new Date() + '" > dev-tools/latest_build.log'
           ].join(' && '),
           PatchLuigiPlugin.execHandler
         );
-        require('fs').unlinkSync('public/luigi.babel.js');
-        console.log('\x1b[33mWebpack [' + new Date().toLocaleTimeString() +']: ', '\x1b[0m', 'Post-processing finished.');
+        console.log(
+          '\x1b[33mWebpack [' + new Date().toLocaleTimeString() + ']: ',
+          '\x1b[0m',
+          'Post-processing finished.'
+        );
       });
     }
   }
