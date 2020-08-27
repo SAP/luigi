0e2b1446531e4d33d60c5a3406203ba1c3439c68
Stanley Hsu <ching.hsiang.hsu@sap.com>
2020-08-27 08:36:45 +0200

    Improve keycode naming

--- core/src/utilities/keycode.js
+++ core/src/utilities/keycode.js
@@ -1,4 +1,4 @@
-export const KEYCODE_UP = 38;
-export const KEYCODE_DOWN = 40;
+export const KEYCODE_ARROW_UP = 38;
+export const KEYCODE_ARROW_DOWN = 40;
 export const KEYCODE_ENTER = 13;
 export const KEYCODE_ESC = 27;
