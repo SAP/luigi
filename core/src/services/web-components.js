38185552f854b773b189139cb7e7fcea8c3fb713
Ndricim Rrapi <ndricim.rrapi@sap.com>
2020-11-10 17:55:12 +0100

    add wc api

--- core/src/services/web-components.js
+++ core/src/services/web-components.js
@@ -4,16 +4,21 @@ class WebComponentSvcClass {
 
   dynamicImport(viewUrl) {
     /** __luigi_dyn_import() is replaced by import() after webpack is done,
-         *    because webpack can't let his hands off imports ;) */
+     *    because webpack can't let his hands off imports ;) */
     return __luigi_dyn_import(viewUrl);
   }
 
   /** Creates a web component with tagname wc_id and adds it to wcItemContainer, if attached to wc_container*/
   attachWC(wc_id, wcItemContainer, wc_container, ctx) {
-    if(wc_container && wc_container.contains(wcItemContainer)) {
+    if (wc_container && wc_container.contains(wcItemContainer)) {
       const wc = document.createElement(wc_id);
       wc.context = ctx;
       wc.luigi = window.Luigi;
+      const clientAPI = {
+        linkManager: window.Luigi.navigation,
+        uxManager: window.Luigi.ux
+      };
+      wc.LuigiClient = clientAPI;
       wcItemContainer.appendChild(wc);
     }
   }
@@ -24,7 +29,7 @@ class WebComponentSvcClass {
    */
   generateWCId(viewUrl) {
     let charRep = '';
-    for(let i = 0; i < viewUrl.length; i++) {
+    for (let i = 0; i < viewUrl.length; i++) {
       charRep += viewUrl.charCodeAt(i).toString(16);
     }
     return 'luigi-wc-' + charRep;
@@ -35,14 +40,16 @@ class WebComponentSvcClass {
    * returns a promise that gets resolved after successfull import */
   registerWCFromUrl(viewUrl, wc_id) {
     return new Promise((resolve, reject) => {
-      this.dynamicImport(viewUrl).then(module => {
-        try {
-          window.customElements.define(wc_id, module.default);
-          resolve();
-        } catch(e) {
-          reject(e);
-        }
-      }).catch(err => reject(err));
+      this.dynamicImport(viewUrl)
+        .then(module => {
+          try {
+            window.customElements.define(wc_id, module.default);
+            resolve();
+          } catch (e) {
+            reject(e);
+          }
+        })
+        .catch(err => reject(err));
     });
   }
 
@@ -58,7 +65,7 @@ class WebComponentSvcClass {
       this.attachWC(wc_id, wcItemCnt, wc_container, context);
     } else {
       /** Custom import function, if defined */
-      if(window.luigiWCFn) {
+      if (window.luigiWCFn) {
         window.luigiWCFn(viewUrl, wc_id, wcItemCnt, () => {
           this.attachWC(wc_id, wcItemCnt, wc_container, context);
         });
