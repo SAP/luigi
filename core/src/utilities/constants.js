186470d608432c9d2f08f94fdb8bf09564b4c90b
JohannesDoberer <johannes.doberer@sap.com>
2020-11-16 10:23:53 +0100

    Drawer component (#1659)

--- core/src/utilities/constants.js
+++ core/src/utilities/constants.js
@@ -5,7 +5,8 @@ export const CSS_BREAKPOINTS = {
 export const MICROFRONTEND_TYPES = [
   { type: 'main', selector: '.iframeContainer iframe' },
   { type: 'split-view', selector: '.iframeSplitViewCnt iframe' },
-  { type: 'modal', selector: '.iframeModalCtn iframe' }
+  { type: 'modal', selector: '.iframeModalCtn._modal iframe' },
+  { type: 'drawer', selector: '.iframeModalCtn._drawer iframe' },
 ];
 
 export const CUSTOM_LUIGI_CONTAINER = {
