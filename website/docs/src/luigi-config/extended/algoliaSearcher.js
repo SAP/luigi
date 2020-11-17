c8e85c82fb31112aafb967a8bccd02fe45e95493
legteodav <davide.teodoro.leggieri@sap.com>
2020-11-17 15:08:39 +0100

    Change Search using Luigi.globalSearch() API (#1690)
    * Change Search using Luigi.globalSearch() API
    
    * Adding Tag clickable + search using Luigi Message
    
    * Adding @legteodav in CODEOWNERS
    
    * Cleaning a bit more the code (console, whitespace)
    
    * Fixing overlay problem
    Removing white spaces
    Fixing highlight html for some key search terms
    Removing search.js
    
    * Removing useless files
    Improving code quality (white spaces, type errors, etc etc)
    
    * More Cleaning code
    
    * fixing css problems
    
    * Integrating code review changes
    
    * Fix Css problem for Mobile application
    
    * Fix Css problem for Arrow (it was not rotating...)

---
+++ website/docs/src/luigi-config/extended/algoliaSearcher.js
@@ -0,0 +1,57 @@
+import algoliasearch from "algoliasearch";
+const appId="BH4D9OD16A";
+const apiKey="5ab04e0673d89f07c964afcf1522ad3a";
+
+class AlgoliaSearcher{
+  constructor() {
+    this.client = algoliasearch(appId, apiKey);
+    this.index = this.client.initIndex("luigi-project");
+    this.searchResult = 10
+    this.isDevelop = parseInt(window.location.port) === 4000;
+    this.coreBaseUrl = window.location.origin;
+  }
+  executeSearch(query){
+    this.index.search(query, {hitsPerPage:  this.searchResult })
+      .then(({ hits }) => {
+        hits = hits.map(this.transformUrls.bind(this)).map(this.transformContent)
+        Luigi.globalSearch().showSearchResult([query].concat(hits));
+      })
+      .catch(err => {
+        console.log('Error in executing the query '+ err);
+      });
+  }
+  transformUrls(hit){
+    if (this.isDevelop) {
+      hit.url = hit.url.replace('https://docs.luigi-project.io', '');
+    }
+    hit.url = hit.url.replace('/docu-microfrontend', '');
+    return hit;
+  }
+  transformContent(hit) {
+    let url = hit.url;
+    let params = {};
+    if (url.indexOf("#") != -1) {
+      params = {
+        section: url.substring(url.indexOf("#") + 1)
+      }
+      url = url.substring(0, url.indexOf("#"))
+    }
+    const title1 = hit.hierarchy['lvl0'];
+    const title2 = hit.hierarchy['lvl1'] || hit.hierarchy['lvl0'];
+    const title3 = hit.hierarchy['lvl2'] || hit.hierarchy['lvl0'];
+    return {
+      pathObject: {
+        link: url,
+        params
+      },
+      label: hit.hierarchy['lvl0'],
+      description: hit.content,
+      title1,
+      title2,
+      title3,
+      hit
+    }
+  }
+}
+
+export const algoliaSearcher = new AlgoliaSearcher();
---
+++ website/docs/src/luigi-config/extended/resultRender.js
@@ -0,0 +1,177 @@
+import Hogan from 'hogan.js';
+
+const suggestionTemplate = `
+       <div class="ds-suggestion" role="option" id="option-27550019" aria-selected="true">
+        <a class="algolia-docsearch-suggestion algolia-docsearch-suggestion__main algolia-docsearch-suggestion__secondary" style="white-space: normal;">
+          <div class="algolia-docsearch-suggestion--category-header">
+              <span class="algolia-docsearch-suggestion--category-header-lvl0">
+               {{{title1}}}
+              </span>
+          </div>
+          <div class="algolia-docsearch-suggestion--wrapper">
+            <div class="algolia-docsearch-suggestion--subcategory-column">
+              <span class="algolia-docsearch-suggestion--subcategory-column-text">{{{title2}}}</span>
+            </div>
+            <div class="algolia-docsearch-suggestion--content">
+              <div class='algolia-docsearch-suggestion--subcategory-inline'>{{{title2}}}</div>
+              <div class="algolia-docsearch-suggestion--title">{{{title3}}}</div>
+              <div class="algolia-docsearch-suggestion--text">{{{description}}}</div>
+            </div>
+          </div>
+        </a>
+    </div>
+        `;
+
+const empty =`
+  <div class="algolia-docsearch">
+    <div class="algolia-docsearch--wrapper">
+        <div class="algolia-docsearch--content algolia-docsearch--no-results">
+            <div class="algolia-docsearch--title">
+                <div class="algolia-docsearch--text">
+                    No results found for query <b>"{{query}}"</b>
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
+  `;
+
+export default class ResultRender {
+  constructor(query, results) {
+    this.query = query;
+    this.results = results;
+  }
+  buildDomResults(){
+    this.cleanResults();
+    const { resultSpan, container } = this.buildContainer();
+    if (this.results.length === 0) {
+      this.domEmpty(resultSpan);
+    }else{
+      this.domResults(resultSpan);
+    }
+    return container;
+  }
+  cleanResults(){
+    let elem = document.querySelector('span.algolia-autocomplete');
+    if (elem){
+      elem.remove();
+    }
+  }
+  domResults(span){
+    let template = Hogan.compile(suggestionTemplate);
+    for (let result of this.results){
+      let data = this.dataTemplate(result);
+      let html = this.renderResult(template, data);
+      let searchItem = this.htmlToElement(html);
+      this.attachItemEvents(searchItem, result);
+      span.appendChild(searchItem);
+    }
+  }
+  domEmpty(span){
+    let template = Hogan.compile(empty);
+    let data = {query: this.query};
+    let output = template.render(data);
+    let searchItem = this.htmlToElement(output);
+    span.appendChild(searchItem);
+  }
+  shortText(text){
+    let short = text;
+    if (short.length > 80){
+      let lastPost = short.substring(80).indexOf(" ") + 80;
+      short = short.substring(0, lastPost)+".."
+    }
+    return this.highlightKeyword(short);
+  }
+  dataTemplate(result){
+    return {
+      label: this.shortText(result.label),
+      description: this.shortText(result.description),
+      title1: this.shortText(result.title1),
+      title2: this.shortText(result.title2),
+      title3: this.shortText(result.title3),
+    };
+  }
+  renderResult(template, data){
+    return template.render(data);
+  }
+  highlightKeyword(text){
+    this.perm(this.query).forEach(subQ => {
+      let replace = '<span class="algolia-docsearch-suggestion--highlight">'+ subQ+ '</span>';
+      text = text.replaceAll(subQ, replace);
+    })
+    return text;
+  }
+  attachItemEvents(searchItem, result){
+    searchItem.addEventListener('click', e => {
+      e.preventDefault();
+      Luigi.navigation().withParams(result.pathObject.params).navigate(result.pathObject.link);
+      Luigi.globalSearch().closeSearchResult();
+      Luigi.globalSearch().clearSearchField();
+    });
+    searchItem.addEventListener('mouseover', e => {
+      e.preventDefault();
+      searchItem.set
+      searchItem.classList.add("ds-cursor");
+
+    });
+    searchItem.addEventListener('mouseout', e => {
+      e.preventDefault();
+      searchItem.classList.remove("ds-cursor");
+    });
+  }
+  buildContainer(){
+    let span = this.createAttribute('span', {
+      "class": "algolia-autocomplete algolia-autocomplete-right",
+      "style": "position: relative; display: inline-block; direction: ltr;"});
+    let span_ = this.createAttribute('span', {
+      "class": "ds-dropdown-menu ds-with-1",
+      "role": "listbox",
+      "id" : "algolia-autocomplete-listbox-0",
+      "style": "position: absolute; top: 100%; z-index: 100; left: 0px; right: auto; display: block;"
+    });
+    let div_1 = this.createAttribute('div', {"class":"ds-dataset-1"});
+    let span_11 = this.createAttribute('span', {"class": "ds-suggestions","style": "display: block;"});
+    span.appendChild(span_);
+    span_.appendChild(div_1);
+    div_1.appendChild(span_11);
+    return {
+      container: span,
+      resultSpan : span_11
+    };
+  }
+  createAttribute(type, attributes){
+    let elem =  document.createElement(type);
+    if (!attributes){
+      return elem;
+    }
+    Object.keys(attributes).forEach(key => elem.setAttribute(key, attributes[key]))
+    return elem;
+  }
+  htmlToElement(html) {
+    return new DOMParser().parseFromString(html, "text/html").body.firstChild;
+  }
+  perm(str){
+    let results = [];
+    let arr = str.split("");
+    let len = Math.pow(arr.length, 2);
+
+    for( let i = 0; i < len; i++ ){
+      for( let k= 0, j = i; k < arr.length; k++, j >>=1){
+        arr[k] = ( j & 1 ) ? arr[k].toUpperCase() : arr[k].toLowerCase();
+      }
+      let combo = arr.join("");
+      results.push(combo);
+    }
+    return results;
+  }
+  createAttribute(type, attributes){
+    let elem =  document.createElement(type);
+    if (!attributes){
+      return elem;
+    }
+    Object.keys(attributes).forEach(key => elem.setAttribute(key, attributes[key]))
+    return elem;
+  }
+}
+
+
