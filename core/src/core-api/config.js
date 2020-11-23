e332828919fdadeb25c444bce2c23cf45385fcf6
JohannesDoberer <johannes.doberer@sap.com>
2020-11-23 19:06:35 +0100

    Update core/src/core-api/config.js
    Co-authored-by: Aleksandra Simeonova <aleksandra.simeonova@sap.com>

--- core/src/core-api/config.js
+++ core/src/core-api/config.js
@@ -237,7 +237,7 @@ class LuigiConfig {
    * Reads the user settings object from the **localStorage**. It is possible to choose a custom storage type by implementing the `settings.userSetting.readFromCustomStorage` function.
    * @memberof Configuration
    * @param {string} key used to identify the item in the storage.
-   * @param {string} property the object traversal path where `readFromCustomStorage` function is implemented. Optional and only needed if there is a custom implementation of `readFromCustomStorage` function in the settings section of the luigi config to read a custom storage.
+   * @param {string} property the object traversal path where the `readFromCustomStorage` function is implemented. Optional and only needed if there is a custom implementation of a `readFromCustomStorage` function in the settings section of the Luigi configuration to read a custom storage.
    * @example
    * Luigi.writeUserSettings('key');
    * Luigi.writeUserSettings('key', 'settings.userSettings');
