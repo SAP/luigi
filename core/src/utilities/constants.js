3c232db2f13468f1ac48a5018beadaf7ade71da2
JohannesDoberer <johannes.doberer@sap.com>
2020-12-02 19:34:57 +0100

    fix tests

--- core/src/utilities/constants.js
+++ core/src/utilities/constants.js
@@ -6,8 +6,7 @@ export const MICROFRONTEND_TYPES = [
   { type: 'main', selector: '.iframeContainer iframe' },
   { type: 'split-view', selector: '.iframeSplitViewCnt iframe' },
   { type: 'modal', selector: '.iframeModalCtn._modal iframe' },
-  { type: 'drawer', selector: '.iframeModalCtn._drawer iframe' },
-  { type: 'user-settings', selector: '.iframeDialogCtn iframe' },
+  { type: 'drawer', selector: '.iframeModalCtn._drawer iframe' }
 ];
 
 export const CUSTOM_LUIGI_CONTAINER = {
