bb069b3c7a5e2ccf0c6e3424d11c4caca0a7d47d
Philipp Pracht <philipp.pracht@sap.com>
2020-12-02 14:48:51 +0100

    Feature wc support (#1751)

--- core/src/utilities/helpers/storage-helper.js
+++ core/src/utilities/helpers/storage-helper.js
@@ -2,52 +2,56 @@ import { IframeHelpers } from './iframe-helpers';
 
 class StorageHelperClass {
   constructor() {
-    this.init=false;
-    this.storage=undefined;
-    this.browseSupported=undefined;
+    this.init = false;
+    this.storage = undefined;
+    this.browseSupported = undefined;
   }
 
-  checkInit(){
-    if (this.init){
+  checkInit() {
+    if (this.init) {
       return;
     }
-    this.storage = window.localStorage
+    this.storage = window.localStorage;
     this.browseSupported = this.supportLocalStorage();
-    this.init=true;
+    this.init = true;
   }
 
-  supportLocalStorage(){
+  supportLocalStorage() {
     try {
       return 'localStorage' in window && window['localStorage'] !== null;
-    } catch(e) {
+    } catch (e) {
       return false;
     }
   }
 
-  checkStorageBrowserSupport(){
-    if (!this.browseSupported){
-      throw "Browser does not support local storage"
+  checkStorageBrowserSupport() {
+    if (!this.browseSupported) {
+      throw 'Browser does not support local storage';
     }
   }
 
-  process(microfrontendId, hostname, id, operation, params){
-    try{
-        this.checkInit();
-        this.checkStorageBrowserSupport();
-        const operationFunction = this[operation];
-        if (typeof operationFunction !== 'function'){
-          throw operation + " is not a supported operation for the storage";
-        }
-        const result = operationFunction.bind(this, this.cleanHostname(hostname), params)();
-        this.sendBackOperation(microfrontendId, id, 'OK', result);
-      }catch(error){
-        console.log(error);
-        this.sendBackOperation(microfrontendId, id, 'ERROR', error);
+  process(microfrontendId, hostname, id, operation, params) {
+    try {
+      this.checkInit();
+      this.checkStorageBrowserSupport();
+      const operationFunction = this[operation];
+      if (typeof operationFunction !== 'function') {
+        throw operation + ' is not a supported operation for the storage';
       }
+      const result = operationFunction.bind(
+        this,
+        this.cleanHostname(hostname),
+        params
+      )();
+      this.sendBackOperation(microfrontendId, id, 'OK', result);
+    } catch (error) {
+      console.log(error);
+      this.sendBackOperation(microfrontendId, id, 'ERROR', error);
+    }
   }
 
-  cleanHostname(hostname){
-    return hostname.replace('http://','').replace("https://",'');
+  cleanHostname(hostname) {
+    return hostname.replace('http://', '').replace('https://', '');
   }
 
   setItem(hostname, params) {
@@ -57,69 +61,69 @@ class StorageHelperClass {
     this.storage.setItem(key, value);
   }
 
-  getItem(hostname, params){
+  getItem(hostname, params) {
     this.checkKey(params);
     const key = this.buildKey(hostname, params.key);
     const item = this.storage.getItem(key);
-    if (item){
+    if (item) {
       return this.parseJsonIfPossible(item);
-    }else{
+    } else {
       return undefined;
     }
   }
 
-  buildKey(hostname, subKey){
-    return  this.buildPrefix(hostname) + subKey.trim();
+  buildKey(hostname, subKey) {
+    return this.buildPrefix(hostname) + subKey.trim();
   }
 
-  buildPrefix(hostname){
-    return "Luigi#"+ hostname + "#";
+  buildPrefix(hostname) {
+    return 'Luigi#' + hostname + '#';
   }
 
-  removeItem(hostname, params){
+  removeItem(hostname, params) {
     this.checkKey(params);
     const key = this.buildKey(hostname, params.key);
     const item = this.storage.getItem(key);
-    if (item){
+    if (item) {
       this.storage.removeItem(key);
       return item;
-    }else{
+    } else {
       return undefined;
     }
   }
 
   clear(hostname, params) {
-    const keyPrefix =  this.buildPrefix(hostname);
+    const keyPrefix = this.buildPrefix(hostname);
     Object.keys(this.storage)
       .filter(key => key.startsWith(keyPrefix))
       .forEach(key => this.storage.removeItem(key));
   }
 
-  has(hostname, params){
+  has(hostname, params) {
     this.checkKey(params);
     const key = this.buildKey(hostname, params.key);
     const item = this.storage.getItem(key);
-    if (item){
+    if (item) {
       return true;
-    }else{
+    } else {
       return false;
     }
   }
 
-  getAllKeys(hostname, params){
-    const keyPrefix =  this.buildPrefix(hostname);
+  getAllKeys(hostname, params) {
+    const keyPrefix = this.buildPrefix(hostname);
     return Object.keys(this.storage)
       .filter(key => key.startsWith(keyPrefix))
-      .map(key=> key.substring(keyPrefix.length));
+      .map(key => key.substring(keyPrefix.length));
   }
 
-  checkKey(params){
-    if (!params.key || params.key.trim().length === 0){
-      throw "Missing key, we cannot execute storage operation";
+  checkKey(params) {
+    if (!params.key || params.key.trim().length === 0) {
+      throw 'Missing key, we cannot execute storage operation';
     }
   }
 
-  parseJsonIfPossible(text){
+  parseJsonIfPossible(text) {
     try {
       return JSON.parse(text);
     } catch (e) {
@@ -127,21 +131,21 @@ class StorageHelperClass {
     }
   }
 
-  stringifyValue(value){
-    if (!value){
-      throw "Value is empty";
+  stringifyValue(value) {
+    if (!value) {
+      throw 'Value is empty';
     }
-    if (typeof value === 'string' || value instanceof String){
+    if (typeof value === 'string' || value instanceof String) {
       return value;
     }
     try {
       return JSON.stringify(value);
     } catch (error) {
-      throw "Value cannot be stringify, error: "+error;
+      throw 'Value cannot be stringify, error: ' + error;
     }
   }
 
-  sendBackOperation(microfrontendId, id, status, result){
+  sendBackOperation(microfrontendId, id, status, result) {
     let message = {
       msg: 'storage',
       data: {
@@ -149,11 +153,13 @@ class StorageHelperClass {
         status,
         result
       }
-    }
+    };
     IframeHelpers.getMicrofrontendsInDom()
       .filter(microfrontendObj => microfrontendObj.id === microfrontendId)
       .map(microfrontendObj => microfrontendObj.container)
-      .map(mfContainer => IframeHelpers.sendMessageToIframe(mfContainer,message ));
+      .map(mfContainer =>
+        IframeHelpers.sendMessageToIframe(mfContainer, message)
+      );
   }
 }
 
--- core/test/utilities/helpers/storage-helpers.spec.js
+++ core/test/utilities/helpers/storage-helpers.spec.js
@@ -2,42 +2,64 @@ const chai = require('chai');
 const assert = chai.assert;
 const sinon = require('sinon');
 import { IframeHelpers, StorageHelper } from '../../../src/utilities/helpers';
-import 'mock-local-storage'
-
+import 'mock-local-storage';
 
 describe('Storage-helpers', () => {
   describe('process', () => {
-    let microfrontendId='mockMicroId';
-    let hostname= 'luigi.core.test';
+    let microfrontendId = 'mockMicroId';
+    let hostname = 'luigi.core.test';
     let key = 'key_';
-    let value = "value_";
+    let value = 'value_';
     let id = 'messageId_';
     let sendBackOperationSpy;
     let sendMessageToIframeSpy;
-    const buildLuigiKey = () =>{
-      return "Luigi#"+hostname + "#"+key;
-    }
+    const buildLuigiKey = () => {
+      return 'Luigi#' + hostname + '#' + key;
+    };
     const assertSendMessage = (status, result) => {
       assert(sendBackOperationSpy.calledOnce);
-      let args = sendBackOperationSpy.getCalls()[0].args
+      let args = sendBackOperationSpy.getCalls()[0].args;
       assert(sendBackOperationSpy.calledOnce);
-      assert.equal(args[0], microfrontendId, "sendBackOperation argument microfrontendId is different from  expected");
-      assert.equal(args[1], id, "sendBackOperation argument id is different from  expected");
-      assert.equal(args[2], status, "sendBackOperation argument status is different from  expected");
-      if (!result){
-        assert.isTrue(!args[3], "sendBackOperation argument result shuld be undefined for this operation");
-        return ;
-      }
-      if (Array.isArray(result)){
-        assert.deepEqual(args[3], result, "sendBackOperation argument result is different from  expected");
+      assert.equal(
+        args[0],
+        microfrontendId,
+        'sendBackOperation argument microfrontendId is different from  expected'
+      );
+      assert.equal(
+        args[1],
+        id,
+        'sendBackOperation argument id is different from  expected'
+      );
+      assert.equal(
+        args[2],
+        status,
+        'sendBackOperation argument status is different from  expected'
+      );
+      if (!result) {
+        assert.isTrue(
+          !args[3],
+          'sendBackOperation argument result shuld be undefined for this operation'
+        );
         return;
       }
-      assert.equal(args[3], result, "sendBackOperation argument result is different from  expected");
-    }
+      if (Array.isArray(result)) {
+        assert.deepEqual(
+          args[3],
+          result,
+          'sendBackOperation argument result is different from  expected'
+        );
+        return;
+      }
+      assert.equal(
+        args[3],
+        result,
+        'sendBackOperation argument result is different from  expected'
+      );
+    };
 
     const assertsendMessageToIframe = () => {
       assert(sendMessageToIframeSpy.calledOnce);
-    }
+    };
 
     beforeEach(() => {
       key = 'key_' + Math.random();
@@ -45,7 +67,9 @@ describe('Storage-helpers', () => {
       id = 'messageId_' + Math.random();
       sendBackOperationSpy = sinon.spy(StorageHelper, 'sendBackOperation');
       sendMessageToIframeSpy = sinon.spy(IframeHelpers, 'sendMessageToIframe');
-      sinon.stub(IframeHelpers, "getMicrofrontendsInDom").callsFake(() => [{ id: microfrontendId, container: {} }]);
+      sinon
+        .stub(IframeHelpers, 'getMicrofrontendsInDom')
+        .callsFake(() => [{ id: microfrontendId, container: {} }]);
       window.localStorage.clear();
     });
 
@@ -54,76 +78,88 @@ describe('Storage-helpers', () => {
     });
 
     it('setItem', () => {
-      StorageHelper.process(microfrontendId, hostname, id, 'setItem', { key, value })
+      StorageHelper.process(microfrontendId, hostname, id, 'setItem', {
+        key,
+        value
+      });
       let luigiKey = buildLuigiKey();
-      assert.equal(window.localStorage.getItem(luigiKey), value, "Luigi value is different for setItem");
-      assertSendMessage("OK", undefined);
+      assert.equal(
+        window.localStorage.getItem(luigiKey),
+        value,
+        'Luigi value is different for setItem'
+      );
+      assertSendMessage('OK', undefined);
       assertsendMessageToIframe();
     });
 
     it('getItem', () => {
       let luigiKey = buildLuigiKey();
       window.localStorage.setItem(luigiKey, value);
-      StorageHelper.process(microfrontendId, hostname, id, 'getItem', { key })
-      assertSendMessage("OK", value);
+      StorageHelper.process(microfrontendId, hostname, id, 'getItem', { key });
+      assertSendMessage('OK', value);
       assertsendMessageToIframe();
     });
 
     it('getItem no value', () => {
-      StorageHelper.process(microfrontendId, hostname, id, 'getItem', { key })
-      assertSendMessage("OK", undefined);
+      StorageHelper.process(microfrontendId, hostname, id, 'getItem', { key });
+      assertSendMessage('OK', undefined);
       assertsendMessageToIframe();
     });
 
     it('has', () => {
       let luigiKey = buildLuigiKey();
       window.localStorage.setItem(luigiKey, value);
-      StorageHelper.process(microfrontendId, hostname, id, 'has', { key })
-      assertSendMessage("OK", true);
+      StorageHelper.process(microfrontendId, hostname, id, 'has', { key });
+      assertSendMessage('OK', true);
       assertsendMessageToIframe();
     });
 
     it('has no value', () => {
-      StorageHelper.process(microfrontendId, hostname, id, 'has', { key })
-      assertSendMessage("OK", false);
+      StorageHelper.process(microfrontendId, hostname, id, 'has', { key });
+      assertSendMessage('OK', false);
       assertsendMessageToIframe();
     });
-
 
     it('clear', () => {
       let luigiKey = buildLuigiKey();
       window.localStorage.setItem(luigiKey, value);
-      StorageHelper.process(microfrontendId, hostname, id, 'clear', { })
-      assert.isTrue(!window.localStorage.getItem(luigiKey), "After clear, item should not be present");
-      assertSendMessage("OK", undefined);
+      StorageHelper.process(microfrontendId, hostname, id, 'clear', {});
+      assert.isTrue(
+        !window.localStorage.getItem(luigiKey),
+        'After clear, item should not be present'
+      );
+      assertSendMessage('OK', undefined);
       assertsendMessageToIframe();
     });
 
     it('removeItem', () => {
       let luigiKey = buildLuigiKey();
       window.localStorage.setItem(luigiKey, value);
-      StorageHelper.process(microfrontendId, hostname, id, 'removeItem', { key })
-      assert.isTrue(!window.localStorage.getItem(luigiKey), "After removed, item should not be present");
-      assertSendMessage("OK", value);
+      StorageHelper.process(microfrontendId, hostname, id, 'removeItem', {
+        key
+      });
+      assert.isTrue(
+        !window.localStorage.getItem(luigiKey),
+        'After removed, item should not be present'
+      );
+      assertSendMessage('OK', value);
       assertsendMessageToIframe();
     });
 
     it('removeItem no value', () => {
-      StorageHelper.process(microfrontendId, hostname, id, 'removeItem', { key })
-      assertSendMessage("OK", undefined);
+      StorageHelper.process(microfrontendId, hostname, id, 'removeItem', {
+        key
+      });
+      assertSendMessage('OK', undefined);
       assertsendMessageToIframe();
     });
 
     it('getAllKeys', () => {
       let luigiKey = buildLuigiKey();
       window.localStorage.setItem(luigiKey, value);
-      StorageHelper.process(microfrontendId, hostname, id, 'getAllKeys', {  })
-      assertSendMessage("OK", [key]);
+      StorageHelper.process(microfrontendId, hostname, id, 'getAllKeys', {});
+      assertSendMessage('OK', [key]);
       assertsendMessageToIframe();
     });
-
   });
-
 });
-
-
