47ff3a536714116605dd10e2ca188a3c3dfc4d03
UlianaMunich <like-me1@yandex.ru>
2021-01-07 15:17:12 +0100

    revert changes in angular.sh to leave only vue.sh PoC

--- scripts/setup/angular.sh
+++ scripts/setup/angular.sh
@@ -13,7 +13,19 @@ fi
 echo ""
 ng new $folder --routing && cd $folder
 
-npm i -P @luigi-project/core @luigi-project/client fundamental-styles@0.11.0 @sap-theming/theming-base-content webpack webpack-cli @babel/core @babel/preset-env babel-loader 
+npm i -P @luigi-project/core @luigi-project/client fundamental-styles@0.11.0 @sap-theming/theming-base-content webpack@4.43.0 webpack-cli@3.3.12 @babel/core @babel/preset-env babel-loader 
+sed 's/"scripts": {/"scripts": {\
+\   "buildConfig":"webpack --entry .\/src\/luigi-config\/luigi-config.es6.js --output-path .\/src\/assets --output-filename luigi-config.js --mode production",/1' package.json > p.tmp.json && mv p.tmp.json package.json
+mkdir -p src/luigi-config
+
+ # the following steps can be copy and pasted to the terminal at once
+mv src/index.html src/angular.html
+
+# download assets
+curl https://raw.githubusercontent.com/SAP/luigi/master/scripts/setup/assets/index.html > src/index.html
+curl https://raw.githubusercontent.com/SAP/luigi/master/scripts/setup/assets/luigi-config.es6.js > src/luigi-config/luigi-config.es6.js
+curl https://raw.githubusercontent.com/SAP/luigi/master/scripts/setup/assets/basicMicroFrontend.html > src/assets/basicMicroFrontend.html
+
 # string replacements in some files
 sed 's#"src/index.html"#"src/angular.html"#g' angular.json > tmp.json && mv tmp.json angular.json
 
@@ -26,31 +38,6 @@ sed 's#"src/assets"#"src/assets",\
               {"glob": "*","input": "node_modules/@sap-theming/theming-base-content","output": "/fonts"},\
               {"glob": "**","input": "node_modules/@luigi-project/core","output": "/luigi-core"},\
               {"glob": "luigi-client.js","input": "node_modules/@luigi-project/client","output": "/luigi-client"}#g' angular.json > tmp.json && mv tmp.json angular.json
-
-echo "const path = require('path');
-module.exports = {
-    entry: './src/luigi-config/luigi-config.es6.js',
-    output: {
-        filename: 'luigi-config.js',
-        path: path.resolve(__dirname, 'public'),
-    },
-};">webpack.config.js
-
-sed 's/"scripts": {/"scripts": {\
-\    "buildConfig":"webpack --config webpack.config.js",/1' package.json > p.tmp.json && mv p.tmp.json package.json
-
-echo '{
-    "globals": {
-        "Luigi": "readonly"
-    }
-}'>.eslintrc.json
-
-# download assets
-
-mkdir -p src/luigi-config
-curl https://raw.githubusercontent.com/SAP/luigi/master/core/examples/luigi-example-angular/src/index.html > src/index.html
-curl https://raw.githubusercontent.com/SAP/luigi/master/scripts/setup/assets/luigi-config.es6.js > src/luigi-config/luigi-config.es6.js
-curl https://raw.githubusercontent.com/SAP/luigi/master/scripts/setup/assets/basicMicroFrontend.html > src/assets/basicMicroFrontend.html
 
 npm run buildConfig
 npm run start
