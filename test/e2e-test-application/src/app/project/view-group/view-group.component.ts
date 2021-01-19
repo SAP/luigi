4142accc7e2d912667b4dfd72ac53ba181505c82
Leggieri, Davide Teodoro <davide.teodoro.leggieri@sap.com>
2021-01-19 09:31:51 +0100

    Adding example of view group

---
+++ test/e2e-test-application/src/app/project/view-group/view-group.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-view-group',
+  templateUrl: './view-group.component.html',
+  styleUrls: ['./view-group.component.css']
+})
+export class ViewGroupComponent implements OnInit {
+
+  vg: string;
+  id: string;
+  code:string;
+  config: string;
+  time: string;
+
+  constructor(
+    private route:ActivatedRoute,
+    private window: Window
+  ) { }
+
+  ngOnInit() {
+    let iframes = Array.from(this.window.parent.document.querySelectorAll('iframe'));
+    if (!iframes || !iframes[0] || !iframes[0]['luigi']){
+      return;
+    }
+
+    console.log("this.window.parent.Luigi --> ",this.window.parent['Luigi'].config);
+
+    this.config = JSON.stringify({
+      viewGroupSettings: this.window.parent['Luigi'].config.navigation.viewGroupSettings,
+      preloadViewGroups: this.window.parent['Luigi'].config.navigation.preloadViewGroups
+    }, null, 3);
+
+    // this.id = iframes[0]['luigi'].id;
+    // let luigiMf = iframes[0]['luigi'];
+    // const clonedLuigiMf = Object.assign({}, luigiMf);
+    // clonedLuigiMf.currentNode =  Object.assign({}, luigiMf.currentNode);
+    // delete clonedLuigiMf.currentNode.parent;
+
+    this.route.paramMap.subscribe(queryParams => {
+      this.vg = queryParams.get("vg");
+      this.time = new Date().toJSON();
+
+      let iframes = Array.from(
+        this.window.parent.document.querySelectorAll('iframe')
+      ).map(this.transformIframes);
+
+      this.code = JSON.stringify(iframes,null,3);
+      let currentIFrame = iframes.find(iframe =>
+        !!iframe.currentNode && !!iframe.currentNode.pathSegment && iframe.currentNode.pathSegment === this.vg);
+
+      if (!!currentIFrame){
+        this.id = currentIFrame['id'];
+      }
+
+    });
+  }
+
+
+  transformIframes(iframe){
+
+    let luigiMf = iframe['luigi'];
+    const clonedLuigiMf = Object.assign({}, luigiMf);
+    clonedLuigiMf.currentNode =  Object.assign({}, luigiMf.currentNode);
+    delete clonedLuigiMf.currentNode.parent;
+
+    return clonedLuigiMf;
+  }
+}
