2f8ffdd57757719cd4067bdbc01c8dbc30e9709e
Ndricim Rrapi <ndricim.rrapi@sap.com>
2021-03-02 15:45:01 +0100

    Small styling space 2

--- scripts/change.node.version.js
+++ scripts/change.node.version.js
@@ -31,7 +31,7 @@ const executeModule = async (module) => {
     const pathArray = [path.resolve(__dirname),'..'].concat(module.split('/'));
     const modulePath =  path.join(...pathArray);
     const moduleDependencies =  path.join(modulePath, 'node_modules');
-    console.log("Deleting directory "+moduleDependencies);
+    console.log("Deleting directory " + moduleDependencies);
     fs.rmdirSync(moduleDependencies, { recursive: true });
 
     process.chdir( modulePath )
