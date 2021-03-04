27de29807591c23059c93ea4496b6283bbbd8adc
Ndricim Rrapi <ndricim.rrapi@sap.com>
2021-03-04 09:34:10 +0100

    remove extra scenario code

--- core/examples/luigi-example-angular/src/app/sample1/sample1.component.ts
+++ core/examples/luigi-example-angular/src/app/sample1/sample1.component.ts
@@ -1,30 +1,9 @@
 import { Component } from '@angular/core';
-import {
-  isLuigiClientInitialized,
-  luigiClientInit,
-} from '@luigi-project/client';
-
 
 @Component({
   selector: 'app-sample1',
   templateUrl: './sample1.component.html'
 })
 export class Sample1Component {
-  constructor() {
-    console.log("constructor");
-  }
-
-  ngOnInit() {
-    console.log("initialized value=", isLuigiClientInitialized());
-
-    setTimeout(() => { 
-      luigiClientInit();
-
-      setTimeout(() => { 
-        console.log("post value", isLuigiClientInitialized());
-      }, 1000);
-
-    }, 3000);
-  }
-  
+  constructor() {}
 }
