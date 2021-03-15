a0beb9b57549c118f80a6a1420b2e120f91d3448
Stanley Hsu <ching.hsiang.hsu@sap.com>
2021-03-15 11:06:46 +0100

    Global side nav (experimental)

--- core/src/utilities/helpers/generic-helpers.js
+++ core/src/utilities/helpers/generic-helpers.js
@@ -330,8 +330,8 @@ class GenericHelpersClass {
    * @returns true, if feature enabled, false otherwise.
    */
   requestExperimentalFeature(expFeatureName, showWarn) {
-    var val = LuigiConfig.getConfigValue(
-      'settings.experimental.' + expFeatureName
+    const val = Boolean(
+      LuigiConfig.getConfigValue('settings.experimental.' + expFeatureName)
     );
     if (showWarn && !val) {
       console.warn('Experimental feature not enabled: ', expFeatureName);
