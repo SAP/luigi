d43fc8e770126f70a7a432c8a25eb49e328e3129
Ndricim Rrapi <ndricim.rrapi@sap.com>
2021-03-29 17:44:01 +0200

    modify auto-router

--- test/e2e-test-application/src/app/services/luigi-auto-navigation.service.ts
+++ test/e2e-test-application/src/app/services/luigi-auto-navigation.service.ts
@@ -29,6 +29,22 @@ export class LuigiAutoNavigationService implements OnDestroy {
           }
         })
     );
+    const customLocalPrefixNavSync = '/nav-sync-example';
+    this.subscriptions.add(
+      this.router.events
+        .pipe(
+          filter(ev => ev instanceof NavigationEnd),
+          filter(ev => (ev as NavigationEnd).url.startsWith(customLocalPrefixNavSync))
+        )
+        .subscribe((ev: NavigationEnd) => {
+          if (ev instanceof NavigationEnd) {
+            linkManager()
+              .fromVirtualTreeRoot()
+              .withoutSync()
+              .fromClosestContext()
+              .navigate(ev.url.substr(customLocalPrefix.length));
+          }
+        }))
   }
 
   ngOnDestroy(): void {
