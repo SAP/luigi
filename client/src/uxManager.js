e1c18685d623513d1ed12a516e40b09797a2be3f
Stanley Hsu <ching.hsiang.hsu@sap.com>
2021-07-26 14:24:14 +0200

    Doc update

--- client/src/uxManager.js
+++ client/src/uxManager.js
@@ -184,8 +184,9 @@ class UxManager extends LuigiClientBase {
   /**
    * @private
    * @memberof uxManager
-   * @param {string} id alert id
-   * @param {string} dismissKey key of the link
+   * @param {Object} alertObj
+   * @param {string} alertObj.id alert id
+   * @param {string} alertObj.dismissKey key of the link
    */
   hideAlert({ id, dismissKey }) {
     const alerts = this.getPromise('alerts');
