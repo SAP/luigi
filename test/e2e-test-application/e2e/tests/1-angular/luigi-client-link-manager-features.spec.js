12ac752ae41f5c79121c3d93a9ad9e517a7f2059
Rafal Gamon <rafal.gamon@sap.com>
2021-08-04 15:44:47 +0200

    2151 | Add tests for drawer component with webcomponent

--- test/e2e-test-application/e2e/tests/1-angular/luigi-client-link-manager-features.spec.js
+++ test/e2e-test-application/e2e/tests/1-angular/luigi-client-link-manager-features.spec.js
@@ -317,5 +317,19 @@ describe('Luigi client linkManager', () => {
         .click();
       cy.get('.drawer').should('not.exist');
     });
+
+    it('Open and close drawer component with webcomponent', () => {
+      // cy.visitLoggedIn('/projects/pr1');
+      cy.window().then(win => {
+        win.Luigi.navigation().openAsDrawer('/projects/pr1/webcomponent');
+        cy.get('.drawer').should('exist');
+        cy.expectPathToBe('/projects/pr2');
+
+        cy.get('.drawer-dialog button[aria-label="close"]')
+          .should('exist')
+          .click();
+        cy.get('.drawer').should('not.exist');
+      });
+    });
   });
 });
