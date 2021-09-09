1f2a51abeaef293cdf93ce003e7fe6332dd8bbd0
Ndricim Rrapi <ndricim.rrapi@sap.com>
2021-09-09 15:41:02 +0200

    Split tests

--- test/e2e-test-application/e2e/tests/1-angular/luigi-client-link-manager-features.spec.js
+++ test/e2e-test-application/e2e/tests/1-angular/luigi-client-link-manager-features.spec.js
@@ -1,188 +1,205 @@
 describe('Luigi client linkManager', () => {
+  const localRetries = {
+    retries: {
+      runMode: 3,
+      openMode: 3
+    }
+  };
+
   beforeEach(() => {
     cy.visitLoggedIn('/');
   });
 
-  it(
-    'linkManager features',
-    {
-      retries: {
-        runMode: 3,
-        openMode: 3
-      }
-    },
-    () => {
-      cy.getIframeBody().then($iframeBody => {
-        cy.goToLinkManagerMethods($iframeBody);
-
-        //navigate using absolute path
-        cy.wrap($iframeBody)
-          .contains('absolute: to overview')
-          .click();
-        cy.expectPathToBe('/overview');
-
-        cy.goToLinkManagerMethods($iframeBody);
-
-        //navigate using relative path
-        cy.wrap($iframeBody)
-          .contains('relative: to stakeholders')
-          .click();
-        cy.expectPathToBe('/projects/pr2/users/groups/stakeholders');
-
-        cy.goToOverviewPage();
-        cy.goToLinkManagerMethods($iframeBody);
-
-        //navigate using closest context
-        cy.wrap($iframeBody)
-          .contains('closest parent: to stakeholders')
-          .click();
-        cy.expectPathToBe('/projects/pr2/users/groups/stakeholders');
-
-        cy.goToOverviewPage();
-        cy.goToLinkManagerMethods($iframeBody);
-
-        //navigate using context
-        cy.wrap($iframeBody)
-          .contains('parent by name: project to settings')
-          .click();
-        cy.expectPathToBe('/projects/pr2/settings');
-
-        cy.wrap($iframeBody).should('contain', 'Settings');
-        cy.wrap($iframeBody)
-          .contains('Click here')
-          .click();
-        cy.expectPathToBe('/projects/pr2');
-        cy.wait(500);
-        //navigate to sibling through parent
-        cy.wrap($iframeBody)
-          .contains('from parent: to sibling')
-          .click();
-        cy.expectPathToBe('/projects/pr1');
-
-        cy.goToOverviewPage();
-        cy.goToLinkManagerMethods($iframeBody);
-
-        //navigate with params
-        cy.wrap($iframeBody)
-          .contains('project to settings with params (foo=bar)')
-          .click();
-        cy.wrap($iframeBody).should('contain', 'Called with params:');
-        cy.wrap($iframeBody).should('contain', '"foo": "bar"');
-
-        cy.expectSearchToBe('?~foo=bar');
-
-        cy.wrap($iframeBody)
-          .contains('Click here')
-          .click();
-        cy.expectPathToBe('/projects/pr2');
-        cy.wait(500);
-        //don't navigate
-        cy.wrap($iframeBody)
-          .contains('parent by name: with nonexisting context')
-          .click();
-        cy.expectPathToBe('/projects/pr2');
-
-        //open webcomponent in splitview
-        cy.wrap($iframeBody)
-          .contains('Open webcomponent in splitView')
-          .click();
-        cy.get('.iframeSplitViewCnt>').then(container => {
-          const root = container.children().prevObject[0].shadowRoot;
-          const wcContent = root.querySelector('p').innerText;
-          expect(wcContent).to.equal('Hello WebComponent!');
-          root.querySelector('button').click();
-          cy.get('[data-testid=luigi-alert]').should('have.class', 'fd-message-strip--information');
-          cy.get('[data-testid=luigi-alert]').should('contain', 'Hello from uxManager in Web Component');
-        });
-        //navigate with intent
-        cy.wrap($iframeBody)
-          .contains('navigate to settings with intent with parameters')
-          .click();
-        cy.expectPathToBe('/projects/pr2/settings');
-        cy.expectSearchToBe('?~param1=abc&~param2=bcd');
-
-        cy.goToOverviewPage();
-        cy.goToLinkManagerMethods($iframeBody);
-
-        // navigate with intent - second option
-        cy.wrap($iframeBody)
-          .contains('navigate to settings of project 1 with alternative intent method')
-          .click();
-        cy.expectPathToBe('/projects/pr1/settings');
-        cy.expectSearchToBe('?~project=pr1&~param2=22');
-
-        cy.goToOverviewPage();
-        cy.goToLinkManagerMethods($iframeBody);
-
-        //navigate with preserve view functionality
-        cy.wrap($iframeBody)
-          .contains('with preserved view: project to global settings and back')
-          .click();
-        cy.expectPathToBe('/settings');
-
-        //wait for the alert coming from an inactive iFrame to be shown and second iFrame to be loaded
-        cy.wait(500);
-        cy.get('.fd-message-strip').should('contain', 'Information alert sent from an inactive iFrame');
-
-        cy.getIframeBody().then($iframeBody => {
-          cy.wrap($iframeBody)
-            .find('input')
-            .clear()
-            .type('tets');
-          cy.wrap($iframeBody)
-            .find('button')
-            .click();
-          cy.expectPathToBe('/projects/pr2');
-        });
-
-        cy.visitLoggedIn('/');
-
-        cy.getIframeBody().then($iframeBody => {
-          // check if path exists
-          cy.goToLinkManagerMethods($iframeBody);
-          [
-            // non-existent relative path
-            { path: 'projects/pr2/', successExpected: false },
-            // non-existent absolute path
-            { path: '/developers', successExpected: false },
-            // existent absolute path with '/' at the end
-            { path: '/projects/pr2/', successExpected: true },
-            // existent absolute path without '/' at the end
-            { path: '/projects/pr2', successExpected: true },
-            // existent path with two dynamic pathSegments
-            {
-              path: '/projects/pr1/users/groups/avengers/settings/dynamic-two',
-              successExpected: true
-            },
-            // existent relative path
-            { path: 'developers', successExpected: true }
-          ].map(data => {
-            const msgExpected = data.successExpected ? `Path ${data.path} exists` : `Path ${data.path} does not exist`;
-            const checkPathSelector = '.link-manager .check-path';
-            cy.wrap($iframeBody)
-              .find(checkPathSelector + ' input')
-              .clear()
-              .type(data.path);
-            cy.wrap($iframeBody)
-              .find(checkPathSelector + ' button')
-              .click();
-            cy.wrap($iframeBody)
-              .find(checkPathSelector + ' .check-path-result')
-              .contains(msgExpected);
-          });
-
-          // go back
-          cy.goToOverviewPage();
-          cy.goToLinkManagerMethods($iframeBody);
-          cy.expectPathToBe('/projects/pr2');
-          cy.wrap($iframeBody)
-            .contains('go back: single iframe')
-            .click();
-          cy.expectPathToBe('/overview');
-        });
+  describe('linkManager features', () => {
+    let $iframeBody;
+    beforeEach(() => {
+      // "clear" variables to make sure they are not reused and throw error in case something goes wrong
+      $iframeBody = undefined;
+      cy.visitLoggedIn('/projects/pr2');
+      cy.getIframeBody().then(result => {
+        $iframeBody = result;
       });
-    }
-  );
+    });
+
+    it('navigate using absolute path', localRetries, () => {
+      // navigate using absolute path
+      cy.wrap($iframeBody)
+        .contains('absolute: to overview')
+        .click();
+      cy.expectPathToBe('/overview');
+    });
+
+    it('navigate using relative path', localRetries, () => {
+      // navigate using relative path
+      cy.wrap($iframeBody)
+        .contains('relative: to stakeholders')
+        .click();
+      cy.expectPathToBe('/projects/pr2/users/groups/stakeholders');
+    });
+
+    it('navigate using closest context', localRetries, () => {
+      // navigate using closest context
+      cy.wrap($iframeBody)
+        .contains('closest parent: to stakeholders')
+        .click();
+      cy.expectPathToBe('/projects/pr2/users/groups/stakeholders');
+    });
+
+    it('navigate using context', localRetries, () => {
+      // navigate using context
+      cy.wrap($iframeBody)
+        .contains('parent by name: project to settings')
+        .click();
+      cy.expectPathToBe('/projects/pr2/settings');
+
+      cy.wrap($iframeBody).should('contain', 'Settings');
+      cy.wrap($iframeBody)
+        .contains('Click here')
+        .click();
+      cy.expectPathToBe('/projects/pr2');
+    });
+
+    it('navigate to sibling through parent', localRetries, () => {
+      // navigate to sibling through parent
+      cy.wrap($iframeBody)
+        .contains('from parent: to sibling')
+        .click();
+      cy.expectPathToBe('/projects/pr1');
+    });
+
+    it('navigate with params', localRetries, () => {
+      cy.wrap($iframeBody)
+        .contains('project to settings with params (foo=bar)')
+        .click();
+      cy.wrap($iframeBody).should('contain', 'Called with params:');
+      cy.wrap($iframeBody).should('contain', '"foo": "bar"');
+
+      cy.expectSearchToBe('?~foo=bar');
+
+      cy.wrap($iframeBody)
+        .contains('Click here')
+        .click();
+      cy.expectPathToBe('/projects/pr2');
+    });
+
+    it('dont navigate with nonexisting context', localRetries, () => {
+      cy.wrap($iframeBody)
+        .contains('parent by name: with nonexisting context')
+        .click();
+      cy.expectPathToBe('/projects/pr2');
+    });
+  });
+
+  describe('linkManager features 2', () => {
+    let $iframeBody;
+    beforeEach(() => {
+      // "clear" variables to make sure they are not reused and throw error in case something goes wrong
+      $iframeBody = undefined;
+      cy.visitLoggedIn('/projects/pr2');
+      cy.getIframeBody().then(result => {
+        $iframeBody = result;
+      });
+    });
+
+    it('open webcomponent in splitview', localRetries, () => {
+      cy.expectPathToBe('/projects/pr2');
+      cy.goToLinkManagerMethods($iframeBody);
+      // open webcomponent in splitview
+      cy.wrap($iframeBody)
+        .contains('Open webcomponent in splitView')
+        .click();
+      cy.get('.iframeSplitViewCnt>').then(container => {
+        const root = container.children().prevObject[0].shadowRoot;
+        const wcContent = root.querySelector('p').innerText;
+        expect(wcContent).to.equal('Hello WebComponent!');
+        root.querySelector('button').click();
+        cy.get('[data-testid=luigi-alert]').should('have.class', 'fd-message-strip--information');
+        cy.get('[data-testid=luigi-alert]').should('contain', 'Hello from uxManager in Web Component');
+      });
+    });
+
+    it('navigate with intent', localRetries, () => {
+      // navigate with intent
+      cy.wrap($iframeBody)
+        .contains('navigate to settings with intent with parameters')
+        .click();
+      cy.expectPathToBe('/projects/pr2/settings');
+      cy.expectSearchToBe('?~param1=abc&~param2=bcd');
+    });
+
+    it('navigate with intent - second api option', localRetries, () => {
+      // navigate with intent - second option
+      cy.wrap($iframeBody)
+        .contains('navigate to settings of project 1 with alternative intent method')
+        .click();
+      cy.expectPathToBe('/projects/pr1/settings');
+      cy.expectSearchToBe('?~project=pr1&~param2=22');
+    });
+
+    it('navigate with preserve view functionality', localRetries, () => {
+      // navigate with preserve view functionality
+      cy.wrap($iframeBody)
+        .contains('with preserved view: project to global settings and back')
+        .click();
+      cy.expectPathToBe('/settings');
+
+      // wait for the alert coming from an inactive iFrame to be shown and second iFrame to be loaded
+      cy.get('.fd-message-strip').should('contain', 'Information alert sent from an inactive iFrame');
+
+      cy.getIframeBody().then($iframeBody => {
+        cy.wrap($iframeBody)
+          .find('input')
+          .clear()
+          .type('tets');
+        cy.wrap($iframeBody)
+          .find('button')
+          .click();
+        cy.expectPathToBe('/projects/pr2');
+      });
+    });
+
+    it('path exists check', localRetries, () => {
+      [
+        // non-existent relative path
+        { path: 'projects/pr2/', successExpected: false },
+        // non-existent absolute path
+        { path: '/developers', successExpected: false },
+        // existent absolute path with '/' at the end
+        { path: '/projects/pr2/', successExpected: true },
+        // existent absolute path without '/' at the end
+        { path: '/projects/pr2', successExpected: true },
+        // existent path with two dynamic pathSegments
+        {
+          path: '/projects/pr1/users/groups/avengers/settings/dynamic-two',
+          successExpected: true
+        },
+        // existent relative path
+        { path: 'developers', successExpected: true }
+      ].forEach(data => {
+        const msgExpected = data.successExpected ? `Path ${data.path} exists` : `Path ${data.path} does not exist`;
+        const checkPathSelector = '.link-manager .check-path';
+        cy.wrap($iframeBody)
+          .find(checkPathSelector + ' input')
+          .clear()
+          .type(data.path);
+        cy.wrap($iframeBody)
+          .find(checkPathSelector + ' button')
+          .click();
+        cy.wrap($iframeBody)
+          .find(checkPathSelector + ' .check-path-result')
+          .contains(msgExpected);
+      });
+    });
+
+    it('go back', localRetries, () => {
+      // go back
+      cy.expectPathToBe('/projects/pr2');
+      cy.wrap($iframeBody)
+        .contains('go back: single iframe')
+        .click();
+      cy.expectPathToBe('/overview');
+    });
+  });
 
   describe('linkManager navigation from a modal', () => {
     let $iframeBody;
@@ -288,10 +305,10 @@ describe('Luigi client linkManager', () => {
         'Could not map the exact target node for the requested route projects/pr2/miscellaneous2/maskopatol'
       );
 
-      //navigate somewhere else
+      // navigate somewhere else
       cy.goToProjectsPage();
 
-      //alert disappears
+      // alert disappears
       cy.get('.fd-message-strip').should('not.exist');
     });
 
@@ -303,18 +320,18 @@ describe('Luigi client linkManager', () => {
 
       cy.get('.fd-message-strip').contains('Could not find the requested route maskopatol/has/a/child');
 
-      //navigate somewhere else
+      // navigate somewhere else
       cy.goToProjectsPage();
 
-      //alert disappears
+      // alert disappears
       cy.get('.fd-message-strip').should('not.exist');
     });
   });
 
   describe('Goes back and pass data ', () => {
     let $iframeBody;
-    let $inputTypeNormal = 'Buongiorno Luigi';
-    let $inputTypeModal = 'Buona notte Luigi';
+    const $inputTypeNormal = 'Buongiorno Luigi';
+    const $inputTypeModal = 'Buona notte Luigi';
     beforeEach(() => {
       cy.visitLoggedIn('/projects/pr2');
       cy.getIframeBody().then(result => {
