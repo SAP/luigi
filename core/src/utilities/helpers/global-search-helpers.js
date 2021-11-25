d7d1f8942953c003996182386acb80e75577b67b
Doberer, Johannes <johannes.doberer@sap.com>
2021-11-25 18:11:19 +0100

    typo

--- core/src/utilities/helpers/global-search-helpers.js
+++ core/src/utilities/helpers/global-search-helpers.js
@@ -5,9 +5,9 @@ class GlobalSearchHelperClass {
   handleVisibilityGlobalSearch() {
     const globalSearchCtn = document.querySelector('.lui-global-search');
     if (globalSearchCtn.offsetWidth <= 384) {
-      globalSearchCtn.classList.add('lui-global-search-toogle');
+      globalSearchCtn.classList.add('lui-global-search-toggle');
     } else {
-      globalSearchCtn.classList.remove('lui-global-search-toogle');
+      globalSearchCtn.classList.remove('lui-global-search-toggle');
     }
   }
 }
