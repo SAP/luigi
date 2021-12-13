3b1b3e26311537a8f3f96e81f9776bc91175337e
Doberer, Johannes <johannes.doberer@sap.com>
2021-12-13 21:12:02 +0100

    introduce cache for whitesource ua file

--- scripts/whiteSource.sh
+++ scripts/whiteSource.sh
@@ -20,17 +20,23 @@ declare -a PROJECT_TOKENS=("a63fd5aaaa2343199327aac6d3e2b5346e930927d66441bf9211
                            "56eb623145264763a82330025e4f11efd538074a3cd04791ab6ab87293174aca"
                           )
 
-cd $BASE_DIR
-echo "Start download.."
-echo $BASE_DIR
-curl -LJO https://github.com/whitesource/unified-agent-distribution/releases/latest/download/wss-unified-agent.jar
-echo "Downloaded"
+cd $BASE_DIR/../../../../whitesource
+CACHED_WHITESOURCE_UA=$BASE_DIR/../../../../whitesource/wss-unified-agent.jar
+echo $CACHED_WHITESOURCE_UA
+if [ -f "$CACHED_WHITESOURCE_UA" ]; then
+    echo "$CACHED_WHITESOURCE_UA exists. Take from Cache."
+else 
+    echo "$CACHED_WHITESOURCE_UA does not exist."
+    echo "Start download.."
+    curl -LJO https://github.com/whitesource/unified-agent-distribution/releases/latest/download/wss-unified-agent.jar
+    echo "Downloaded"
+fi
 
 for ((i=0;i<${#FOLDERS[@]};++i)); do
   cd $BASE_DIR/../${FOLDERS[i]}
-  echo "create config File:"
+  echo "Create config File:"
   echo $'apiKey='${WHITESOURCE_APIKEY}$'\nuserKey='$WHITESOURCE_USER_TOKEN$'\nproductName='$WHITESOURCE_PRODUCT_TOKEN$'\nprojectToken='${PROJECT_TOKENS[i]}$'\ndevDep=false\nforceUpdate=true\ncheckPolicies=true\nwss.url=https://sap.whitesourcesoftware.com/agent' > wss-generated-file.config
-  java -jar $BASE_DIR/wss-unified-agent.jar -c wss-generated-file.config -d . -scanComment "test"
+  java -jar $BASE_DIR/../../../../whitesource/wss-unified-agent.jar -c wss-generated-file.config -d . -scanComment "test"
 
   RV=$?
   echo "Exit code: $RV"
