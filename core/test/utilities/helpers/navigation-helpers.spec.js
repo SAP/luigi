7b894cf934fd40bd95cb7743b073159591f4ac21
Wiktor Doberschuetz <wdoberschuetz@gmail.com>
2021-12-16 12:54:00 +0100

    2384: Add tests

--- core/test/utilities/helpers/navigation-helpers.spec.js
+++ core/test/utilities/helpers/navigation-helpers.spec.js
@@ -522,4 +522,19 @@ describe('Navigation-helpers', () => {
       assert.equal(NavigationHelpers.generateTooltipText(node, 'LuigiNode'), '');
     });
   });
+
+  describe('renderIconClassName', () => {
+    it('should render sap-icon to standard icon suite', () => {
+      assert.equal(NavigationHelpers.renderIconClassName('home'), 'sap-icon--home');
+    });
+    it('should render sap-icon to standard TNT suite', () => {
+      assert.equal(NavigationHelpers.renderIconClassName('TNT--home'), 'sap-icon-TNT--home');
+    });
+    it('should render sap-icon to standard businessSuiteInAppSymbols suite', () => {
+      assert.equal(
+        NavigationHelpers.renderIconClassName('businessSuiteInAppSymbols--home'),
+        'sap-icon-businessSuiteInAppSymbols--home'
+      );
+    });
+  });
 });
