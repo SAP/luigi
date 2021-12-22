2a06b5d3bb8738e6bbce800096ca5d74649a6e0b
Philipp Pracht <philipp.pracht@sap.com>
2021-12-22 14:27:33 +0100

    test rehype

--- website/docs/src/unified-plugins/rehype-luigi-oldVersions.js
+++ website/docs/src/unified-plugins/rehype-luigi-oldVersions.js
@@ -1,10 +1,12 @@
 import visit from 'unist-util-visit';
+import h from 'hastscript';
 
 export default function oldVersion() {
   return function transformer(tree) {
     visit(tree, 'element', function(node) {
       if (node.type === 'comment' && node.value.trim() === 'oldVersionsDropdown') {
-        console.log('#################### FOUND COMMENT');
+        const oldVerDropdown = h('div.oldverdrop');
+        Object.assign(node, oldVerDropdown);
       }
     });
   };
