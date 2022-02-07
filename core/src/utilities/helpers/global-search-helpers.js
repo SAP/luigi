df575f4c7d84d3b2050782e52abcdce23b1114ce
Doberer, Johannes <johannes.doberer@sap.com>
2022-02-07 21:17:52 +0100

    GlobalSearch console error

--- core/src/utilities/helpers/global-search-helpers.js
+++ core/src/utilities/helpers/global-search-helpers.js
@@ -3,6 +3,7 @@ class GlobalSearchHelperClass {
   constructor() {}
 
   handleVisibilityGlobalSearch() {
+    if (!document.querySelector('.lui-global-search')) return;
     const globalSearchCtn = document.querySelector('.lui-global-search');
     const condition = globalSearchCtn.offsetWidth <= 384;
     globalSearchCtn.classList.toggle('lui-global-search-toggle', condition);
