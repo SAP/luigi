66abf1a179c231a00311de70e3250ba639569f83
Philipp Pracht <philipp.pracht@sap.com>
2022-04-07 17:39:03 +0200

    another try

--- core/src/core-api/routing.js
+++ core/src/core-api/routing.js
@@ -79,8 +79,7 @@ class LuigiRouting {
   }
 
   sanitizeUrl(url) {
-    console.log('##############################', location.origin, new URL(url).origin);
-    return location.origin === new URL(url).origin ? url : undefined;
+    return new URL(location).origin === new URL(url).origin ? url : undefined;
   }
 
   handleBrowserHistory(keepBrowserHistory, url) {
