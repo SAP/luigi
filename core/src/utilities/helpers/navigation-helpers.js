8625be1aa17b1d81286651365834c63d599bf671
Doberer, Johannes <johannes.doberer@sap.com>
2022-04-11 22:56:18 +0200

    Error handling on productswitcher columns calculation

--- core/src/utilities/helpers/navigation-helpers.js
+++ core/src/utilities/helpers/navigation-helpers.js
@@ -19,8 +19,9 @@ class NavigationHelpersClass {
 
   getProductSwitcherColumnsNumber() {
     const productSwitcherConfig = this.getProductSwitcherConfig();
+    if (!productSwitcherConfig.items) return;
     let productSwitcherColumns = productSwitcherConfig.columns;
-    let productSwitcherItemsAmount = productSwitcherConfig.items().length;
+    let productSwitcherItemsAmount = productSwitcherConfig.items.length;
     if (productSwitcherColumns === 'auto') {
       if (productSwitcherItemsAmount <= 6) {
         return (productSwitcherConfig.columns = 3);
