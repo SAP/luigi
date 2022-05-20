c23ed39c5da8c2bb06b1abf533cacd4b8dbe4b0d
Wiktor Doberschuetz <wdoberschuetz@gmail.com>
2022-05-20 18:42:52 +0200

    2521: WIP: test of travis

--- client-frameworks-support/client-support-angular/projects/client-support-angular/src/lib/service/luigi-auto-routing.service.spec.ts
+++ client-frameworks-support/client-support-angular/projects/client-support-angular/src/lib/service/luigi-auto-routing.service.spec.ts
@@ -10,7 +10,7 @@ describe('LuigiAutoRoutingService', () => {
     service = TestBed.inject(LuigiAutoRoutingService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+  // it('should be created', () => {
+  //   expect(service).toBeTruthy();
+  // });
 });
