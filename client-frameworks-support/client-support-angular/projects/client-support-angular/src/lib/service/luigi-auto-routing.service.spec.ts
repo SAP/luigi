a2eee021f56f4772ee2b93b4da825b03449f211c
Wiktor Doberschuetz <wdoberschuetz@gmail.com>
2022-05-31 23:18:24 +0200

    2521: Add test for LuigiAutoRoutingService

--- client-frameworks-support/client-support-angular/projects/client-support-angular/src/lib/service/luigi-auto-routing.service.spec.ts
+++ client-frameworks-support/client-support-angular/projects/client-support-angular/src/lib/service/luigi-auto-routing.service.spec.ts
@@ -1,16 +1,22 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { LuigiAutoRoutingService } from './luigi-auto-routing.service';
+import { LuigiContextService } from './luigi-context-service';
 
 describe('LuigiAutoRoutingService', () => {
   let service: LuigiAutoRoutingService;
+  let luigiContextService = {};
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule.withRoutes([])],
+      providers: [{ provide: LuigiContextService, use: luigiContextService }]
+    });
     service = TestBed.inject(LuigiAutoRoutingService);
   });
 
-  // it('should be created', () => {
-  //   expect(service).toBeTruthy();
-  // });
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
 });
