99b9d8be0d642c9f597977786291873c93b44c89
Wiktor Doberschuetz <wdoberschuetz@gmail.com>
2022-06-01 13:22:57 +0200

    Test

--- client-frameworks-support/client-support-angular/projects/client-support-angular/karma.conf.js
+++ client-frameworks-support/client-support-angular/projects/client-support-angular/karma.conf.js
@@ -31,7 +31,7 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadlessNoSandbox'],
+    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox'],
     singleRun: false,
     restartOnFileChange: true
   });
