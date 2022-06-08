5d724bbcbf8df90c157e5cfd91922ad93bffbc27
Ndricim Rrapi <ndricim.rrapi@sap.com>
2022-06-08 19:13:17 +0200

    Add e2e tests 1

--- test/e2e-test-application/src/app/project/users/groups/group-details/group-details.component.ts
+++ test/e2e-test-application/src/app/project/users/groups/group-details/group-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ChangeDetectorRef, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { getPathParams, linkManager } from '@luigi-project/client';
+import { NgZone } from '@angular/core';
 
 import { LuigiContextService, IContextMessage } from '../../../../services/luigi-context.service';
 import { toTitleCase } from '../../../../services/helpers';
@@ -15,8 +16,9 @@ export class GroupDetailsComponent implements OnInit, OnDestroy {
   public pathParams: { [key: string]: string };
   public groupLabel: string;
   private lcSubscription: Subscription;
+  public currentRoute: string;
 
-  constructor(private luigiService: LuigiContextService, private cdr: ChangeDetectorRef) {}
+  constructor(private luigiService: LuigiContextService, private cdr: ChangeDetectorRef, private ngZone: NgZone) {}
 
   ngOnInit() {
     this.lcSubscription = this.luigiService.getContext().subscribe((ctx: IContextMessage) => {
@@ -26,6 +28,14 @@ export class GroupDetailsComponent implements OnInit, OnDestroy {
       // Default way, if context is not specified in node configuration
       this.pathParams = getPathParams();
       this.groupLabel = this.pathParams && this.pathParams.group && toTitleCase(this.pathParams.group);
+
+      linkManager()
+        .fromContext('project')
+        .getCurrentRoute()
+        .then(route => {
+          this.currentRoute = route;
+          console.log('route', route);
+        });
 
       if (!this.cdr['destroyed']) {
         this.cdr.detectChanges();
@@ -37,5 +47,15 @@ export class GroupDetailsComponent implements OnInit, OnDestroy {
     if (this.lcSubscription) {
       this.lcSubscription.unsubscribe();
     }
+  }
+
+  getCurrentRoute() {
+    linkManager()
+      .fromContext('project')
+      .getCurrentRoute()
+      .then(route => {
+        this.currentRoute = route;
+        console.log('route', route);
+      });
   }
 }
