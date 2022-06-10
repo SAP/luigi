b0b6d26608977210cb4d41257626c0e6f413f72c
viktorsperling <viktor.sperling@sap.com>
2022-06-10 11:07:09 +0200

    Negative test cypress 2

--- test/e2e-test-application/e2e/test3/0-setuptests/setup-test.spec.js
+++ test/e2e-test-application/e2e/test3/0-setuptests/setup-test.spec.js
@@ -5,7 +5,7 @@ describe('Navigation', () => {
   describe('iFrame check', () => {
     it('Check if iFrame exists in running application.', () => {
       cy.wait(5000);
-      cy.get('iframe');
+      cy.get('ui5-dance-module');
     });
   });
 });
