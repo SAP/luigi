482db4e2936a533fc27ca5f90c41d5ba9915be38
Stanley Hsu <ching.hsiang.hsu@sap.com>
2022-07-05 11:10:06 +0200

    Refactor handleRouteChange function

--- core/src/services/routing.js
+++ core/src/services/routing.js
@@ -167,120 +167,137 @@ class RoutingClass {
       : GenericHelpers.trimLeadingSlash(window.location.pathname);
   }
 
-  async handleRouteChange(path, component, iframeElement, config, withoutSync, preventContextUpdate = false) {
+  setFeatureToggle(path) {
     const featureToggleProperty = LuigiConfig.getConfigValue('settings.featureToggles.queryStringParam');
-    if (featureToggleProperty) {
-      RoutingHelpers.setFeatureToggles(featureToggleProperty, path);
-    }
+    featureToggleProperty && RoutingHelpers.setFeatureToggles(featureToggleProperty, path);
+  }
 
+  shouldSkipRoutingForUrlPatterns() {
     const defaultPattern = [/access_token=/, /id_token=/];
     const patterns = LuigiConfig.getConfigValue('routing.skipRoutingForUrlPatterns') || defaultPattern;
-    const hasSkipMatches = patterns.filter(p => window.location.href.match(p)).length !== 0;
-    if (hasSkipMatches) {
-      return;
+
+    return patterns.filter(p => window.location.href.match(p)).length !== 0;
+  }
+
+  showUnsavedChangesModal(path, component, iframeElement, config) {
+    const newUrl = window.location.href;
+    const oldUrl = component.get().unsavedChanges.persistUrl;
+
+    //pretend the url hasn't been changed
+    oldUrl && history.replaceState(window.state, '', oldUrl);
+    component.showUnsavedChangesModal().then(
+      () => {
+        path &&
+          this.handleRouteChange(path, component, iframeElement, config) &&
+          history.replaceState(window.state, '', newUrl);
+      },
+      () => {}
+    );  
+  }
+
+  async shouldShowModalPathInUrl() {
+    if (LuigiConfig.getConfigValue('routing.showModalPathInUrl')) {
+      await this.handleBookmarkableModalPath();
     }
+  }
+
+  async handleViewUrlMisconfigured(pathUrlRaw, nodeObject, previousCompData, component) {
+    const { viewUrl = '', children, intendToHaveEmptyViewUrl, compound } = nodeObject
+    const hasChildrenNode =
+      (children && Array.isArray(children) && children.length > 0) ||
+      children ||
+      false;
+
+    if (!compound && viewUrl.trim() === '' && !hasChildrenNode && !intendToHaveEmptyViewUrl) {
+      console.warn(
+        "The intended target route can't be accessed since it has neither a viewUrl nor children. This is most likely a misconfiguration."
+      );
+
+      // redirect to root when this empty viewUrl node be reached directly
+      if (
+        !(
+          previousCompData &&
+          (previousCompData.viewUrl || (previousCompData.currentNode && previousCompData.currentNode.compound))
+        )
+      ) {
+        const rootPathData = await Navigation.getNavigationPath(
+          LuigiConfig.getConfigValueAsync('navigation.nodes'),
+          '/'
+        );
+        const rootPath = await RoutingHelpers.getDefaultChildNode(rootPathData);
+        this.showPageNotFoundError(component, rootPath, pathUrlRaw);
+        this.navigateTo(rootPath);
+      }
+      return true;
+    }
+    return false
+  }
+
+
+  async handlePageNotFound(nodeObject, viewUrl, pathData, path, component, pathUrlRaw, config) {
+    if (!viewUrl && !nodeObject.compound) {
+      const defaultChildNode = await RoutingHelpers.getDefaultChildNode(pathData, async (node, ctx) => {
+        return await Navigation.getChildren(node, ctx);
+      });
+
+      if (pathData.isExistingRoute) {
+        //normal navigation can be performed
+        const trimmedPathUrl = GenericHelpers.getTrimmedUrl(path);
+        this.navigateTo(`${trimmedPathUrl ? `/${trimmedPathUrl}` : ''}/${defaultChildNode}`, { keepBrowserHistory: false});
+        // reset comp data
+        component.set({ navigationPath: [] });
+      } else {
+        if (defaultChildNode && pathData.navigationPath.length > 1) {
+          //last path segment was invalid but a default node could be in its place
+          this.showPageNotFoundError(
+            component,
+            GenericHelpers.trimTrailingSlash(pathData.matchedPath) + '/' + defaultChildNode,
+            pathUrlRaw,
+            true
+          );
+          return true;
+        }
+
+        //ERROR  404
+        //the path is unrecognized at all and cannot be fitted to any known one
+        const rootPathData = await Navigation.getNavigationPath(
+          LuigiConfig.getConfigValueAsync('navigation.nodes'),
+          '/'
+        );
+        const rootPath = await RoutingHelpers.getDefaultChildNode(rootPathData);
+        this.showPageNotFoundError(component, rootPath, pathUrlRaw, false, config);
+      }
+      return true;
+    }
+
+    if (!pathData.isExistingRoute) {
+      this.showPageNotFoundError(component, pathData.matchedPath, pathUrlRaw, true);
+      returntrue;
+    }
+    return false;
+  }
+
+  async handleRouteChange(path, component, iframeElement, config, withoutSync, preventContextUpdate = false) {
+    this.setFeatureToggle(path);
+    if (this.shouldSkipRoutingForUrlPatterns()) return;
 
     try {
       // just used for browser changes, like browser url manual change or browser back/forward button click
       if (component.shouldShowUnsavedChangesModal()) {
-        const newUrl = window.location.href;
-        const oldUrl = component.get().unsavedChanges.persistUrl;
-
-        //pretend the url hasn't been changed
-        oldUrl && history.replaceState(window.state, '', oldUrl);
-
-        component.showUnsavedChangesModal().then(
-          () => {
-            path &&
-              this.handleRouteChange(path, component, iframeElement, config) &&
-              history.replaceState(window.state, '', newUrl);
-          },
-          () => {}
-        );
+        this.showUnsavedChangesModal(path, component, iframeElement, config);
         return;
       }
 
-      const isShowModalPathInUrl = LuigiConfig.getConfigValue('routing.showModalPathInUrl');
-      if (isShowModalPathInUrl) {
-        await this.handleBookmarkableModalPath();
-      }
+      await this.shouldShowModalPathInUrl();
 
       const previousCompData = component.get();
       this.checkInvalidateCache(previousCompData, path);
       const pathUrlRaw = path && path.length ? GenericHelpers.getPathWithoutHash(path) : '';
       const { nodeObject, pathData } = await Navigation.extractDataFromPath(path);
       const viewUrl = nodeObject.viewUrl || '';
-      const hasChildrenNode =
-        (nodeObject.children && Array.isArray(nodeObject.children) && nodeObject.children.length > 0) ||
-        nodeObject.children ||
-        false;
-      const intendToHaveEmptyViewUrl =
-        (nodeObject.intendToHaveEmptyViewUrl && nodeObject.intendToHaveEmptyViewUrl === true) || false;
-
-      if (!nodeObject.compound && viewUrl.trim() === '' && !hasChildrenNode && !intendToHaveEmptyViewUrl) {
-        console.warn(
-          "The intended target route can't be accessed since it has neither a viewUrl nor children. This is most likely a misconfiguration."
-        );
-
-        // redirect to root when this empty viewUrl node be reached directly
-        if (
-          !(
-            previousCompData &&
-            (previousCompData.viewUrl || (previousCompData.currentNode && previousCompData.currentNode.compound))
-          )
-        ) {
-          const rootPathData = await Navigation.getNavigationPath(
-            LuigiConfig.getConfigValueAsync('navigation.nodes'),
-            '/'
-          );
-          const rootPath = await RoutingHelpers.getDefaultChildNode(rootPathData);
-          this.showPageNotFoundError(component, rootPath, pathUrlRaw);
-          this.navigateTo(rootPath);
-        }
-
-        return;
-      }
-
-      if (!viewUrl && !nodeObject.compound) {
-        const defaultChildNode = await RoutingHelpers.getDefaultChildNode(pathData, async (node, ctx) => {
-          return await Navigation.getChildren(node, ctx);
-        });
-
-        if (pathData.isExistingRoute) {
-          //normal navigation can be performed
-          const trimmedPathUrl = GenericHelpers.getTrimmedUrl(path);
-          this.navigateTo(`${trimmedPathUrl ? `/${trimmedPathUrl}` : ''}/${defaultChildNode}`, {
-            keepBrowserHistory: false
-          });
-          // reset comp data
-          component.set({ navigationPath: [] });
-        } else {
-          if (defaultChildNode && pathData.navigationPath.length > 1) {
-            //last path segment was invalid but a default node could be in its place
-            this.showPageNotFoundError(
-              component,
-              GenericHelpers.trimTrailingSlash(pathData.matchedPath) + '/' + defaultChildNode,
-              pathUrlRaw,
-              true
-            );
-            return;
-          }
-          //ERROR  404
-          //the path is unrecognized at all and cannot be fitted to any known one
-          const rootPathData = await Navigation.getNavigationPath(
-            LuigiConfig.getConfigValueAsync('navigation.nodes'),
-            '/'
-          );
-          const rootPath = await RoutingHelpers.getDefaultChildNode(rootPathData);
-          this.showPageNotFoundError(component, rootPath, pathUrlRaw, false, config);
-        }
-        return;
-      }
-
-      if (!pathData.isExistingRoute) {
-        this.showPageNotFoundError(component, pathData.matchedPath, pathUrlRaw, true);
-        return;
-      }
+      
+      if (await this.handleViewUrlMisconfigured(pathUrlRaw, nodeObject, previousCompData, component)) return;
+      if (await this.handlePageNotFound(nodeObject, viewUrl, pathData, path, component, pathUrlRaw, config)) return;
 
       const hideNav = LuigiConfig.getConfigBooleanValue('settings.hideNavigation');
       const params = RoutingHelpers.parseParams(pathUrlRaw.split('?')[1]);
