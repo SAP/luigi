8a045ac8ac508810cbc3ce9131aeac51237e6216
viktorsperling <viktor.sperling@sap.com>
2022-08-08 12:33:19 +0200

    Add todo comment for index.ts

--- client-frameworks-support/testing-utilities/src/index.ts
+++ client-frameworks-support/testing-utilities/src/index.ts
@@ -1,2 +1,4 @@
 export { LuigiMockUtil } from './luigi-mock-util.js';
 export { LuigiMockEngine } from './luigi-mock-engine.js';
+
+//TODO: Check why this import from string has to end on .js and can't be .ts
