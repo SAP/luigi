634f4b4f2959a718e9305617b919e9f054523231
viktorsperling <viktor.sperling@sap.com>
2022-08-26 12:25:14 +0200

    Add .sh file to run tests

---
+++ test/mockengine.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -e # exit on errors
+
+BASE_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+
+source $BASE_DIR/../scripts/shared/bashHelpers.sh
+
+
+waitForWebServer() {
+  PORT=$1
+
+  while [`lsof -i :${PORT} | tail -n 1 | tr -s ' ' | cut -d ' ' -f 2` == ""]
+  do
+    sleep 5
+  done
+
+  npm run e2e:run
+  killWebserver $PORT
+}
+
+cd ../client-frameworks-support/testing-utilities
+npm install
+
+npm run bundle
+
+lerna bootstrap --force-local
+
+cd test
+#Run acutal test
+(set -e && waitForWebServer 8080) & (npm start)
