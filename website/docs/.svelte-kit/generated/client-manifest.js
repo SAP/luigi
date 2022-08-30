1a584429a11ef6bd28d06840460f16bddd06600a
Rafal Gamon <rafal.gamon@sap.com>
2022-08-30 15:25:28 +0200

    Migrate to sveltekit

---
+++ website/docs/.svelte-kit/generated/client-manifest.js
@@ -0,0 +1,7 @@
+export { matchers } from './client-matchers.js';
+
+export const nodes = [() => import('./nodes/0'), () => import('./nodes/1')];
+
+export const server_loads = [];
+
+export const dictionary = {};
--- website/docs/scripts/generate-docu-sitemap.js
+++ website/docs/scripts/generate-docu-sitemap.js
@@ -1,23 +1,28 @@
 import { getParsedDocs } from '../src/routes/docs/_parser';
-var fs = require('fs')
+import * as fs from 'fs';
 
 getParsedDocs().then(raw => {
   const docs = JSON.parse(raw);
-  var header = '<?xml version="1.0" encoding="utf-8"?>\n'
-  + '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">\n';
-  var footer = '</urlset>';
-  var sitemapString = "";
+  const header =
+    '<?xml version="1.0" encoding="utf-8"?>\n' +
+    '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">\n';
+  const footer = '</urlset>';
+  let sitemapString;
 
   sitemapString = header;
-  for(var i=0; i<docs.length; i++) {
-    sitemapString = sitemapString + '\xa0\xa0<url>\n'
-    + '\xa0\xa0\xa0\xa0<loc>https://docs.luigi-project.io/docs/' + docs[i].shortName + '</loc>\n'
-    + '\xa0\xa0\xa0\xa0<changefreq>monthly</changefreq>\n\xa0\xa0\xa0\xa0<priority>1.0</priority>\n'
-    + '\xa0\xa0</url>\n'
+  for (let i = 0; i < docs.length; i++) {
+    sitemapString =
+      sitemapString +
+      '\xa0\xa0<url>\n' +
+      '\xa0\xa0\xa0\xa0<loc>https://docs.luigi-project.io/docs/' +
+      docs[i].shortName +
+      '</loc>\n' +
+      '\xa0\xa0\xa0\xa0<changefreq>monthly</changefreq>\n\xa0\xa0\xa0\xa0<priority>1.0</priority>\n' +
+      '\xa0\xa0</url>\n';
   }
   sitemapString = sitemapString + footer;
 
-  fs.writeFile('sitemap.xml', sitemapString,  function (err,data) {
+  fs.writeFile('sitemap.xml', sitemapString, function(err, data) {
     if (err) {
       return console.log(err);
     }
