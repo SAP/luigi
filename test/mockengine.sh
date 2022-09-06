9a59a5b37171740e7de4a96621a0d434250ceb78
Ndricim Rrapi <ndricim.rrapi@sap.com>
2022-09-06 18:05:09 +0200

    fix port

--- test/mockengine.sh
+++ test/mockengine.sh
@@ -7,19 +7,30 @@ BASE_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 source $BASE_DIR/../scripts/shared/bashHelpers.sh
 
 
-waitForWebServer() {
-  PORT=$1
+runApp() {
+  local PORT=$1
 
-  while [`lsof -i :${PORT} | tail -n 1 | tr -s ' ' | cut -d ' ' -f 2` == ""]
-  do
-    sleep 5
+  echo ""
+  echo "Starting webserver on port $PORT"
+  pwd
+  npm run start &
+  PID=$!
+
+  echo "Webserver running with PID $PID"
+  # wait until example is built and running
+  local SLEEPSECS=1 # sleep time between webserver availability check
+  local WAITCOUNT=0
+  until $(curl --output /dev/null --silent --head --fail http://localhost:$PORT); do
+    if [ $WAITCOUNT -gt 15 ]; then
+      echo "Starting Webserver on $PORT timed out."
+      exit 1;
+    fi
+    printf '.'
+    sleep $SLEEPSECS
+    WAITCOUNT=$(($WAITCOUNT + $SLEEPSECS))
   done
-
-  npm run e2e:run
-  echo "Killing the webserver.................."
-  # killWebserver $PORT
-  echo "Exiting script with 0.................."
-  break
+  echo ""
+  echo "Webserver was ready after $WAITCOUNT seconds"
 }
 
 echo ""
@@ -69,4 +80,10 @@ lerna bootstrap --no-ci --force-local --include-dependents --include-dependencie
 
 cd $BASE_DIR/../client-frameworks-support/testing-utilities/test
 #Run acutal test
-(set -e && waitForWebServer 8080) & (npm start)
+PORT=8181
+runApp $PORT
+npm run e2e:run
+killWebserver $PORT
+# (set -e && waitForWebServer 8080) & (npm start)
+
+echo "Exiting script with 0.................."
