b666161aa65471ab9acc509c12ea98b431717bfb
Ndricim Rrapi <ndricim.rrapi@sap.com>
2022-09-16 11:07:35 +0200

    Add template folder

---
+++ cra-template/template/public/luigi-config.js
@@ -0,0 +1,47 @@
+Luigi.setConfig({
+  navigation: {
+    nodes: () => [
+      {
+        pathSegment: 'home',
+        label: 'Home',
+        icon: 'home',
+        viewUrl: '/sampleapp.html#/home',
+        children: [
+          {
+            pathSegment: 'sample1',
+            label: 'First',
+            icon: 'nutrition-activity',
+            viewUrl: '/sampleapp.html#/sample1'
+          },
+          {
+            pathSegment: 'sample2',
+            label: 'Second',
+            icon: 'paper-plane',
+            viewUrl: '/sampleapp.html#/sample2'
+          },
+          {
+            category: { label: 'Links', icon: 'cloud' },
+            label: 'Luigi Project',
+            externalLink: {
+              url: 'https://luigi-project.io/'
+            }
+          },
+          {
+            category: 'Links',
+            label: 'React.js',
+            externalLink: {
+              url: 'https://reactjs.org/'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  settings: {
+    header: {
+      title: 'Luigi React App',
+      logo: '/logo.png'
+    },
+    responsiveNavigation: 'simpleMobileOnly'
+  }
+});
