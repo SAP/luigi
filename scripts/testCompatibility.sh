62dc8e7ea8c26e3c811cb7eac6deeb7780e6c82c
Doberer, Johannes <johannes.doberer@sap.com>
2022-10-11 08:54:20 +0200

    add comment

--- scripts/testCompatibility.sh
+++ scripts/testCompatibility.sh
@@ -241,7 +241,7 @@ verifyAndStartWebserver() {
     cd $LUIGI_DIR_TESTING/${APP_FOLDERS[$i]}
     echoe "Here I'm $(pwd)"
     if [ "${APP_FOLDERS[$i]}" == "/test/e2e-test-application/externalMf" ]; then
-      # required for starting externalMF
+      # required for starting externalMF, otherwise webserver tries to start on /test/e2e-test-application/externalMf/externalMf
       echoe "Stepping out"
       cd ..
     fi
