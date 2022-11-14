882ae0fc52ae7319c37eeef82f23a1ba752d882b
Ndricim Rrapi <ndricim.rrapi@sap.com>
2022-11-14 15:07:15 +0100

    Fix warning

--- website/fiddle/src/App.svelte
+++ website/fiddle/src/App.svelte
@@ -268,13 +268,15 @@
           <a
             class="fd-link"
             href="https://www.sap.com/about/legal/privacy.html"
-            target="_blank">
+            target="_blank"
+            rel="noreferrer">
             Privacy Policy &nbsp&nbsp
           </a>
           <a
             class="fd-link"
             href="https://www.sap.com/about/legal/impressum.html"
-            target="_blank">
+            target="_blank"
+            rel="noreferrer">
             Legal
           </a>
         </span>
