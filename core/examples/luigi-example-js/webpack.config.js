2b978ca1a5c144a60bf0f6d8fbf824e365907f8d
Wiktor Doberschuetz <wdoberschuetz@gmail.com>
2022-11-30 10:54:02 +0100

    Change from main.js to luigi-config.js

--- core/examples/luigi-example-js/webpack.config.js
+++ core/examples/luigi-example-js/webpack.config.js
@@ -5,7 +5,8 @@ const path = require('path');
 
 module.exports = {
   output: {
-    path: path.resolve(__dirname, 'public')
+    path: path.resolve(__dirname, 'public'),
+    filename: 'luigi-config.js'
   },
   plugins: [
     new CopyPlugin({
