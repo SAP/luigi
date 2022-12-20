5104c1eaa4b0682ac375cc6a5d739e1af69f4645
Doberer, Johannes <johannes.doberer@sap.com>
2022-12-20 16:36:48 +0100

    add test

--- test/e2e-test-application/e2e/tests/0-js-test-app/js-test-app-history-for-modals.spec.js
+++ test/e2e-test-application/e2e/tests/0-js-test-app/js-test-app-history-for-modals.spec.js
@@ -44,6 +44,22 @@ describe('JS-TEST-APP', () => {
     }
   };
 
+  const simulateMultipleWizardNavigation = () => {
+    let $iframeBody;
+    cy.getIframeBody({}, 0, '[modal-container-index=0]').then(result => {
+      $iframeBody = result;
+      cy.wrap($iframeBody)
+        .contains('open US')
+        .click();
+    });
+    cy.getIframeBody({}, 0, '[modal-container-index=0]').then(result => {
+      $iframeBody = result;
+      cy.wrap($iframeBody)
+        .contains('go back to modalMf')
+        .click();
+    });
+  };
+
   const expectedPathAfterForward = path => {
     cy.go('forward');
     cy.expectPathToBe(path);
@@ -169,6 +185,44 @@ describe('JS-TEST-APP', () => {
         openModal();
         closeModal();
         openModal();
+        closeModal();
+        cy.expectPathToBe('/home');
+      });
+      it('Go back when history is 50 and modalHistoryLength > historygap', () => {
+        newConfig.navigation.nodes[0].children.push({
+          pathSegment: 'usersettings',
+          label: 'Usersettings MF',
+          viewUrl: '/examples/microfrontends/customUserSettingsMf.html'
+        });
+        cy.visitTestApp('/home', newConfig);
+        cy.get('#app[configversion="js-test-app-history-handling-modals-1"]');
+        for (let i = 0; i < 20; i++) {
+          clickingAroundInNavigation();
+        }
+        cy.window()
+          .its('history')
+          .and('have.property', 'length')
+          .should('eq', 50);
+        openModal();
+        cy.window()
+          .its('history')
+          .and('have.property', 'state')
+          .should('deep.include', {
+            modalHistoryLength: 1,
+            historygap: 50,
+            pathBeforeHistory: '/home'
+          });
+        for (let i = 0; i < 27; i++) {
+          simulateMultipleWizardNavigation();
+        }
+        cy.window()
+          .its('history')
+          .and('have.property', 'state')
+          .should('deep.include', {
+            modalHistoryLength: 55,
+            historygap: 50,
+            pathBeforeHistory: '/home'
+          });
         closeModal();
         cy.expectPathToBe('/home');
       });
