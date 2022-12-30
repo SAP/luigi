5e63f02ee5030e728307378b8f3e837badf1cbfb
Ndricim Rrapi <ndricim.rrapi@sap.com>
2022-12-30 15:42:29 +0100

    update plugins package

--- plugins/auth/src/auth-oidc/webpack-extra.config.js
+++ plugins/auth/src/auth-oidc/webpack-extra.config.js
@@ -3,16 +3,13 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const pluginRoot = __dirname + '/../../../';
 module.exports = {
   plugins: [
-    new CopyWebpackPlugin(
-      [
+    new CopyWebpackPlugin({
+      patterns: [
         {
           from: pluginRoot + 'node_modules/oidc-client/dist/oidc-client.min.js',
           to: pluginRoot + 'auth/public/auth-oidc'
         }
-      ],
-      {
-        verbose: true
-      }
-    )
+      ]
+    })
   ]
 };
