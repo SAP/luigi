41f5b97d15889049e45f7182abd71acf8fc6d92b
Ndricim Rrapi <ndricim.rrapi@sap.com>
2023-02-02 11:22:00 +0100

    Refactor

--- container/src/constants/event-type.ts
+++ container/src/constants/event-type.ts
@@ -1,27 +1,3 @@
+// TODO: Add and extend event to inclide custom typings/interface to make it easier to use on the listener parameter
 export interface ParamsEvent extends Event {
-    // isAcceptable(s: string): boolean;
-}
-
-export interface ParamsEvent2 extends Event {
-    // isAcceptable(s: string): boolean;
-}
-
-export interface ParamsEvent3 extends Event {
-    // isAcceptable(s: string): boolean;
-}
-
-export interface ParamsEvent4 extends Event {
-    // isAcceptable(s: string): boolean;
-}
-
-export interface ParamsEvent5 extends Event {
-    // isAcceptable(s: string): boolean;
-}
-
-export interface ParamsEvent6 extends Event {
-    // isAcceptable(s: string): boolean;
-}
-
-export interface ParamsEvent7 extends Event {
-    // isAcceptable(s: string): boolean;
 }
