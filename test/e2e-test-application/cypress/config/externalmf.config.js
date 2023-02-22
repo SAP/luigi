125bc243941b9e119953e96be854ba859a1dbd6b
Ndricim Rrapi <ndricim.rrapi@sap.com>
2023-02-22 20:49:43 +0100

    Add project id

--- test/e2e-test-application/cypress/config/externalmf.config.js
+++ test/e2e-test-application/cypress/config/externalmf.config.js
@@ -1,15 +1,16 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  projectId: 'czq7qc',
   e2e: {
-    fixturesFolder: "cypress/e2e/fixtures",
-    specPattern: "cypress/e2e/test2",
-    supportFile: "cypress/e2e/support/index.js",
-    videosFolder: "cypress/e2e/videos",
-    screenshotsFolder: "cypress/screenshots",
+    fixturesFolder: 'cypress/e2e/fixtures',
+    specPattern: 'cypress/e2e/test2',
+    supportFile: 'cypress/e2e/support/index.js',
+    videosFolder: 'cypress/e2e/videos',
+    screenshotsFolder: 'cypress/screenshots',
     chromeWebSecurity: true,
     viewportWidth: 1250,
     viewportHeight: 790,
-    baseUrl: "http://localhost:4200/"
+    baseUrl: 'http://localhost:4200/'
   }
 });
