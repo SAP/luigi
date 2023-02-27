e4a9a597793eac6128b0b3e7c23f6d8d8c9e950d
Ndricim Rrapi <ndricim.rrapi@sap.com>
2023-02-27 17:22:25 +0100

    Remove comments

--- core/examples/luigi-example-vue/src/router/index.js
+++ core/examples/luigi-example-vue/src/router/index.js
@@ -3,24 +3,6 @@ import Home from '../views/Home.vue';
 import Sample1 from '../views/Sample1.vue';
 import Sample2 from '../views/Sample2.vue';
 
-// const routes = [
-//   {
-//     path: '/',
-//     name: 'home',
-//     component: HomeView
-//   },
-//   {
-//     path: '/about',
-//     name: 'about',
-//     // route level code-splitting
-//     // this generates a separate chunk (about.[hash].js) for this route
-//     // which is lazy-loaded when the route is visited.
-//     component: function () {
-//       return import(/* webpackChunkName: "about" *../views/Sample1.vuevue')
-//     }
-//   }
-// ]
-
 const routes = [
   {
     path: '/home',
