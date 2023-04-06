9c29803c9ec2d270b54ff872a58bb98728833218
Philipp Pracht <philipp.pracht@sap.com>
2023-04-06 15:51:54 +0200

    less, more self-explaining code ;)

--- core/src/services/routing.js
+++ core/src/services/routing.js
@@ -612,21 +612,16 @@ class RoutingClass {
   }
 
   navigateToExternalLink(externalLink, node, pathParams) {
-    let updatedExternalLink = {
-      ...NAVIGATION_DEFAULTS.externalLink,
-      ...(node?.externalLink || {})
-    };
-    if (externalLink) {
-      if (GenericHelpers.isObject(externalLink)) {
-        updatedExternalLink = externalLink;
-      } else {
-        updatedExternalLink.url = externalLink;
-      }
-    }
     if (node) {
+      const updatedExternalLink = {
+        ...NAVIGATION_DEFAULTS.externalLink,
+        ...(node?.externalLink || {})
+      };
       updatedExternalLink.url = RoutingHelpers.calculateNodeHref(node, pathParams);
+      window.open(updatedExternalLink.url, updatedExternalLink.sameWindow ? '_self' : '_blank').focus();
+    } else if (GenericHelpers.isObject(externalLink)) {
+      window.open(externalLink.url, externalLink.sameWindow ? '_self' : '_blank').focus();
     }
-    window.open(updatedExternalLink.url, updatedExternalLink.sameWindow ? '_self' : '_blank').focus();
   }
 
   /**
