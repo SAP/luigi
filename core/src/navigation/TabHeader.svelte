e76dd38fb7a9716cbbf7e76f5cc7151cb7f6ea92
Ndricim Rrapi <ndricim.rrapi@sap.com>
2023-04-11 16:37:27 +0200

    Add showAsTabHeader check

--- core/src/navigation/TabHeader.svelte
+++ core/src/navigation/TabHeader.svelte
@@ -21,11 +21,11 @@
   };
 
   onMount(() => {
-    // render webcomponent based on pased node object 
-    if (node.webcomponent) {
+    // render webcomponent based on pased node object only if it is a webcomponent and showAsTabHeader is set to true
+    if (node.webcomponent && node.tabNav.showAsTabHeader) {
       WebComponentService.renderWebComponent(node.viewUrl, document.querySelector('.lui-tab-header'), node.context, node);
     } else { 
-      console.warn('Horizontal navigation custom header microfrontend requires a webcomponent type node.')
+      console.warn('Horizontal navigation custom header microfrontend requires a webcomponent type node and tabNav.showAsTabHeader property set.')
     }
   });
 
