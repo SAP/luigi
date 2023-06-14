921ec0132e5e57f626129971997243a121315f36
Ndricim Rrapi <ndricim.rrapi@sap.com>
2023-06-14 17:59:20 +0200

    test

--- website/docs/vite.config.ts
+++ website/docs/vite.config.ts
@@ -1,8 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [
+    sveltekit()
+    // viteStaticCopy({
+    //   targets: [
+    //     {
+    //       src: 'build/docs',
+    //       dest: 'public/docs'
+    //     }
+    //   ]
+    // })
+  ],
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}']
   },
