4dc25b8f4d472d5998741dda0dc91103412937d8
Ndricim Rrapi <ndricim.rrapi@sap.com>
2023-06-14 10:59:02 +0200

    Fix json not found

--- website/docs/src/markdown-conversion/plugins/buildDocsContent.js
+++ website/docs/src/markdown-conversion/plugins/buildDocsContent.js
@@ -5,7 +5,7 @@ import { getParsedDocs } from './parser.js';
 getParsedDocs().then(function(a) {
   var __dirname = path.resolve();
   var dataFolder = __dirname + '/src/data';
-  var docsFilePath = dataFolder + '/docs';
+  var docsFilePath = dataFolder + '/docs.json';
   writeFileSync(docsFilePath, JSON.stringify(a));
 });
 console.log('Generating docs file to save to');
