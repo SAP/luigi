9e1738db88ca5b6330a65a32aa55f708d27f7bd4
Philipp Pracht <philipp.pracht@sap.com>
2023-06-22 14:38:57 +0200

    Transfer theme vars (#3234)

--- core/test/core-api/theming.spec.js
+++ core/test/core-api/theming.spec.js
@@ -7,26 +7,26 @@ import { LuigiConfig, LuigiTheming } from '../../src/core-api';
 
 describe('Core API - Theming', function() {
   jest.retryTimes(2);
-  const getMockTheming = () => {
-    return {
-      themes: [
-        { id: 'light', name: 'Light Theme' },
-        { id: 'dark', name: 'Dark Theme' }
-      ],
-      defaultTheme: 'light'
-    };
-  };
-  beforeEach(() => {
-    sinon.stub(LuigiConfig);
-    LuigiConfig.getConfigValue.returns(getMockTheming());
-    LuigiConfig.getConfigValueAsync.returns(getMockTheming().themes);
-  });
-  afterEach(() => {
-    sinon.reset();
-    sinon.restore();
-  });
 
   describe('getAvailableThemes', () => {
+    const getMockTheming = () => {
+      return {
+        themes: [
+          { id: 'light', name: 'Light Theme' },
+          { id: 'dark', name: 'Dark Theme' }
+        ],
+        defaultTheme: 'light'
+      };
+    };
+    beforeEach(() => {
+      sinon.stub(LuigiConfig);
+      LuigiConfig.getConfigValue.returns(getMockTheming());
+      LuigiConfig.getConfigValueAsync.returns(getMockTheming().themes);
+    });
+    afterEach(() => {
+      sinon.reset();
+      sinon.restore();
+    });
     it('has themes', async () => {
       const res = await LuigiTheming.getAvailableThemes();
       assert.deepEqual(res, getMockTheming().themes);
@@ -46,6 +46,24 @@ describe('Core API - Theming', function() {
     assert.deepEqual(LuigiTheming.currentTheme, mockTheme);
   });
   describe('getThemeObject', () => {
+    const getMockTheming = () => {
+      return {
+        themes: [
+          { id: 'light', name: 'Light Theme' },
+          { id: 'dark', name: 'Dark Theme' }
+        ],
+        defaultTheme: 'light'
+      };
+    };
+    beforeEach(() => {
+      sinon.stub(LuigiConfig);
+      LuigiConfig.getConfigValue.returns(getMockTheming());
+      LuigiConfig.getConfigValueAsync.returns(getMockTheming().themes);
+    });
+    afterEach(() => {
+      sinon.reset();
+      sinon.restore();
+    });
     it('theme not found', async () => {
       const res = await LuigiTheming.getThemeObject('notexistingtheme');
       assert.isUndefined(res);
@@ -59,6 +77,84 @@ describe('Core API - Theming', function() {
     it('get theme object', async () => {
       const res = await LuigiTheming.getThemeObject('dark');
       assert.deepEqual(res, { id: 'dark', name: 'Dark Theme' });
+    });
+  });
+
+  describe('getCSSVariables', () => {
+    beforeEach(() => {
+      sinon.stub(LuigiConfig);
+    });
+    afterEach(() => {
+      sinon.reset();
+      sinon.restore();
+      delete window.Luigi.__cssVars;
+    });
+    it('getCSSVariables fiori style', async () => {
+      window.__luigiThemeVars = ['sapBackgroundColor'];
+      LuigiConfig.getConfigValue.withArgs('settings.theming.variables').returns('fiori');
+      sinon.stub(window, 'getComputedStyle').callsFake(() => {
+        return {
+          getPropertyValue: sinon.stub().returns('green')
+        };
+      });
+      assert.equal(window.Luigi.__cssVars, undefined);
+      await LuigiTheming.getCSSVariables();
+      assert.equal(window.Luigi.__cssVars.sapBackgroundColor, 'green');
+    });
+    it('getCSSVariables own file', async () => {
+      LuigiConfig.getConfigValue.withArgs('settings.theming.variables.file').returns('/path/to/file');
+      global.fetch = jest.fn(() =>
+        Promise.resolve({
+          json: () => Promise.resolve({ root: { myOwnCSSVar: 'red' } })
+        })
+      );
+      sinon.stub(window, 'getComputedStyle').callsFake(() => {
+        return {
+          getPropertyValue: sinon.stub().returns('green')
+        };
+      });
+      assert.equal(window.Luigi.__cssVars, undefined);
+      await LuigiTheming.getCSSVariables();
+      assert.equal(window.Luigi.__cssVars.myOwnCSSVar, 'green');
+    });
+    it('getCSSVariables own file with error', async () => {
+      LuigiConfig.getConfigValue.withArgs('settings.theming.variables.file').returns('/path/to/file');
+      console.error = sinon.spy();
+      global.fetch = jest.fn(() =>
+        Promise.resolve({
+          json: () => Promise.reject('error')
+        })
+      );
+      sinon.stub(window, 'getComputedStyle').callsFake(() => {
+        return {
+          getPropertyValue: sinon.stub().returns('green')
+        };
+      });
+      assert.equal(window.Luigi.__cssVars, undefined);
+      await LuigiTheming.getCSSVariables();
+      assert.equal(window.Luigi.__cssVars, undefined);
+      sinon.assert.calledOnce(console.error);
+    });
+    it('getCSSVariables own file with custom error handling', async () => {
+      LuigiConfig.getConfigValue.withArgs('settings.theming.variables.file').returns('/path/to/file');
+      LuigiConfig.getConfigValue.withArgs('settings.theming.variables.errorHandling').returns(() => {
+        console.log('This is my custom error');
+      });
+      console.log = sinon.spy();
+      global.fetch = jest.fn(() =>
+        Promise.resolve({
+          json: () => Promise.reject('error')
+        })
+      );
+      sinon.stub(window, 'getComputedStyle').callsFake(() => {
+        return {
+          getPropertyValue: sinon.stub().returns('green')
+        };
+      });
+      assert.equal(window.Luigi.__cssVars, undefined);
+      await LuigiTheming.getCSSVariables();
+      assert.equal(window.Luigi.__cssVars, undefined);
+      sinon.assert.calledOnce(console.log);
     });
   });
 });
