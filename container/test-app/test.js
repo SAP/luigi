af98e606fbd94bc35104b02dd6aec9309bf5a2b2
Doberer, Johannes <johannes.doberer@sap.com>
2023-07-19 22:06:01 +0200

    adjust example

--- container/test-app/test.js
+++ container/test-app/test.js
@@ -1,7 +1,7 @@
-import { LuigiElement } from 'https://unpkg.com/@luigi-project/client@2.2.1/luigi-element.js';
+import { LuigiElement } from 'https://unpkg.com/@luigi-project/client@2.2.2-dev.202307191937/luigi-element.js';
 export default class extends LuigiElement {
   constructor() {
-    super({ openShadow: 'open', deferLuigiClientWCInit: false });
+    super({ openShadow: 'open', deferLuigiClientWCInit: true });
   }
   render() {
     this._shadowRoot.innerHTML = `<div>Das ist ein TESt</div`;
@@ -10,6 +10,7 @@ export default class extends LuigiElement {
   connectedCallback() {
     setTimeout(() => {
       this.LuigiClient.luigiClientInit();
+      console.log('LuigiClient initialized for LuigiElement WC');
     }, 3000);
   }
 }
