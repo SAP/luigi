bfe39d2075f04d0f967fd4422361f489d64d9634
Doberer, Johannes <johannes.doberer@sap.com>
2023-07-24 21:48:00 +0200

    onload for compound

--- container/src/LuigiCompoundContainer.svelte
+++ container/src/LuigiCompoundContainer.svelte
@@ -1,9 +1,13 @@
 <svelte:options tag={null} accessors={true} />
 
 <script lang="ts">
-  // export let viewurl;
+  export let viewurl;
   export let context;
   // export let label;
+  // if `true` at LuigiContainer tag, LuigiContainer sends an event `initialzed` to mfe. Mfe is immediately ready.
+  export let initialize_mfe_directly;
+  // once the microfrontend is initialized, this value is set to `true`.
+  export let is_mfe_initialized;
   let compoundConfig;
 
   let initialized = false;
@@ -14,6 +18,7 @@
   import { get_current_component } from 'svelte/internal';
   import { ContainerService } from './services/container.service';
   import { WebComponentService } from './services/webcomponents.service';
+  import { Events } from './constants/communication';
 
   const containerService = new ContainerService();
   const webcomponentService = new WebComponentService();
@@ -22,12 +27,12 @@
       linkManager: () => {}, //window.Luigi.navigation,
       uxManager: () => {
         return {
-          showAlert: (alertSettings) => {
+          showAlert: alertSettings => {
             dispatchLuigiEvent('alert-request', alertSettings, {});
           },
-          showConfirmationModal: async (settings) => {
+          showConfirmationModal: async settings => {
             return new Promise((resolve, reject) => {
-              dispatchLuigiEvent('confirmation-request', settings, (data) => {
+              dispatchLuigiEvent('confirmation-request', settings, data => {
                 if (data) {
                   resolve(data);
                 } else {
@@ -35,39 +40,69 @@
                 }
               });
             });
-          },
+          }
         };
       }, //window.Luigi.ux,
       getCurrentLocale: () => {}, //() => window.Luigi.i18n().getCurrentLocale(),
-      publishEvent: (ev) => {
+      publishEvent: ev => {
         if (eventBusElement && eventBusElement.eventBus) {
           eventBusElement.eventBus.onPublishEvent(ev, nodeId, wc_id);
         }
       },
+      luigiClientInit: () => {
+        dispatchLuigiEvent(Events.INITIALIZED, {});
+      }
     };
   };
 
   const thisComponent = get_current_component();
   let deferInit = !!thisComponent.attributes['defer-init'];
 
+  function canMfeInitialized(): boolean {
+    return !!initialize_mfe_directly;
+  }
+
   thisComponent.init = () => {
-    if (!thisComponent.compoundConfig || initialized) return;
+    if (!thisComponent.compoundConfig || initialized) {
+      console.log('return from init');
+      return;
+    }
     deferInit = false;
     const node = {
       compound: thisComponent.compoundConfig,
+      viewUrl: viewurl ? viewurl : undefined,
+      webcomponent: true
     }; // TODO: fill with sth
     webcomponentService
       .renderWebComponentCompound(node, mainComponent, context)
-      .then((compCnt) => {
+      .then(compCnt => {
         eventBusElement = compCnt;
+        console.log(
+          'tagName',
+          customElements.get((compCnt as HTMLElement).tagName)
+        );
+        console.log('compCnt', (compCnt as HTMLElement).tagName);
+        if (canMfeInitialized()) {
+          is_mfe_initialized = true;
+          setTimeout(() => {
+            dispatchLuigiEvent(Events.INITIALIZED, {});
+          });
+        } else if ((compCnt as HTMLElement).tagName) {
+          console.log('ELSE');
+          eventBusElement.addEventListener('wc_ready', () => {
+            if (!(eventBusElement as any).deferLuigiClientWCInit) {
+              is_mfe_initialized = true;
+              dispatchLuigiEvent(Events.INITIALIZED, {});
+            }
+          });
+        }
       });
-
     initialized = true;
   };
 
   containerService.registerContainer(thisComponent);
 
-  function dispatchLuigiEvent(msg, data, callback) {
+  function dispatchLuigiEvent(msg: string, data: any, callback?: Function) {
     containerService.dispatch(msg, thisComponent, data, callback);
   }
 
---
+++ container/test-app/nested.js
@@ -0,0 +1,31 @@
+import { LuigiElement } from './luigi-element.js';
+export default class extends LuigiElement {
+  constructor() {
+    super({ openShadow: 'open', deferLuigiClientWCInit: true });
+
+    const template = document.createElement('template');
+
+    template.innerHTML = `
+        <section style="display: flex; height: 100%; flex-direction: column">
+          <header><slot name="header">header</slot></header>
+          <main style="flex: auto; overflow: auto"><slot name="content">content</slot><slot></slot></main>
+          <footer><slot name="footer">footer</slot></footer>
+        </section>
+      `;
+    // this._shadowRoot = this.attachShadow({ mode: 'open', delegatesFocus: false });
+    this._shadowRoot.appendChild(template.content.cloneNode(true));
+    // this.$paragraph = this._shadowRoot.querySelector('p');
+  }
+
+  set context(ctx) {
+    // this.$paragraph.innerHTML = 'TEST';
+  }
+
+  connectedCallback() {
+    console.log('connectedCallback');
+    setTimeout(() => {
+      this.LuigiClient.luigiClientInit();
+      console.log('LuigiClient initialized for LuigiElement WC');
+    }, 10000);
+  }
+}
