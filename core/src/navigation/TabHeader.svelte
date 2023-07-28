636019a934780ecdbcecdaca032e71ee96fe1e3e
Philipp Pracht <philipp.pracht@sap.com>
2023-07-28 09:51:14 +0200

    tab header wc  ctx update

--- core/src/navigation/TabHeader.svelte
+++ core/src/navigation/TabHeader.svelte
@@ -7,31 +7,27 @@
 
   export let node;
 
-  // define get and setters 
-  const __this = {
-    get: () => ({
-      node
-    }),
-    set: obj => {
-      if (obj) {
-        Object.getOwnPropertyNames(obj).forEach(prop => {
-          if (prop === 'node') {
-            node = obj.node;
-          }
-        });        
-      }
-    }
-  };
+
+  async function getCurrentContext() {
+    const route = RoutingHelpers.mapPathToNode(Routing.getCurrentPath(), node);
+    const data = await Navigation.extractDataFromPath(route);
+    return data?.pathData?.context || node.context;
+  }
 
   onMount(() => {
     
     document.querySelector('.lui-tab-header').innerHTML = '';
     setTimeout(async ()=>{
       // render webcomponent based on passed node object only if it is a webcomponent and showAsTabHeader is set to true
-      if (node.webcomponent && node.tabNav.showAsTabHeader) {
-        const route = RoutingHelpers.mapPathToNode(Routing.getCurrentPath(), node);
-        const data = await Navigation.extractDataFromPath(route);
-        WebComponentService.renderWebComponent(node.viewUrl, document.querySelector('.lui-tab-header'), data?.pathData?.context || node.context, node);
+      if (node.webcomponent && node.tabNav.showAsTabHeader) {        
+        const tabHeaderCnt = document.querySelector('.lui-tab-header');
+        WebComponentService.renderWebComponent(node.viewUrl, tabHeaderCnt, await getCurrentContext(), node);
+        tabHeaderCnt.addEventListener('lui_ctx_update', async () => {
+          const wc = document.querySelector('.lui-tab-header [lui_web_component]');
+          if (wc) {
+            wc.context = await getCurrentContext();
+          }
+        });
       } else { 
         console.warn('Horizontal navigation custom header microfrontend requires a webcomponent type node and tabNav.showAsTabHeader property set.')
       }
