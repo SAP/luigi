15b6aebf474d49c20138e9b212d0b1c0dd38ce13
Ndricim Rrapi <ndricim.rrapi@sap.com>
2023-07-31 17:47:53 +0200

    fix pckgjson

--- container/cypress.config.ts
+++ container/cypress.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  projectId: 'czq7qc',
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    }
+    viewportWidth: 1250,
+    viewportHeight: 790,
+    chromeWebSecurity: false,
+    baseUrl: 'http://localhost:8080/'
   }
 });
