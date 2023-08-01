f8245dc45eedbdfdf867959f9012ae861f4e4fb1
Doberer, Johannes <johannes.doberer@sap.com>
2023-08-01 17:20:32 +0200

    add more projects

--- scripts/whiteSource.sh
+++ scripts/whiteSource.sh
@@ -11,6 +11,9 @@ declare -a FOLDERS=("client"
                     "website/fiddle"
                     "plugins"
                     "container"
+                    "client-framework-support/client-support-ui5"
+                    "client-framework-support/client-support-angular"
+                    "client-framework-support/testing-utilities"
                    )
 
 # Project tokens for corresponding folders
@@ -20,6 +23,9 @@ declare -a PROJECT_TOKENS=("a63fd5aaaa2343199327aac6d3e2b5346e930927d66441bf9211
                            "8b3ce944979d49c8b1d2917533199aeb4e73a993843f4820a509a33932e18a29"
                            "56eb623145264763a82330025e4f11efd538074a3cd04791ab6ab87293174aca"
                            "9bbbcde5bec3443b872ca3ceab2d223576b63221f437432fa182e9a57c684d02"
+                           "e24f24fa43fc4a47974d2691f39012e04cbd76efa77d4d0db0b3c82c42f0ee58"
+                           "314e91b6c0864db09077ab4af1c802ffce5af09c2c314065bb8868f8b0393ce1"
+                           "ba242462d46e4d98870ae5d00b95ec3c4782cf2647da44868be0fc3a0803caca"
                           )
 
 CACHED_WHITESOURCE_UA=~/.cache/whitesource/wss-unified-agent.jar
