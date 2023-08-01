002aaa310febb6213cd59081bbc25a5afd9d43f6
Doberer, Johannes <johannes.doberer@sap.com>
2023-08-01 16:56:03 +0200

    add projects to whitesource

--- scripts/whiteSource.sh
+++ scripts/whiteSource.sh
@@ -9,7 +9,8 @@ declare -a FOLDERS=("client"
                     "core"
                     "website/landingpage/dev"
                     "website/fiddle"
-                    "plugins"
+                    "plugins",
+                    "container"
                    )
 
 # Project tokens for corresponding folders
@@ -18,6 +19,7 @@ declare -a PROJECT_TOKENS=("a63fd5aaaa2343199327aac6d3e2b5346e930927d66441bf9211
                            "0bc00859083a4cf98e1246c91cabc56a4623fb69f1f24cdd8ca10f9f0d3dfbb4"
                            "8b3ce944979d49c8b1d2917533199aeb4e73a993843f4820a509a33932e18a29"
                            "56eb623145264763a82330025e4f11efd538074a3cd04791ab6ab87293174aca"
+                           "9bbbcde5bec3443b872ca3ceab2d223576b63221f437432fa182e9a57c684d02"
                           )
 
 CACHED_WHITESOURCE_UA=~/.cache/whitesource/wss-unified-agent.jar
