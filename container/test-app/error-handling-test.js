ef2f3a0269507de4c7360de7245cc9f6560980ea
Ndricim Rrapi <ndricim.rrapi@sap.com>
2023-08-02 15:12:03 +0200

    Add error handler for container (#3363)

---
+++ container/test-app/error-handling-test.js
@@ -0,0 +1,52 @@
+/**
+ * This class is used to test LuigiClient webcomponent based functionality
+ */
+export default class extends HTMLElement {
+  constructor() {
+    super();
+    const template = document.createElement('template');
+    template.innerHTML = `<section><p>Hello World - Error Handling!</p></section>`;
+
+    const templateBtn = document.createElement('template');
+    templateBtn.innerHTML = '<button>Click me!</button>';
+
+    const empty = document.createElement('template');
+    empty.innerHTML = `<section><p>Test!</p><br/><br/></section>`;
+
+    this._shadowRoot = this.attachShadow({
+      mode: 'open',
+      delegatesFocus: false
+    });
+    this._shadowRoot.appendChild(template.content.cloneNode(true));
+    this._shadowRoot.appendChild(templateBtn.content.cloneNode(true));
+
+    for (let index = 0; index < 10; index++) {
+      this._shadowRoot.appendChild(empty.content.cloneNode(true));
+    }
+
+    this.$paragraph = this._shadowRoot.querySelector('p');
+    this.$button = this._shadowRoot.querySelector('button');
+    this.$button.addEventListener('click', () => {
+      if (this.LuigiClient) {
+        this.LuigiClient.uxManager().showAlert({
+          text: 'Hello from uxManager in Web Component, Language:' + this.LuigiClient.getCurrentLocale(),
+          type: 'info'
+        });
+
+        this.LuigiClient.uxManager().showAlert({
+          text: 'Active feature toggles list: ' + this.LuigiClient.getActiveFeatureToggles(),
+          type: 'info'
+        });
+
+        this.LuigiClient.uxManager().showAlert({
+          text: 'Active feature toggles: ' + this.LuigiClient.uxManager().getCurrentTheme(),
+          type: 'info'
+        });
+      }
+    });
+  }
+
+  set context(ctx) {
+    this.$paragraph.innerHTML = ctx.title;
+  }
+}
