1aa631c36cd0b94260450dac9aba12c83c3142be
Doberer, Johannes <johannes.doberer@sap.com>
2023-08-08 18:58:04 +0200

    test

--- test/e2e-test-application/cypress/e2e/tests/0-js-test-app/js-test-app-history-for-modals.cy.js
+++ test/e2e-test-application/cypress/e2e/tests/0-js-test-app/js-test-app-history-for-modals.cy.js
@@ -94,7 +94,7 @@ describe('JS-TEST-APP', () => {
           openNodeInModal: true
         });
       });
-      it('Path routing, open modal and close via [X]', () => {
+      it('Path routing, open modal and close via [x]', () => {
         cy.vistTestAppPathRouting('', newConfig);
         cy.get('#app[configversion="js-test-app-history-handling-modals-1"]');
         openModal();
