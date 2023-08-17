3ec4ff61cb0377d6f6eb6f3dc3c045459c782ef0
Philipp Pracht <philipp.pracht@sap.com>
2023-08-17 20:10:08 +0200

    tests

--- container/test/services/webcomponents.service.spec.ts
+++ container/test/services/webcomponents.service.spec.ts
@@ -1,10 +1,5 @@
 import { WebComponentService } from '../../src/services/webcomponents.service';
 
-const chai = require('chai');
-const expect = chai.expect;
-const assert = chai.assert;
-const sinon = require('sinon');
-
 describe('Webcomponents Service', () => {
   let service;
   beforeEach(() => {
@@ -13,6 +8,6 @@ describe('Webcomponents Service', () => {
 
   it('generateWCId', () => {
     const wcId = service.generateWCId('http://localhost:4200/foo/bar');
-    expect(wcId).to.equal('luigi-wc-687474703a2f2f6c6f63616c686f73743a343230302f666f6f2f626172');
+    expect(wcId).toEqual('luigi-wc-687474703a2f2f6c6f63616c686f73743a343230302f666f6f2f626172');
   });
 });
