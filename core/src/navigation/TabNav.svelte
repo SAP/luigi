0fc45a64ba42d1abe6822c524fb1bdae19fac813
Doberer, Johannes <johannes.doberer@sap.com>
2023-08-23 09:00:12 +0200

    statusbadge-horizontal-nav

--- core/src/navigation/TabNav.svelte
+++ core/src/navigation/TabNav.svelte
@@ -3,6 +3,7 @@
   import { Navigation } from './services/navigation';
   import { NavigationHelpers, RoutingHelpers, StateHelpers } from '../utilities/helpers';
   import { LuigiConfig } from '../core-api';
+  import StatusBadge from './StatusBadge.svelte';
   
   import TabHeader from './TabHeader.svelte'; 
 
@@ -278,7 +279,10 @@
                         aria-selected={node === selectedNodeForTabNav}
                         on:click|preventDefault={() => handleClick(node)}
                       >
-                        <span class="fd-tabs__tag">{getNodeLabel(node)}</span>
+                        <span class="fd-tabs__tag"
+                          >{getNodeLabel(node)}
+                          <StatusBadge {node} /></span
+                        >
                       </a>
                     </span>
                   {/if}
@@ -324,7 +328,8 @@
                                   aria-selected={node === selectedNodeForTabNav}
                                 >
                                   <span class="fd-menu__title"
-                                    >{getNodeLabel(node)}</span
+                                    >{getNodeLabel(node)}
+                                    <StatusBadge {node} /></span
                                   >
                                 </a>
                               </li>
@@ -371,7 +376,8 @@
                           aria-selected={node === selectedNodeForTabNav}
                         >
                           <span class="fd-nested-list__title"
-                            >{getNodeLabel(node)}</span
+                            >{getNodeLabel(node)}
+                            <StatusBadge {node} /></span
                           >
                         </a>
                       </li>
@@ -436,7 +442,8 @@
                                 aria-selected={node === selectedNodeForTabNav}
                               >
                                 <span class="fd-nested-list__title"
-                                  >{getNodeLabel(node)}</span
+                                  >{getNodeLabel(node)}
+                                  <StatusBadge {node} /></span
                                 >
                               </a>
                             </li>
