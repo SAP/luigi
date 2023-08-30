aa5d5160e64e5b7e51ce4a51a2b26880e9b31fc0
Philipp Pracht <philipp.pracht@sap.com>
2023-08-30 17:57:20 +0200

    remove typings from public

--- container/public/LuigiCompoundContainer.svelte.d.ts
+++
@@ -1,8 +0,0 @@
-export default class LuigiCompoundContainer extends HTMLElement {
-  compoundConfig: any;
-
-  /**
-   * Manually triggers the micro frontend rendering process when using defer-init attribute
-   */
-  init(): Function;
-}
