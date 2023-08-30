5f6aacd5741b58358fb9a8177ccd5f25a3db2409
Philipp Pracht <philipp.pracht@sap.com>
2023-08-30 18:24:40 +0200

    Eslint infra for container (#3419)

---
+++ container/.eslintrc.cjs
@@ -0,0 +1,45 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'eslint:recommended',
+        "plugin:svelte/recommended",
+    ],
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.json'],
+        extraFileExtensions: ['.svelte']
+    },
+    env: {
+        es6: true,
+        browser: true
+    },
+    rules: {
+        // override/add rules settings here, such as:
+        // 'svelte/rule-name': 'error'
+    },
+    overrides: [
+        {
+            files: ["*.svelte"],
+            parser: "svelte-eslint-parser",
+            parserOptions: {
+                parser: '@typescript-eslint/parser'
+            }
+        }
+    ],
+    plugins: ['@typescript-eslint'],
+    ignorePatterns: [
+        'node_modules',
+        'test-app',
+        '*.config.js',
+        'public/*.js',
+        'public/index.d.ts',
+        'coverage',
+        'cypress',
+        '*.md',
+        '*.map',
+        '*.json',
+        '.*.*'
+    ]
+}
