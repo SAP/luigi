74b864aad8c42d1f0cd7f8c28921cc6b1137899a
Johannes Doberer <johannes.doberer@sap.com>
2023-08-30 14:17:55 +0200

    Return empty object for getContext (#3405)

--- client-frameworks-support/client-support-angular/projects/client-support-angular/src/lib/service/luigi-context-service.ts
+++ client-frameworks-support/client-support-angular/projects/client-support-angular/src/lib/service/luigi-context-service.ts
@@ -9,7 +9,7 @@ export abstract class LuigiContextService {
   abstract contextObservable(): Observable<IContextMessage>;
 
   /**
-   * Get latest set context object (can be null/undefined, if not set yet)
+   * Get latest set context object
    */
   abstract getContext(): Context;
 
