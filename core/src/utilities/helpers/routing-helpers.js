f14d4c36c2ab170330154f3ed4d6b844cf80bef7
Doberer, Johannes <johannes.doberer@sap.com>
2023-10-04 12:08:01 +0200

    format

--- core/src/utilities/helpers/routing-helpers.js
+++ core/src/utilities/helpers/routing-helpers.js
@@ -210,7 +210,11 @@ class RoutingHelpersClass {
   buildRoute(node, path, params) {
     return !node.parent
       ? path + (params ? '?' + params : '')
-      : this.buildRoute(node.parent, `${node.parent.pathSegment === '' ? `${path}` : `/${node.parent.pathSegment}${path}`}`, params);
+      : this.buildRoute(
+          node.parent,
+          `${node.parent.pathSegment === '' ? `${path}` : `/${node.parent.pathSegment}${path}`}`,
+          params
+        );
   }
 
   /**
