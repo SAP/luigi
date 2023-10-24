472f9edbd46e6a84b995cb2b657ef1683a7fbfca
Ndricim Rrapi <ndricim.rrapi@sap.com>
2023-10-24 11:19:16 +0200

    Add container-api base tests

--- container/test/api/container-api.spec.ts
+++ container/test/api/container-api.spec.ts
@@ -1,7 +1,79 @@
-describe('Container Service', () => {
+import { LuigiInternalMessageID } from '../../src/constants/internal-communication';
+import { Events } from '../../src/constants/communication';
+import { ContainerAPIFunctions } from '../../src/api/container-api';
+import { containerService } from '../../src/services/container.service';
 
-    it('test', () => {
-        const a = 1;
-        expect(a).toEqual(1);
+
+describe('Container Service', () => {
+    describe('updateContext', () => {
+        let containerAPI = new ContainerAPIFunctions();
+
+        it('iframeHandle exists', () => {
+            // mock and spy
+            const contextObj = {};
+            const internal = {};
+            const iframeHandle = {
+                data: 'test'
+            };
+            containerService.sendCustomMessageToIframe = jest.fn();
+            const spy = jest.spyOn(containerService, 'sendCustomMessageToIframe');
+
+            containerAPI.updateContext(contextObj, internal,iframeHandle);
+
+            expect(spy).toHaveBeenCalledWith(iframeHandle, {context: contextObj, internal: internal }, LuigiInternalMessageID.SEND_CONTEXT_OBJECT)
+        });
     });
+
+    describe('closeAlert', () => {
+        let containerAPI = new ContainerAPIFunctions();
+
+        it('internal method properly called', () => {
+            // mock and spy
+            const id = 'some-id';
+            const dismissKey = 'key';
+            const iframeHandle = {
+                data: 'test'
+            };
+            containerService.sendCustomMessageToIframe = jest.fn();
+            const spy = jest.spyOn(containerService, 'sendCustomMessageToIframe');
+
+            containerAPI.closeAlert(id, dismissKey,iframeHandle );
+
+            expect(spy).toHaveBeenCalledWith(iframeHandle, {id, dismissKey}, LuigiInternalMessageID.ALERT_CLOSED)
+        });
+    });
+    
+
+    describe('sendCustomMessage', () => {
+        let containerAPI = new ContainerAPIFunctions();
+
+        it('isWebComponent microfrontend', () => {
+            // mock and spy
+            const id = 'some-id';
+            const mainComponent = {
+                _luigi_mfe_webcomponent: {
+                    testData: 'your wc data'
+                }
+            };
+            const data = {
+                sample: 'test'
+            };
+            const isWebComponent = true;
+            const iframeHandle = {
+                data: 'test'
+            };
+
+            containerService.dispatch = jest.fn();
+            const spy = jest.spyOn(containerService, 'dispatch');
+
+            containerAPI.sendCustomMessage(id, mainComponent, isWebComponent,iframeHandle, data );
+
+            expect(spy).toHaveBeenCalledWith(id, mainComponent._luigi_mfe_webcomponent, data)
+        });
+    });
+    
+
+    
+
+    
 });
