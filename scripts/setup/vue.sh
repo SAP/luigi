048b210a9672794f1cdaaad420ea87029edc9f29
Ndricim Rrapi <ndricim.rrapi@sap.com>
2023-11-06 10:59:46 +0100

    rename

--- scripts/setup/vue.sh
+++ scripts/setup/vue.sh
@@ -68,7 +68,7 @@ module.exports = {
 # fetch assets from vue example
 curl https://raw.githubusercontent.com/SAP/luigi/main/core/examples/luigi-example-vue/public/index.html > public/index.html
 curl https://raw.githubusercontent.com/SAP/luigi/main/core/examples/luigi-example-vue/public/sampleapp.html > public/sampleapp.html
-curl https://raw.githubusercontent.com/SAP/luigi/main/core/examples/luigi-example-vue/src/App.vue > src/App.vue
+curl https://raw.githubusercontent.com/SAP/luigi/main/core/examples/luigi-example-vue/src/app.vue > src/app.vue
 curl https://raw.githubusercontent.com/SAP/luigi/main/core/examples/luigi-example-vue/src/main.js > src/main.js
 curl https://raw.githubusercontent.com/SAP/luigi/main/core/examples/luigi-example-vue/public/luigi-config.js > public/luigi-config.js
 
