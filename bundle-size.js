d14c412d0c2d7e765c793797f80705c255305c4b
Ndricim Rrapi <ndricim.rrapi@sap.com>
2023-11-16 16:24:05 +0100

    Remove bundlesize pacakge in favor of own script, update gitignore, update package-locks

--- bundle-size.js
+++ bundle-size.js
@@ -1,26 +1,50 @@
 /**
- * This file is used to run the bundleSizeOnly scrpt to check if bundle size threshold is not exceded
+ * This file is used to check if bundle size threshold is not exceded
  */
 
-const { execSync } = require('child_process');
+const fs = require('fs');
 const path = require('path');
 
-// List of folders to run bundleSizeOnly script needed for pipeline
-const folders = ['core', 'client', 'plugins'];
+const filesAndLimits = [
+  {
+    path: 'client/public/luigi-client.js',
+    limit: 50,
+  },
+  {
+    path: 'core/public/luigi.js',
+    limit: 650,
+  },
+  {
+    path: 'core/public/luigi.css',
+    limit: 800,
+  },
+  {
+    path: 'plugins/auth/public/auth-oauth2/plugin.js',
+    limit: 75,
+  },
+];
 
-console.log('Running bundleSizeOnly to check core, client, plugins file size limit not exceeded');
-// Run "bundleSizeOnly" script in each folder
-for (const folder of folders) {
-  try {
-    console.log(`\x1b[33m\n\nRunning "bundleSizeOnly" in ${folder}\x1b[32m...\x1b[0m`);
-    const commandToRun = `npm run --prefix ${folder} bundlesizeOnly`;
-    console.log(commandToRun);
+async function checkFileSizes() {
+  for (const { path: filePath, limit } of filesAndLimits) {
+    const fullPath = path.resolve(__dirname, filePath);
+    console.log(`\x1b[33m\n\nChecking bundlesize in \x1b[32m${filePath}\x1b[0m`);
 
-    const logs = execSync(commandToRun, { encoding: 'utf-8', stdio: 'inherit' });
-    console.log(logs);
-    console.log(`\x1b[32m"bundleSizeOnly" completed successfully in ${folder}\x1b[0m\n\n`);
-  } catch (error) {
-    console.error(`Error running "bundleSizeOnly" in ${folder}:`, error.stderr, error.message);
-    process.exit(1);
+    try {
+      const stats = await fs.promises.stat(fullPath);
+      const fileSizeInKB = (stats.size / 1024).toFixed(2);
+
+      if (fileSizeInKB > limit) {
+        console.error(`\x1b[31m \n ERROR: ${filePath} exceeds the size limit.  Actual size: ${fileSizeInKB} KB, Limit: ${limit} KB ! \x1b[0m\n`);
+        process.exit(1);
+      } else {
+        console.log(`\x1b[32m\u2713\x1b[0m  ${filePath} is within the size limit. Size: \x1b[33m${fileSizeInKB} KB\x1b[0m < \x1b[32m${limit} KB\x1b[0m`);      }
+    } catch (error) {
+      console.error(`Error reading file ${filePath}: ${error.message}`);
+      process.exit(1);
+    }
   }
+
+  console.log('\x1b[32m\u2713\x1b[0m \x1b[32m All files are within their size limits.\x1b[0m \x1b[32m\u2713\x1b[0m');
 }
+
+checkFileSizes();
