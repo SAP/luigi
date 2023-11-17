2d58a224096dd68d4a8c1c98def8470be1748549
Ndricim Rrapi <ndricim.rrapi@sap.com>
2023-11-17 15:43:03 +0100

    Last update

--- scripts/setup/no-framework.sh
+++ scripts/setup/no-framework.sh
@@ -14,31 +14,20 @@ echo ""
 
 mkdir $folder && cd $folder
 
-# npm init -y > /dev/null
-
-# add "start" command to the package.json file. This command is split into 2 lines on purpose!
-# sed 's/"scripts": {/"scripts": {\
-# \   "start":"server public",/1' package.json > p.tmp.json && mv p.tmp.json package.json
-
-curl https://raw.githubusercontent.com/SAP/luigi/93d3e2436ca00dfc1595d4d586bfe10086d20ba9/core/examples/luigi-example-js/package.json > package.json
+# download the package.json
+curl https://raw.githubusercontent.com/SAP/luigi/main/core/examples/luigi-example-js/package.json > package.json
 
 npm i 
 mkdir -p public/
 mkdir -p public/views
 
 # download assets from core/examples folder
-# UNCOMMENT after testing
-# curl https://raw.githubusercontent.com/SAP/luigi/main/core/examples/luigi-example-js/public/index.html > public/index.html
-# curl https://raw.githubusercontent.com/SAP/luigi/main/core/examples/luigi-example-js/public/luigi-config.js > public/luigi-config.js
-# curl https://raw.githubusercontent.com/SAP/luigi/main/core/examples/luigi-example-js/public/views/home.html > public/views/home.html
-# curl https://raw.githubusercontent.com/SAP/luigi/main/core/examples/luigi-example-js/public/views/sample1.html > public/views/sample1.html
-
-curl https://raw.githubusercontent.com/SAP/luigi/93d3e2436ca00dfc1595d4d586bfe10086d20ba9/core/examples/luigi-example-js/public/favicon.ico > public/favicon.ico
-curl https://raw.githubusercontent.com/SAP/luigi/93d3e2436ca00dfc1595d4d586bfe10086d20ba9/core/examples/luigi-example-js/public/logo.png > public/logo.png
-curl https://raw.githubusercontent.com/SAP/luigi/93d3e2436ca00dfc1595d4d586bfe10086d20ba9/core/examples/luigi-example-js/public/index.html > public/index.html
-curl https://raw.githubusercontent.com/SAP/luigi/93d3e2436ca00dfc1595d4d586bfe10086d20ba9/core/examples/luigi-example-js/public/luigi-config.js > public/luigi-config.js
-curl https://raw.githubusercontent.com/SAP/luigi/93d3e2436ca00dfc1595d4d586bfe10086d20ba9/core/examples/luigi-example-js/public/views/home.html > public/views/home.html
-curl https://raw.githubusercontent.com/SAP/luigi/93d3e2436ca00dfc1595d4d586bfe10086d20ba9/core/examples/luigi-example-js/public/views/sample1.html > public/views/sample1.html
+curl https://raw.githubusercontent.com/SAP/luigi/main/core/examples/luigi-example-js/public/favicon.ico > public/favicon.ico
+curl https://raw.githubusercontent.com/SAP/luigi/main/core/examples/luigi-example-js/public/logo.png > public/logo.png
+curl https://raw.githubusercontent.com/SAP/luigi/main/core/examples/luigi-example-js/public/index.html > public/index.html
+curl https://raw.githubusercontent.com/SAP/luigi/main/core/examples/luigi-example-js/public/luigi-config.js > public/luigi-config.js
+curl https://raw.githubusercontent.com/SAP/luigi/main/core/examples/luigi-example-js/public/views/home.html > public/views/home.html
+curl https://raw.githubusercontent.com/SAP/luigi/main/core/examples/luigi-example-js/public/views/sample1.html > public/views/sample1.html
 
 echo "Running server with command: npm run start"
 npm run start
