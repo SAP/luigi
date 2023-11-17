4ac7439141964d03b339ba190e0c707ba0b39634
Ndricim Rrapi <ndricim.rrapi@sap.com>
2023-11-17 15:23:48 +0100

    test

--- scripts/setup/no-framework.sh
+++ scripts/setup/no-framework.sh
@@ -20,7 +20,7 @@ mkdir $folder && cd $folder
 # sed 's/"scripts": {/"scripts": {\
 # \   "start":"server public",/1' package.json > p.tmp.json && mv p.tmp.json package.json
 
-curl https://raw.githubusercontent.com/SAP/luigi/f3728e1c91f1ea2deb97836d4ca323e471e3845a/core/examples/luigi-example-js/package.json > package.json
+curl https://raw.githubusercontent.com/SAP/luigi/0b3dff050f5808b8e7852fe9e757a87be3b87de7/core/examples/luigi-example-js/package.json > package.json
 
 
 npm i 
@@ -34,10 +34,10 @@ mkdir -p public/views
 # curl https://raw.githubusercontent.com/SAP/luigi/main/core/examples/luigi-example-js/public/views/home.html > public/views/home.html
 # curl https://raw.githubusercontent.com/SAP/luigi/main/core/examples/luigi-example-js/public/views/sample1.html > public/views/sample1.html
 
-curl https://raw.githubusercontent.com/SAP/luigi/f3728e1c91f1ea2deb97836d4ca323e471e3845a/core/examples/luigi-example-js/public/index.html > public/index.html
-curl https://raw.githubusercontent.com/SAP/luigi/f3728e1c91f1ea2deb97836d4ca323e471e3845a/core/examples/luigi-example-js/public/luigi-config.js > public/luigi-config.js
-curl https://raw.githubusercontent.com/SAP/luigi/f3728e1c91f1ea2deb97836d4ca323e471e3845a/core/examples/luigi-example-js/public/views/home.html > public/views/home.html
-curl https://raw.githubusercontent.com/SAP/luigi/f3728e1c91f1ea2deb97836d4ca323e471e3845a/core/examples/luigi-example-js/public/views/sample1.html > public/views/sample1.html
+curl https://raw.githubusercontent.com/SAP/luigi/0b3dff050f5808b8e7852fe9e757a87be3b87de7/core/examples/luigi-example-js/public/index.html > public/index.html
+curl https://raw.githubusercontent.com/SAP/luigi/0b3dff050f5808b8e7852fe9e757a87be3b87de7/core/examples/luigi-example-js/public/luigi-config.js > public/luigi-config.js
+curl https://raw.githubusercontent.com/SAP/luigi/0b3dff050f5808b8e7852fe9e757a87be3b87de7/core/examples/luigi-example-js/public/views/home.html > public/views/home.html
+curl https://raw.githubusercontent.com/SAP/luigi/0b3dff050f5808b8e7852fe9e757a87be3b87de7/core/examples/luigi-example-js/public/views/sample1.html > public/views/sample1.html
 
 echo "Running server with command: npm run start"
 npm run start
