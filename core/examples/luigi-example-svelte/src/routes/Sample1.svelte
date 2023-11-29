38e9b70a2db33f2bc6989cdcc00e0d41ae01f14a
Ndricim Rrapi <ndricim.rrapi@sap.com>
2023-11-29 15:37:47 +0100

    clean 3

--- core/examples/luigi-example-svelte/src/routes/Sample1.svelte
+++ core/examples/luigi-example-svelte/src/routes/Sample1.svelte
@@ -1,6 +1,4 @@
-
-
-    <h1 class="fd-section__title">Sample 1</h1>
-  <div class="fd-panel">
+ <h1>Sample 1</h1>
+  <div>
     Luigi ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
   </div>
