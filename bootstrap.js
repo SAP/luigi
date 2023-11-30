728f9f707905dc46c33bf0921865b22bc5f0609e
Ndricim Rrapi <ndricim.rrapi@sap.com>
2023-11-30 12:06:07 +0100

    Replace Lerna with custom scripts (#3527)

---
+++ bootstrap.js
@@ -0,0 +1,79 @@
+/**
+ * This file is used to run (npm install) in all of the folders which have it as a prerequisite in the pipeline and during local development
+ */
+
+const { exec } = require('child_process');
+
+// Array of folder names
+const folders = [
+  './',
+  'core',
+  'client',
+  'client-frameworks-support/client-support-angular',
+  'client-frameworks-support/testing-utilities',
+  'container',
+  'plugins',
+  'scripts',
+  'test/e2e-test-application',
+  'test/e2e-js-test-application',
+  'client-frameworks-support/testing-utilities/test'
+];
+// Check for verbose flag
+const verboseFlagIndex = process.argv.indexOf('--verbose');
+let isVerbose = verboseFlagIndex !== -1;
+
+// Function to install npm packages in given folder
+function installPackages(folder, index, totalFolders) {
+  return new Promise((resolve, reject) => {
+    const command = `cd ${folder} && npm install`;
+
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`\x1b[31mError installing npm packages in ${folder} \x1b[0m`);
+        reject(error);
+      } else {
+        console.log(
+          `\x1b[32m[${index + 1}/${totalFolders}] : npm packages installed successfully in \x1b[33m${folder}\x1b[0m`
+        );
+        // Print logs if needed
+        if (isVerbose) {
+          console.log('VERBOSE:', stdout);
+        }
+        resolve();
+      }
+    });
+  });
+}
+
+// Function to install packages in all folders sequentially
+async function installAllPackages() {
+  for (let i = 0; i < folders.length; i++) {
+    await installPackages(folders[i], i, folders.length);
+  }
+}
+
+/**
+ * Function to handle the error case for promises
+ * @param {*} error error
+ */
+function errorHandler(error) {
+  console.error('Stopping execution of the process due to error:', error);
+  process.exit(1);
+}
+
+// EXECUTE CODE STARTS HERE
+
+console.log(
+  `\x1b[36m\n\nInstalling node_modules packages in these folders in the following order:\x1b[0m ${
+    isVerbose ? '\x1b[41m\x1b[37m(VERBOSE)\x1b[0m' : ''
+  }`
+);
+for (const folder of folders) {
+  console.log('- ' + folder);
+}
+
+console.log('Starting ---------->');
+
+installAllPackages().then(() => {
+  console.log('\x1b[32m+++++++++++> Finished installing packages successfuly. <++++++++++++++++\x1b[0m\n');
+}, errorHandler);
---
+++ bundle-size.js
@@ -0,0 +1,55 @@
+/**
+ * This file is used to check if bundle size threshold is not exceded
+ */
+
+const fs = require('fs');
+const path = require('path');
+
+const filesAndLimits = [
+  {
+    path: 'client/public/luigi-client.js',
+    limit: 50
+  },
+  {
+    path: 'core/public/luigi.js',
+    limit: 650
+  },
+  {
+    path: 'core/public/luigi.css',
+    limit: 800
+  },
+  {
+    path: 'plugins/auth/public/auth-oauth2/plugin.js',
+    limit: 75
+  }
+];
+
+async function checkFileSizes() {
+  for (const { path: filePath, limit } of filesAndLimits) {
+    const fullPath = path.resolve(__dirname, filePath);
+    console.log(`\x1b[33m\n\nChecking bundlesize in \x1b[32m${filePath}\x1b[0m`);
+
+    try {
+      const stats = await fs.promises.stat(fullPath);
+      const fileSizeInKB = (stats.size / 1024).toFixed(2);
+
+      if (fileSizeInKB > limit) {
+        console.error(
+          `\x1b[31m \n ERROR: ${filePath} exceeds the size limit.  Actual size: ${fileSizeInKB} KB, Limit: ${limit} KB ! \x1b[0m\n`
+        );
+        process.exit(1);
+      } else {
+        console.log(
+          `\x1b[32m\u2713\x1b[0m  ${filePath} is within the size limit. Size: \x1b[33m${fileSizeInKB} KB\x1b[0m < \x1b[32m${limit} KB\x1b[0m`
+        );
+      }
+    } catch (error) {
+      console.error(`Error reading file ${filePath}: ${error.message}`);
+      process.exit(1);
+    }
+  }
+
+  console.log('\x1b[32m\u2713\x1b[0m \x1b[32m All files are within their size limits.\x1b[0m \x1b[32m\u2713\x1b[0m');
+}
+
+checkFileSizes();
