8dee7224079c0f1dd84a5891148cbb91dc398bd9
Ndricim Rrapi <ndricim.rrapi@sap.com>
2024-01-17 17:12:53 +0100

    test NEXTRELEASE

--- container/typings/LuigiCompoundContainer.svelte.d.ts
+++ container/typings/LuigiCompoundContainer.svelte.d.ts
@@ -14,12 +14,14 @@ export default class LuigiCompoundContainer extends HTMLElement {
   /**
    * The configuration for the compound microfrontend
    * Take a look at https://docs.luigi-project.io/docs/navigation-parameters-reference/?section=compound
+   * @since NEXTRELEASE
    * 
    */
   compoundConfig: any;
 
   /**
    * If set to true defers from initializing the microfronted automatically. In that case init() can be used
+   * @since NEXTRELEASE
    */
   deferInit: boolean;
 
@@ -27,31 +29,37 @@ export default class LuigiCompoundContainer extends HTMLElement {
    * The context to be passed to the microfrontend. It can be an object or a stringified object.
    * @param {Object} object to be passed to the microfrontend
    * @param {string} string must be a stringified JSON object.
+   * @since NEXTRELEASE
    */
   context: object | string;
 
   /**
    * The search parameters to be passed to the compound micro frontend.
+   * @since NEXTRELEASE
    */
   searchParams: any;
 
   /**
    * The path parameters to be passed to the compound micro frontend.
+   * @since NEXTRELEASE
    */
   pathParams: any;
 
   /**
    * The clientPermissions to be passed to the compound micro frontend.
+   * @since NEXTRELEASE
    */
   clientPermissions: any;
 
   /**
    * The user settings to be passed to the compound micro frontend
+   * @since NEXTRELEASE
    */
   userSettings: UserSettings;
 
   /**
    * The anchor value to be passed to the compound micro frontend.
+   * @since NEXTRELEASE
    */
   anchor: string;
 
@@ -62,17 +70,20 @@ export default class LuigiCompoundContainer extends HTMLElement {
    * @param {boolean} WebComponentSettings.selfRegistered: if it is true, the web component bundle will be added via script tag.
    * @param {string} WebComponentSettings.tagName: tag name where web component is added to DOM.
    * @param {string} string must be a stringified JSON object from type `WebComponentSettings`.
+   * @since NEXTRELEASE
    */
   webcomponent: boolean | WebComponentSettings | stringf;
 
   /**
    * Updates the context of the compound microfrontend
    * @param contextObj The context data
+   * @since NEXTRELEASE
    */
   updateContext(contextObj: any): void;
 
   /**
    * Manually triggers the micro frontend rendering process when using defer-init attribute
+   * @since NEXTRELEASE
    */
   init(): Function;
 }
--- container/typings/LuigiContainer.svelte.d.ts
+++ container/typings/LuigiContainer.svelte.d.ts
@@ -15,11 +15,15 @@ export declare interface WebComponentSettings {
 export default class LuigiContainer extends HTMLElement {
   /**
    * The URL of the microfrontend to be rendered
+   * 
+   * @since NEXTRELEASE
    */
   viewurl: string;
 
   /**
    * If set to true defers from initializing the microfronted automatically. In that case init() can be used
+   * 
+   * @since NEXTRELEASE
    */
   deferInit: boolean;
 
@@ -27,11 +31,13 @@ export default class LuigiContainer extends HTMLElement {
    * The context to be passed to the microfrontend. It can be an object or a stringified object.
    * @param {Object} object to be passed to the microfrontend
    * @param {string} string must be a stringified JSON object.
+   * @since NEXTRELEASE
    */
   context: object | string;
 
   /**
    * Label information for the microfrontend
+   * @since NEXTRELEASE
    */
   label: string;
 
@@ -43,56 +49,67 @@ export default class LuigiContainer extends HTMLElement {
    * @param {boolean} WebComponentSettings.selfRegistered: if it is true, the web component bundle will be added via script tag.
    * @param {string} WebComponentSettings.tagName: tag name where web component is added to DOM.
    * @param {string} string must be a stringified boolean or JSON object from type `WebComponentSettings`.
+   * @since NEXTRELEASE
    */
   webcomponent: boolean | WebComponentSettings | string;
 
   /**
    * The locale to be passed to the web-component-based micro frontend
+   * @since NEXTRELEASE
    */
   locale: string;
 
   /**
    * The theme to be passed to the  web-component-based micro frontend
+   * @since NEXTRELEASE
    */
   theme: string;
 
   /**
    * The list of active feature toggles to be passed to the web-component-based micro frontend
+   * @since NEXTRELEASE
    */
   activeFeatureToggleList: string[];
 
   /**
    * If set to true, skips handshake and ready event is fired immediately 
+   * @since NEXTRELEASE
    */
   skipInitCheck: boolean;
 
   /**
    * The parameters to be passed to the web-component-based micro frontend. Will not be passed to the compound children.
+   * @since NEXTRELEASE
    */
   nodeParams: NodeParams;
 
   /**
    * The search parameters to be passed to the web-component-based micro frontend.
+   * @since NEXTRELEASE
    */
   searchParams: any;
 
   /**
    * The path parameters to be passed to the web-component-based micro frontend.
+   * @since NEXTRELEASE
    */
   pathParams: any;
 
   /**
    * The clientPermissions to be passed to the web-component-based micro frontend.
+   * @since NEXTRELEASE
    */
   clientPermissions: any;
 
   /**
    * The user settings to be passed to the web-component-based micro frontend
+   * @since NEXTRELEASE
    */
   userSettings: UserSettings;
 
   /**
    * The anchor value to be passed to the web-component-based micro frontend.
+   * @since NEXTRELEASE
    */
   anchor: string;
 
@@ -100,6 +117,7 @@ export default class LuigiContainer extends HTMLElement {
    * Updates the context of the microfrontend
    * @param contextObj The context data
    * @param internal internal luigi legacy data used for iframes
+   * @since NEXTRELEASE
    */
   updateContext(contextObj: any, internal?: any): void;
 
@@ -107,6 +125,7 @@ export default class LuigiContainer extends HTMLElement {
    * Send a custom message to the microfronted
    * @param id a string containing the message id
    * @param data data to be sent alongside the custom message
+   * @since NEXTRELEASE
    */
   sendCustomMessage(id: string, data?: any): void;
 
@@ -114,11 +133,13 @@ export default class LuigiContainer extends HTMLElement {
    * Notifies the microfrontend that the opened alert has been closed
    * @param id the id of the opened alert
    * @param dismissKey the key specifying which dismiss link was clicked on the alert message
+   * @since NEXTRELEASE
    */
   closeAlert(id: any, dismissKey: any): Function;
 
   /**
    * Manually triggers the micro frontend rendering process when using defer-init attribute
+   * @since NEXTRELEASE
    */
   init(): Function;
 }
