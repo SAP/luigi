2887df164c2960f1c0c413bd6d5d7aa0c4cc3777
Christian Geyer <149682967+camelCaseChris@users.noreply.github.com>
2024-01-25 13:25:23 +0100

    Remove some waits

--- test/e2e-test-application/cypress/e2e/tests/1-angular/microfrontends.cy.js
+++ test/e2e-test-application/cypress/e2e/tests/1-angular/microfrontends.cy.js
@@ -161,7 +161,6 @@ describe('SplitView Microfrontend', () => {
         }
       ];
       tests.forEach(test => {
-        cy.wait(50);
         test.buttonsVisible.forEach((enabled, index) => {
           cy.splitViewButtons($iframeBody)
             .contains(buttons[index])
@@ -242,22 +241,20 @@ describe('Context Update Listener test', () => {
   });
 
   it('Context in content Area', () => {
-    cy.window().then(win => {
-      cy.wait(2500);
+    // This wait is required to let the Angular component render
+    cy.wait(2500);
 
       cy.getIframeBody().then($iframeBody => {
         cy.wrap($iframeBody)
           .find('#console')
           .should('have.text', 'InitListener called');
 
-        win.Luigi.configChanged();
-        cy.wait(500);
+        cy.window().its('Luigi').invoke('configChanged');
 
         cy.wrap($iframeBody)
           .find('#console')
           .should('have.text', 'ContextUpdateListener called');
       });
-    });
   });
 });
 
@@ -269,8 +266,6 @@ describe('Context Update Listener test', () => {
   });
 
   it('Context in modal', () => {
-    cy.wait(2500);
-
     cy.get('[data-testid=modal-mf] iframe')
       .eq(0)
       .iframe()
@@ -281,7 +276,6 @@ describe('Context Update Listener test', () => {
 
         cy.window().then(win => {
           win.Luigi.configChanged();
-          cy.wait(500);
 
           cy.wrap($iframeBody)
             .find('#console')
