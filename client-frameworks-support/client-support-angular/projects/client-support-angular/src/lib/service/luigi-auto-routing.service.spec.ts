7f86821c5967a7dffba4327886fea5dca19f66ce
Christian Geyer <149682967+camelCaseChris@users.noreply.github.com>
2024-02-08 09:48:27 +0100

    Fix injection error in test

--- client-frameworks-support/client-support-angular/projects/client-support-angular/src/lib/service/luigi-auto-routing.service.spec.ts
+++ client-frameworks-support/client-support-angular/projects/client-support-angular/src/lib/service/luigi-auto-routing.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-
 import { LuigiAutoRoutingService } from './luigi-auto-routing.service';
+import { LuigiContextService } from './luigi-context-service';
 
 describe('LuigiAutoRoutingService', () => {
   let service: LuigiAutoRoutingService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [LuigiContextService]
+    });
     service = TestBed.inject(LuigiAutoRoutingService);
   });
 
