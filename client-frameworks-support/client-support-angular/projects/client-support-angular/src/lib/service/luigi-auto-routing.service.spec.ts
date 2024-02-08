8bc1363b1732076f27e5d3c7c4625058db1dce3c
Christian Geyer <149682967+camelCaseChris@users.noreply.github.com>
2024-02-08 12:29:52 +0100

    Add tests for doFilter

--- client-frameworks-support/client-support-angular/projects/client-support-angular/src/lib/service/luigi-auto-routing.service.spec.ts
+++ client-frameworks-support/client-support-angular/projects/client-support-angular/src/lib/service/luigi-auto-routing.service.spec.ts
@@ -1,4 +1,7 @@
 import { TestBed } from '@angular/core/testing';
+import { NavigationEnd } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
 import { LuigiAutoRoutingService } from './luigi-auto-routing.service';
 import { LuigiContextService } from './luigi-context-service';
 
@@ -7,12 +10,48 @@ describe('LuigiAutoRoutingService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [LuigiContextService]
+      providers: [LuigiAutoRoutingService, LuigiContextService],
+      imports: [RouterTestingModule.withRoutes([])]
     });
     service = TestBed.inject(LuigiAutoRoutingService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('doFilter', () => {
+    it('doFilter should return a function', () => {
+      expect(service.doFilter()).toBeInstanceOf(Function);
+    });
+
+    it('doFilter should return a function that takes an event keeps it if that event is a NavigationEnd event', () => {
+      const event = new NavigationEnd(0, 'url', 'urlAfterRedirects');
+      const event$ = of(event);
+      const filterResult$ = service.doFilter()(event$);
+
+      filterResult$.subscribe(result => {
+        expect(result).toBeInstanceOf(NavigationEnd);
+      });
+    });
+
+    it('doFilter should return a function that takes an event and filters it out if it is not a NavigationEnd event', () => {
+      const event = new NavigationEnd(0, 'url', 'urlAfterRedirects');
+      const notAnEvent = new Object();
+      const wrongEvent = new CustomEvent('wrongEvent');
+      const events$ = of(notAnEvent, event, wrongEvent);
+      const filterResult$ = service.doFilter()(events$);
+      let count = 0;
+
+      filterResult$.subscribe({
+        next: result => {
+          expect(result).toBeInstanceOf(NavigationEnd);
+          count++;
+        },
+        complete: () => {
+          expect(count).toBe(1);
+        }
+      });
+    });
   });
 });
