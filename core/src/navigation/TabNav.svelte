ccd0d9b9fcb615ed8c840b450c93fc3e23c2366f
Christian Geyer <149682967+camelCaseChris@users.noreply.github.com>
2024-02-27 13:31:19 +0100

    Add collapse/expand

--- core/src/navigation/TabNav.svelte
+++ core/src/navigation/TabNav.svelte
@@ -345,11 +345,14 @@
               {/if}
             {/each}
           {:else if isTabItemWithSubItems(nodes)}
+            {@const uid = `${index}-0`}
+            {@const popoverId = `lui-${uid}-popover`}
             {#if tabItemNameCanNavigate(nodes)}
               <span
-                uid="{index}-0"
+                {uid}
                 role="presentation"
                 class="fd-icon-tab-bar__item fd-icon-tab-bar__item--multi-click"
+                on:click={(event) => event.stopPropagation()}
               >
                 <!-- svelte-ignore a11y-missing-attribute -->
                 <a role="tab" class="fd-icon-tab-bar__tab" tabindex="0">
@@ -362,11 +365,11 @@
                     <div class="fd-icon-tab-bar__button-container">
                       <button
                         class="fd-button fd-button--transparent fd-icon-tab-bar__button"
-                        aria-controls="TODO"
-                        aria-expanded="true"
+                        aria-controls="{popoverId}"
+                        aria-expanded={!!dropDownStates[key]}
                         aria-haspopup="true"
                         aria-label="open menu button"
-                        onclick="TODO"
+                        on:click|preventDefault={() => toggleDropdownState(key)}
                       >
                         <i class="sap-icon--slim-arrow-down"></i>
                       </button>
@@ -374,8 +377,8 @@
                   </div>
                   <div
                     class="fd-popover__body fd-popover__body--no-arrow fd-popover__body--right fd-icon-tab-bar__popover-body"
-                    aria-hidden="false"
-                    id="TODO"
+                    aria-hidden={!dropDownStates[key]}
+                    id="{popoverId}"
                   >
                     <ul
                       class="fd-list fd-list--navigation fd-list--no-border fd-icon-tab-bar__list"
@@ -410,7 +413,7 @@
               <!-- svelte-ignore a11y-click-events-have-key-events -->
               <span
                 class="fd-icon-tab-bar__item fd-icon-tab-bar__item--single-click"
-                uid="{index}-0"
+                {uid}
                 on:click={(event) => event.stopPropagation()}
                 isSelected={isSelectedCat(key, selectedNodeForTabNav)}
               >
