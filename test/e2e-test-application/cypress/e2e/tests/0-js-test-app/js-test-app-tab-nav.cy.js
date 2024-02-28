8846719ef0f87579bffe5fd6fe43fcba522dabff
Christian Geyer <149682967+camelCaseChris@users.noreply.github.com>
2024-02-28 13:26:33 +0100

    Add tests for first use-case

--- test/e2e-test-application/cypress/e2e/tests/0-js-test-app/js-test-app-tab-nav.cy.js
+++ test/e2e-test-application/cypress/e2e/tests/0-js-test-app/js-test-app-tab-nav.cy.js
@@ -1,9 +1,28 @@
 import defaultLuigiConfig from '../../configs/default';
 
-describe('JS-TEST-APP with tab navigation', () => {
+describe('JS-TEST-APP with multi-click tab navigation', () => {
   let newConfig;
+  let nodes;
 
   beforeEach(() => {
+    nodes = [
+      {
+        pathSegment: 'one',
+        label: 'Section one',
+        viewUrl: '/examples/microfrontends/multipurpose.html',
+        category: {
+          label: 'Multi-Click Category A'
+        }
+      },
+      {
+        pathSegment: 'two',
+        label: 'Section two',
+        viewUrl: '/examples/microfrontends/multipurpose.html',
+        category: {
+          label: 'Multi-Click Category A'
+        }
+      }
+    ];
     newConfig = structuredClone(defaultLuigiConfig);
     newConfig.tag = 'tab-navigation';
     newConfig.navigation.nodes[0].children.push({
@@ -11,51 +30,70 @@ describe('JS-TEST-APP with tab navigation', () => {
       label: 'Tab Navigation',
       viewUrl: '/examples/microfrontends/multipurpose.html',
       tabNav: true,
-      children: [
-        {
-          pathSegment: 'one',
-          label: 'Section one',
-          viewUrl: '/examples/microfrontends/multipurpose.html',
-          category: {
-            label: 'Sections'
-          }
-        },
-        {
-          pathSegment: 'two',
-          label: 'Section two',
-          viewUrl: '/examples/microfrontends/multipurpose.html',
-          category: {
-            label: 'Sections'
-          }
-        }
-      ]
+      children: nodes
     });
   });
 
-  it('renders the tab navigation as expected', () => {
+  it('renders the tab as expected', () => {
+    nodes[0].category.navigateOnClick = true;
+
     cy.visitTestApp('/home/tabnav', newConfig);
     cy.get('#app[configversion="tab-navigation"]');
-    cy.get('a.luigi__more').as('tabMenu');
-    cy.get('@tabMenu').should('have.length', 2);
-    // cy.getIframeBody().then($iframeBody => {
-    //   cy.wrap($iframeBody)
-    //     .find('button.openSplitView')
-    //     .click();
-    // });
+    cy.get('.fd-icon-tab-bar__item.fd-icon-tab-bar__item--multi-click').as('tabA');
+    cy.get('@tabA')
+      .find('.fd-list__item')
+      .should('have.length', 2);
+  });
 
-    // cy.get('div.fd-page.iframeContainer')
-    //   .invoke('width')
-    //   .then(iframeContainerWidth => {
-    //     cy.get('div#splitViewContainer')
-    //       .invoke('width')
-    //       .then(splitViewContainerWidth => {
-    //         cy.get('div#splitViewDragger')
-    //           .invoke('width')
-    //           .then(splitViewDraggerWidth => {
-    //             expect(iframeContainerWidth).to.equal(splitViewContainerWidth);
-    //             expect(iframeContainerWidth).to.equal(splitViewDraggerWidth);
-    //           });
-    //       });
-    //   });
+  it('navigates with the sub-items', () => {
+    nodes[0].category.navigateOnClick = true;
+
+    cy.visitTestApp('/home/tabnav', newConfig);
+    cy.get('.fd-icon-tab-bar__item.fd-icon-tab-bar__item--multi-click').as('tabA');
+    cy.get('@tabA')
+      .find('button')
+      .as('openPopover');
+
+    cy.get('@openPopover').click();
+    cy.get('@tabA')
+      .contains('Section one')
+      .click();
+    cy.expectPathToBe('/home/tabnav/one');
+
+    cy.get('@openPopover').click();
+    cy.get('@tabA')
+      .contains('Section two')
+      .click();
+    cy.expectPathToBe('/home/tabnav/two');
+
+    cy.get('@openPopover').click();
+    cy.get('@tabA')
+      .contains('Section two')
+      .should('have.attr', 'aria-selected', 'true');
+    cy.get('@tabA')
+      .contains('Section one')
+      .should('have.attr', 'aria-selected', 'false');
+  });
+
+  it('navigates with the header if navigateOnClick is true for the first sub-item', () => {
+    nodes[0].category.navigateOnClick = true;
+
+    cy.visitTestApp('/home/tabnav', newConfig);
+    cy.get('.fd-icon-tab-bar__item.fd-icon-tab-bar__item--multi-click').as('tabA');
+    cy.get('@tabA')
+      .find('a.fd-icon-tab-bar__tab')
+      .click();
+    cy.expectPathToBe('/home/tabnav/one');
+  });
+
+  it('navigates with the header if navigateOnClick is true for the second sub-item', () => {
+    nodes[1].category.navigateOnClick = true;
+
+    cy.visitTestApp('/home/tabnav', newConfig);
+    cy.get('.fd-icon-tab-bar__item.fd-icon-tab-bar__item--multi-click').as('tabA');
+    cy.get('@tabA')
+      .find('a.fd-icon-tab-bar__tab')
+      .click();
+    cy.expectPathToBe('/home/tabnav/two');
   });
 });
