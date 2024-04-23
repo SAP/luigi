0b2cc035330cc7e4a84d9e2c281fb75b6f3c70e6
hardl <philipp.pracht@sap.com>
2024-04-23 21:27:18 +0200

    version sorting

--- website/fiddle/src/App.svelte
+++ website/fiddle/src/App.svelte
@@ -131,7 +131,12 @@
     if(!versions) {
       const response = await fetch('https://registry.npmjs.org/@luigi-project/core');
       const entries = await response.json();
-      versions = [...new Set(Object.values(entries['dist-tags']) || [])].sort();
+      versions = [...new Set(Object.values(entries['dist-tags']) || [])].sort((a, b) => {
+        const a_parts = a.split('.');
+        const b_parts = b.split('.');
+        console.log('comparing', a, b, '---', (a_parts[0] * 10000 + a_parts[1] * 1),' - ', (b_parts[0] * 10000 + b_parts[1] * 1), '=', (a_parts[0] * 10000 + a_parts[1]) - (b_parts[0] * 10000 + b_parts[1]))
+        return (a_parts[0] * 10000 + a_parts[1] * 1) - (b_parts[0] * 10000 + b_parts[1] * 1);
+      });
       versions.push('Reset...');
     }
   }
