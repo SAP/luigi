a7ffb8f474be75d049b65defda22d621f7a77184
hardl <philipp.pracht@sap.com>
2024-04-23 17:07:29 +0200

    prettier

--- website/fiddle/src/main.js
+++ website/fiddle/src/main.js
@@ -1,10 +1,10 @@
 import App from './App.svelte';
 
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'Luigi Fiddle'
-	}
+  target: document.body,
+  props: {
+    name: 'Luigi Fiddle'
+  }
 });
 
 window.app = app;
