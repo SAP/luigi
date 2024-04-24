cad5c056b300537aeeddd8825506b4086935dea1
Ndricim Rrapi <ndricim.rrapi@sap.com>
2024-04-24 16:59:27 +0200

    test

--- container/cypress/e2e/simple-wc-setup.cy.js
+++ container/cypress/e2e/simple-wc-setup.cy.js
@@ -2,6 +2,18 @@ describe('Simple Examples Iframe Container Test', () => {
   beforeEach(() => {
     cy.visit('http://localhost:2222');
   });
+  
+  it('Iframe Container Test', () => {
+    cy.visit('http://localhost:2222/container-iframe/index.html');
+
+    cy.get('luigi-container')
+      .shadow()
+      .get('iframe')
+      .then(iframe => {
+        const $body = iframe.contents().find('body');
+        cy.wrap($body).contains('This is an iframe based microfrontend container');
+      });
+  });
 
   it('WebComponent Container Test', () => {
     cy.visit('http://localhost:2222/container-wc/index.html');
