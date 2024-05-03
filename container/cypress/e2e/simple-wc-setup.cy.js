8badf52cdf1e8014e8d70f4d9fff872696dd566b
Ndricim Rrapi <ndricim.rrapi@sap.com>
2024-05-03 16:12:17 +0200

    Update tests

--- container/cypress/e2e/simple-wc-setup.cy.js
+++ container/cypress/e2e/simple-wc-setup.cy.js
@@ -3,43 +3,22 @@ describe('Simple Examples Iframe Container Test', () => {
     cy.visit('http://localhost:2222');
   });
 
-  it('Iframe Container Test', () => {
-    cy.visit('http://localhost:2222/container-iframe/index.html');
+  // it('Iframe Container Test', () => {
+  //   cy.visit('http://localhost:2222/container-iframe/index.html');
 
-    cy.get('luigi-container')
-      .shadow()
-      .get('iframe')
-      .then(iframe => {
-        const $body = iframe.contents().find('body');
-        cy.wrap($body).contains('This is an iframe based microfrontend container');
-      });
-  });
+  //   cy.get('luigi-container')
+  //     .shadow()
+  //     .get('iframe')
+  //     .then(iframe => {
+  //       const $body = iframe.contents().find('body');
+  //       cy.wrap($body).contains('This is an iframe based microfrontend container');
+  //     });
+  // });
 
   it('WebComponent Container Test', () => {
     cy.visit('http://localhost:2222/container-wc/index.html');
 
-    // // cy.get('luigi-container').shadow();
-    // const a = cy
-    //   .get('luigi-container')
-    //   .shadow()
-    //   .then(t => {
-    //     console.log('luigi-container', t);
-
-    //     console.log('luigi-container[0]', t[0]);
-
-    //     console.log('luigi-container[0].innerHtml', t[0].innerHTML);
-    //     console.log('luigi-container[0].children[1]', t[0].children[1]);
-    //   });
-
-    // // console.log(
-    // //   JSON.stringify(a)
-    // //   )
-
-    cy.get('luigi-container')
-      .shadow()
-      .find('main');
-
-    cy.get('luigi-container')
+     cy.get('luigi-container')
       .shadow()
       .find('h2')
       .should('contain.text', 'This is a webcomponent based microfrontend container  -- some content --');
