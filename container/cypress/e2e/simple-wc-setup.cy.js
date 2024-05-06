3d15b63b764d3bb184336724c5116c3e9f4aa73b
Ndricim Rrapi <ndricim.rrapi@sap.com>
2024-05-06 11:42:56 +0200

    Update tests

--- container/cypress/e2e/simple-wc-setup.cy.js
+++ container/cypress/e2e/simple-wc-setup.cy.js
@@ -22,10 +22,11 @@ describe('Simple Examples Iframe Container Test', () => {
     // .shadow()
     // .contains('This is a webcomponent based microfrontend container  -- some content --')
 
-
     cy.get('luigi-container')
       .shadow()
-      .get('luigi-wc-687474703a2f2f6c6f63616c686f73743a323232322f636f6e7461696e65722d77632f636f6e7461696e65722d77632f6d79576562636f6d706f6e656e742e6a73')
+      .get(
+        'luigi-wc-687474703a2f2f6c6f63616c686f73743a323232322f636f6e7461696e65722d77632f636f6e7461696e65722d77632f6d79576562636f6d706f6e656e742e6a73'
+      )
       .shadow()
       // .shadow()
       .find('h2')
