ccb21af5f155b0d6c4a6844897cb986d6cbcf02a
Ndricim Rrapi <ndricim.rrapi@sap.com>
2024-05-06 12:25:50 +0200

    Update tests 4

--- container/cypress/e2e/simple-wc-setup.cy.js
+++ container/cypress/e2e/simple-wc-setup.cy.js
@@ -15,28 +15,32 @@ describe('Simple Examples Iframe Container Test', () => {
   //     });
   // });
 
-  it('WebComponent Container Test',{
-    defaultCommandTimeout: 10000
-  },() => {
-    cy.visit('http://localhost:2222/container-wc/index.html');
+  it(
+    'WebComponent Container Test',
+    {
+      defaultCommandTimeout: 10000
+    },
+    () => {
+      cy.visit('http://localhost:2222/container-wc/index.html');
 
-    // cy.get('[data-test-id="luigi-container-element"]')
-    // .shadow()
-    // .contains('This is a webcomponent based microfrontend container  -- some content --')
+      // cy.get('[data-test-id="luigi-container-element"]')
+      // .shadow()
+      // .contains('This is a webcomponent based microfrontend container  -- some content --')
 
-    cy.wait(1000);
+      cy.wait(1000);
 
-    // cy.get('luigi-container', { timeout: 50000 })
+      // cy.get('luigi-container', { timeout: 50000 })
 
-    cy.get('luigi-container')
-      .shadow()
-      .get('main')
-      // .get('main', {timeout: 50000})
-      .children()
-      .first()
-      .shadow()
-      // .shadow(undefined, { timeout: 50000 })
-      // .find('h2')
-      .should('contain.text', 'This is a webcomponent based microfrontend container  -- some content --');
-  });
+      cy.get('luigi-container')
+        .shadow()
+        .get('main')
+        // .get('main', {timeout: 50000})
+        .children()
+        .first()
+        .shadow()
+        // .shadow(undefined, { timeout: 50000 })
+        // .find('h2')
+        .should('contain.text', 'This is a webcomponent based microfrontend container  -- some content --');
+    }
+  );
 });
