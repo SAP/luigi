5fce7e1bacff937281b38ab340f50227bae13ccd
Ndricim Rrapi <ndricim.rrapi@sap.com>
2024-05-14 16:03:38 +0200

    fix wc container test

--- container/cypress/e2e/wc-container.cy.js
+++ container/cypress/e2e/wc-container.cy.js
@@ -3,7 +3,7 @@ describe('Web Container Test', () => {
     let stub;
     beforeEach(() => {
       cy.visit('http://localhost:8080');
-      cy.visit('http://localhost:8080/index.html');
+      cy.visit('http://localhost:8080/#hello-world-wc');
       stub = cy.stub();
     });
 
