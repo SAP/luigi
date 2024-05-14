da8a7e62b82339f79f909ce3171863dfa107c9fc
Ndricim Rrapi <ndricim.rrapi@sap.com>
2024-05-14 10:51:47 +0200

    Move port to 2222 - add iframe and compond test

--- container/cypress/e2e/simple-iframe-setup.cy.js
+++ container/cypress/e2e/simple-iframe-setup.cy.js
@@ -1,17 +1,17 @@
-// describe('Simple Examples Iframe Container Test', () => {
-//   beforeEach(() => {
-//     cy.visit('http://localhost:2222');
-//   });
+describe('Simple Examples Iframe Container Test', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:2222');
+  });
 
-//   it('Iframe Container Test', () => {
-//     cy.visit('http://localhost:2222/container-iframe/index.html');
+  it('Iframe Container Test', () => {
+    cy.visit('http://localhost:2222/container-iframe/index.html');
 
-//     cy.get('luigi-container')
-//       .shadow()
-//       .get('iframe')
-//       .then(iframe => {
-//         const $body = iframe.contents().find('body');
-//         cy.wrap($body).contains('This is an iframe based microfrontend container');
-//       });
-//   });
-// });
+    cy.get('luigi-container')
+      .shadow()
+      .get('iframe')
+      .then(iframe => {
+        const $body = iframe.contents().find('body');
+        cy.wrap($body).contains('This is an iframe based microfrontend container');
+      });
+  });
+});
