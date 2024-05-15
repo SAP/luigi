cccf874c9c4cbe6ef461b01090a07d94a990e1d4
Ndricim Rrapi <ndricim.rrapi@sap.com>
2024-05-15 11:09:12 +0200

    Separate Compound Container test

---
+++ container/cypress/e2e/wc-compound-container.cy.js
@@ -0,0 +1,34 @@
+describe('Compound Container Tests', () => {
+  
+  describe('LuigiClient API - LuigiCompoundContainer', () => {
+    let stub;
+    beforeEach(() => {
+      cy.visit('http://localhost:8080/compoundClientAPI.html');
+      stub = cy.stub();
+    });
+
+    it('LuigiClient API getUserSettings for LuigiCompoundContainer', () => {
+      const stub = cy.stub();
+      cy.on('window:alert', stub);
+      cy.get('[data-test-id="luigi-client-api-test-compound-01"]')
+        .shadow()
+        .contains('getUserSettings')
+        .click()
+        .then(() => {
+          expect(stub.getCall(0)).to.be.calledWith('LuigiClient.getUserSettings()={"language":"it","date":""}');
+        });
+    });
+    it('LuigiClient API getAnchor for LuigiCompoundContainer', () => {
+      const stub = cy.stub();
+      cy.on('window:alert', stub);
+      cy.get('[data-test-id="luigi-client-api-test-compound-01"]')
+        .shadow()
+        .contains('getAnchor')
+        .click()
+        .then(() => {
+          expect(stub.getCall(0)).to.be.calledWith('LuigiClient.getAnchor()="testAnchorCompound"');
+        });
+    });
+  });
+
+});
