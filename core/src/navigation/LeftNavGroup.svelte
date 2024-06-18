5a623f7c22735ac1898e86da62b3e6c1a693501f
hardl <philipp.pracht@sap.com>
2024-06-18 17:19:25 +0200

    initial POC

---
+++ core/src/navigation/LeftNavGroup.svelte
@@ -0,0 +1,47 @@
+<script>
+	export let navGroup;
+    export let expanded = true;
+
+    function toggleExpanded() {
+        expanded = !expanded;
+    }
+</script>
+
+{#if navGroup.isSingleEntry}    
+    <slot></slot>
+{:else}
+    <li class="fd-navigation__list-item" aria-hidden="true">
+        <div 
+            class="fd-navigation__item fd-navigation__item--group" 
+            aria-level="1" 
+            role="treeitem" 
+            title="{navGroup.title} Group"
+            aria-roledescription="Navigation List Tree Item - Group"
+            aria-selected="false"
+            aria-expanded={expanded}
+        >
+            <a class="fd-navigation__link" role="button" tabindex="-1" on:click|preventDefault|stopPropagation={toggleExpanded}>
+                <span class="fd-navigation__text">{navGroup.title}</span>
+                <span class="fd-navigation__has-children-indicator" role="presentation" aria-hidden="true" aria-label="has children indicator, expanded"></span>
+            </a>
+        </div>
+
+        <ul 
+            class="fd-navigation__list fd-navigation__list--parent-items" 
+            role="tree" 
+            aria-roledescription="Navigation List Tree - Parent Items" 
+            tabindex="-1"
+        >
+        <slot></slot>
+        </ul>
+    </li>
+{/if}
+
+<style lang="scss">
+    .fd-navigation__link {
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+    }
+</style>
