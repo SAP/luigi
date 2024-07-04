57cc82aced98d2ec0e5b053b37376596d5004879
Waldemar Mazurek <waldemar.mazurek@sap.com>
2024-07-04 09:08:10 +0200

    Fixes mockengine test

--- client-frameworks-support/testing-utilities/test/cypress/e2e/0-luigimockengine/mockengine-test.cy.js
+++ client-frameworks-support/testing-utilities/test/cypress/e2e/0-luigimockengine/mockengine-test.cy.js
@@ -1,9 +1,14 @@
-let uxManager, linkManager;
+const containerSelector = '[id^="luigi-debug-vis-cnt"]';
 
 describe('Luigi Mock Engine', () => {
-  before(() => {
-    cy.visit('http://localhost:8181/');
+  beforeEach(() => {
+    cy.visit('http://localhost:8181/', {
+      onBeforeLoad: (win) => {
+        win.parent = win;
+      }
+    });
   });
+
   /**
    * Testing Luigi Client UX Manager features
    */
@@ -11,7 +16,7 @@ describe('Luigi Mock Engine', () => {
     it('Check LuigiClient.uxManager().alert', () => {
       cy.get('[id^=uxbutton1]').click();
 
-      cy.get('[id^="luigi-debug-vis-cnt"]')
+      cy.get(containerSelector)
         .children()
         .contains('"msg":"luigi.ux.alert.show"');
     });
@@ -19,7 +24,7 @@ describe('Luigi Mock Engine', () => {
     it('Check LuigiClient.uxManager().confirmModal', () => {
       cy.get('[id^=uxbutton2]').click();
 
-      cy.get('[id^="luigi-debug-vis-cnt"]')
+      cy.get(containerSelector)
         .children()
         .contains('"msg":"luigi.ux.confirmationModal.show"');
     });
@@ -27,7 +32,7 @@ describe('Luigi Mock Engine', () => {
     it('Check LuigiClient.uxManager().loadIndicator', () => {
       cy.get('[id^=uxbutton3]').click();
 
-      cy.get('[id^="luigi-debug-vis-cnt"]')
+      cy.get(containerSelector)
         .children()
         .contains('"msg":"luigi.show-loading-indicator"');
     });
@@ -35,7 +40,7 @@ describe('Luigi Mock Engine', () => {
     it('Check LuigiClient.uxManager().setCurrentLocale', () => {
       cy.get('[id^=uxbutton4]').click();
 
-      cy.get('[id^="luigi-debug-vis-cnt"]')
+      cy.get(containerSelector)
         .children()
         .contains('"msg":"luigi.current-locale-changed"');
     });
@@ -48,7 +53,7 @@ describe('Luigi Mock Engine', () => {
     it('Check LuigiClient.linkManager().openAsModal', () => {
       cy.get('[id^=button1]').click();
 
-      cy.get('[id^="luigi-debug-vis-cnt"]')
+      cy.get(containerSelector)
         .children()
         .contains('"msg":"luigi.navigate.ok"');
     });
@@ -56,19 +61,19 @@ describe('Luigi Mock Engine', () => {
     it('Check LuigiClient.linkManager().split', () => {
       cy.get('[id^=button2]').click();
 
-      cy.get('[id^="luigi-debug-vis-cnt"]').contains('"msg":"luigi.navigate.ok"');
+      cy.get(containerSelector).contains('"msg":"luigi.navigate.ok"');
     });
 
     it('Check LuigiClient.linkManager().drawer', () => {
       cy.get('[id^=button3]').click();
 
-      cy.get('[id^="luigi-debug-vis-cnt"]').contains('"msg":"luigi.navigate.ok"');
+      cy.get(containerSelector).contains('"msg":"luigi.navigate.ok"');
     });
 
     it('Check LuigiClient.linkManager().pathExists', () => {
       cy.get('[id^=button4]').click();
 
-      cy.get('[id^="luigi-debug-vis-cnt"]').contains('"msg":"luigi.navigation.pathExists.answer"');
+      cy.get(containerSelector).contains('"msg":"luigi.navigation.pathExists.answer"');
     });
   });
 });
