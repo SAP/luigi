28c5e636f7861e8a1499cfab05d7d9c51df500a4
Ndricim Rrapi <ndricim.rrapi@sap.com>
2024-07-24 13:55:29 +0200

    Typo container missing Since tag

--- container/typings/LuigiContainer.svelte.d.ts
+++ container/typings/LuigiContainer.svelte.d.ts
@@ -7,14 +7,14 @@ export declare interface WebComponentSettings {
 export default class LuigiContainer extends HTMLElement {
   /**
    * The URL of the microfrontend to be rendered
-   * 
+   *
    * @since 1.0.0
    */
   viewurl: string;
 
   /**
    * If set to true defers from initializing the microfronted automatically. In that case init() can be used
-   * 
+   *
    * @since 1.0.0
    */
   deferInit: boolean;
@@ -39,7 +39,7 @@ export default class LuigiContainer extends HTMLElement {
    * @param {boolean} WebComponentSettings.selfRegistered: if it is true, the web component bundle will be added via script tag.
    * @param {string} WebComponentSettings.tagName: tag name where web component is added to DOM.
    * @param {string} string must be a stringified boolean or JSON object from type `WebComponentSettings`.
-   * 
+   *
    * </br></br>
    * <blockquote class="warning">
    * <p>
@@ -74,7 +74,7 @@ export default class LuigiContainer extends HTMLElement {
   activeFeatureToggleList: string[];
 
   /**
-   * If set to true, skips handshake and ready event is fired immediately 
+   * If set to true, skips handshake and ready event is fired immediately
    * @since 1.0.0
    */
   skipInitCheck: boolean;
@@ -124,14 +124,14 @@ export default class LuigiContainer extends HTMLElement {
   /**
    * The list of rules for the content in the iframe, managed by the HTML `allow` attribute.
    * You can use one or more rules by adding them to the array, for example allowRules: ["microphone", "camera"].
-   * @since NEXT_RELEASE_CONTAINER 
+   * @since NEXT_RELEASE_CONTAINER
    */
   allowRules: string[];
 
   /**
    * The list of rules for the content in the iframe, managed by the HTML `sandbox` attribute.
    * You can use one or more rules by adding them to the array, for example sandboxRules: ["allow-scripts", "allow-same-origin"].
-   * @since NEXT_RELEASE_CONTAINER 
+   * @since NEXT_RELEASE_CONTAINER
    */
   sandboxRules: string[];
 
@@ -143,7 +143,7 @@ export default class LuigiContainer extends HTMLElement {
 
   /**
    * The hasBack value to be passed to the web-component-based micro frontend.
-   * It indicates that there is one or more preserved views. Useful when you need to show a back button. 
+   * It indicates that there is one or more preserved views. Useful when you need to show a back button.
    * @since NEXT_RELEASE_CONTAINER
    */
   hasBack: string;
@@ -159,7 +159,7 @@ export default class LuigiContainer extends HTMLElement {
    * Function that updates the context of the microfrontend
    * @param {Object} contextObj The context data
    * @param {Object} internal internal luigi legacy data used for iframes
-   * 
+   *
    * @example
    * containerElement.updateContext({newContextData: 'some data'})
    * @since 1.0.0
@@ -170,7 +170,7 @@ export default class LuigiContainer extends HTMLElement {
    * Send a custom message to the microfronted
    * @param id a string containing the message id
    * @param data data to be sent alongside the custom message
-   * 
+   *
    * @example
    * containerElement.sendCustomMessage('my-message-id', {dataToSend: 'some data'})
    * @since 1.0.0
@@ -181,7 +181,7 @@ export default class LuigiContainer extends HTMLElement {
    * A function that notifies the microfrontend that the opened alert has been closed
    * @param id the id of the opened alert
    * @param dismissKey the key specifying which dismiss link was clicked on the alert message
-   * 
+   *
    * @example
    * containerElement.closeAlert('my-alert-id', 'my-dismiss-key')
    * @since 1.0.0
@@ -190,7 +190,7 @@ export default class LuigiContainer extends HTMLElement {
 
   /**
    * Manually triggers the micro frontend rendering process when using defer-init attribute
-   * 
+   *
    * @example
    * containerElement.init()
    * @since 1.0.0
@@ -201,5 +201,5 @@ export default class LuigiContainer extends HTMLElement {
    * The authData value to be passed to the iframe-based micro frontend.
    * @since NEXT_RELEASE_CONTAINER
    */
-    authData: Object;
+  authData: Object;
 }
