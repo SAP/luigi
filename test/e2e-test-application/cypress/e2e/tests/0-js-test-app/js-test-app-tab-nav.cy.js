136e519e4cd74c0a7c461b5f88ea4b7c0283f7eb
Doberer, Johannes <johannes.doberer@sap.com>
2024-08-19 09:19:56 +0200

    e2e test

--- test/e2e-test-application/cypress/e2e/tests/0-js-test-app/js-test-app-tab-nav.cy.js
+++ test/e2e-test-application/cypress/e2e/tests/0-js-test-app/js-test-app-tab-nav.cy.js
@@ -84,4 +84,49 @@ describe('JS-TEST-APP with multi-click tab navigation', () => {
       .click();
     cy.expectPathToBe('/home/tabnav/one');
   });
+  
+  describe.only('btpToolLayout', ()=>{
+    beforeEach(()=>{
+      for(let i = 0; i<9; i++){
+        newConfig.navigation.nodes[0].children[2].children.push({
+          pathSegment: `node${i}`,
+          label: `Node${i}`,
+          viewUrl: '/examples/microfrontends/multipurpose.html',
+        });
+      }
+      newConfig.navigation.nodes[0].children[2].children.push(
+        {
+          pathSegment: `node9`,
+          label: `Node9`,
+          viewUrl: '/examples/microfrontends/multipurpose.html',
+          category: {label: 'myCat', collapsible: true}
+        },
+        {
+          pathSegment: `Node11`,
+          label: `Node11 with a suuuuuuuuuuuuuuppppper looooooooooooonnnng labelllll`,
+          viewUrl: '/examples/microfrontends/multipurpose.html',
+          category: 'myCat'
+        },
+      );
+      newConfig.settings.btpToolLayout= true;
+      newConfig.settings.experimental= {
+        btpToolLayout: true
+      };
+    });
+    it('popover opens to the left if not enough space to the right', ()=>{
+      newConfig.tag = 'tabNavPopoverRight';
+      cy.viewport(1500, 750);
+      
+      cy.visitTestApp('/home/tabnav', newConfig);
+      cy.get('#app[configversion="tabNavPopoverRight"]');
+      cy.get('[uid="2-0"]').find('.fd-popover__body').should('have.class', 'fd-popover__body--right')
+    });
+    it('enough space, render popover left', ()=>{
+      newConfig.tag='tabNavPopoverLeft'
+      cy.viewport(2000, 750);
+      cy.visitTestApp('/home/tabnav', newConfig);
+      cy.get('#app[configversion="tabNavPopoverLeft"]');
+      cy.get('[uid="2-0"]').find('.fd-popover__body').should('not.have.class', 'fd-popover__body--right')
+    });
+  });  
 });
