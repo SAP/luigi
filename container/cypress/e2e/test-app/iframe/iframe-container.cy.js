365fa402ef80ed331e1c840934358c976b26f425
Vincent Öller <vincentt.oeller@gmail.com>
2024-08-20 14:17:28 +0200

    test sendCustomMessage() for iframe container

--- container/cypress/e2e/test-app/iframe/iframe-container.cy.js
+++ container/cypress/e2e/test-app/iframe/iframe-container.cy.js
@@ -23,6 +23,32 @@ describe('Iframe Container Test', () => {
       });
   });
 
+  it('sendCustomMessage', () => {
+    cy.get(containerSelector)
+      .shadow()
+      .get('iframe')
+      .then(iframe => {
+        const $body = iframe.contents().find('body')
+        cy.wrap($body)
+          .find('#content')
+          .should('have.text', ' ');
+      });
+
+    cy.get('#btn-1')
+      .click()
+      .then(() => {
+        cy.get(containerSelector)
+          .shadow()
+          .get('iframe')
+          .then(iframe => {
+            const $body = iframe.contents().find('body')
+            cy.wrap($body)
+              .find('#content')
+              .should('have.text', 'Received Custom Message: some data');
+          });
+      });
+  });
+
   it('custom message sent', () => {
     cy.on('window:alert', stub);
 
