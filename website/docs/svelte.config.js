0343436fff3a2dd4a55c4c2c4ef93a61d2dc254c
Vincent Öller <vincentt.oeller@gmail.com>
2024-09-04 12:59:52 +0200

    update dependencies

--- website/docs/svelte.config.js
+++ website/docs/svelte.config.js
@@ -1,4 +1,4 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import staticAdapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
