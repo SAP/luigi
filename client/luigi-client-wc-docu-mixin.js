695f6500a4f63c84c14d363d2975961c5154b946
Mahati Shankar <m.shankar@sap.com>
2024-09-30 11:21:37 +0200

    cosmetics

--- client/luigi-client-wc-docu-mixin.js
+++ client/luigi-client-wc-docu-mixin.js
@@ -8,24 +8,22 @@
  * @memberof Lifecycle
  *
  * @example
- * ** WC Container Scenario **
- * Sending a message from a WC based microfrontend to parent host
+ * // case 1: publish an event from a WC based microfrontend
  *
  * // wcComponent.js
+ * // sending a message to parent host
  * this.LuigiClient.publishEvent(new CustomEvent('sendSomeMsg', { detail: 'My own message' }));
  *
  * // host.html
  * myContainer.addEventListener('custom-message', event => {
- *   console.log('My custom message from microfrontend', event.detail.data);
+ *   console.log('My custom message from the microfrontend', event.detail.data);
  * }
  *
- * ** Compound Container Scenario **
- *
- * // Sending a message from child compound microfrontend (secondChild.js) to its parent (main.html)
- * // and siblings (firstChild.js) through the event bus
+ * // case 2: publish an event from a compound microfrontend
  *
  * // secondChild.js
- * // Set the custom event name = sendInput
+ * // Set the custom event name = sendInput and
+ * // send a message to its parent (main.html) and siblings
  * this.LuigiClient.publishEvent(new CustomEvent('sendInput', { detail: 'My own message' }));
  *
  * // main.html
