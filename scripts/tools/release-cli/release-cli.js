64a5406e16f238324c5800d8fb541631a45d22fb
Waldemar Mazurek <waldemar.mazurek@accenture.com>
2024-10-02 11:02:37 +0200

    Adds new line marker

--- scripts/tools/release-cli/release-cli.js
+++ scripts/tools/release-cli/release-cli.js
@@ -200,7 +200,7 @@ function addToChangelog(versionText, changelog, lastline) {
       const versionSuffix = getVersionSuffix();
 
       inputVersion = containerNightlyVersion + versionSuffix;
-      logHeadline('\Container updated to v' + inputVersion + ':');
+      logHeadline('\nContainer updated to v' + inputVersion + ':');
     }
 
     writeVersion(pkgJsonPaths[name], inputVersion);
