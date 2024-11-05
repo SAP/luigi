d22d0c5ea9769ab335516603d38d406c7f409af5
Waldemar Mazurek <waldemar.mazurek@accenture.com>
2024-11-05 15:20:34 +0100

    Adds stylistic eslint rules

--- client-frameworks-support/client-support-angular/eslint.config.mjs
+++ client-frameworks-support/client-support-angular/eslint.config.mjs
@@ -1,20 +1,32 @@
-import globals from "globals";
-import angular from "angular-eslint";
-import tseslint from "typescript-eslint";
+import globals from 'globals';
+import angular from 'angular-eslint';
+import tseslint from 'typescript-eslint';
+import stylistic from '@stylistic/eslint-plugin';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.ts"]},
-  {ignores: ["**/*.js", "**/*.d.ts", "**/*.spec.ts"]},
+  {files: ['**/*.ts']},
+  {ignores: ['**/*.js', '**/*.d.ts', '**/*.spec.ts']},
   {languageOptions: { globals: globals.browser }},
+  {plugins: { '@stylistic': stylistic }},
   ...angular.configs.tsRecommended,
   ...tseslint.configs.recommended,
   {
     processor: angular.processInlineTemplates,
     rules: {
-      "curly": "error",
-      "no-console": ["error", {allow: ["debug"]}],
-      "no-undef": "error"
+      'curly': 'error',
+      'no-console': ['error', {allow: ['debug']}],
+      'no-undef': 'error',
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/max-len': ['error', { 'code': 140 }],
+      '@stylistic/no-trailing-spaces': 'error',
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/semi': ['error', 'always'],
+      '@stylistic/space-before-function-paren': ['error', {
+        'anonymous': 'never',
+        'asyncArrow': 'always',
+        'named': 'never'
+      }]
     }
   }
 ];
--- client-frameworks-support/client-support-angular/projects/client-support-angular/src/lib/luigi.angular.support.module.ts
+++ client-frameworks-support/client-support-angular/projects/client-support-angular/src/lib/luigi.angular.support.module.ts
@@ -6,25 +6,28 @@ import { LuigiContextService } from './service/luigi-context.service';
 import { LuigiContextServiceImpl } from './service/luigi-context.service.impl';
 
 export const staticRoutes: Routes = [
-  /** here an example if you want to specify that this component is a virtualTree element in Luigi Core navigation*/
+  // here an example if you want to specify that this component is a virtualTree element in Luigi Core navigation
   {
     path: 'luigi-client-support-preload',
     component: LuigiPreloadComponent,
     data: { fromVirtualTreeRoot: true }
   },
-  /** here an example if you want to specify that this component it is a luigi component and u want to change the navigation in Luigi core*/
+  // here an example if you want to specify that this component is a Luigi component and you want to change the navigation in Luigi core
   {
     path: 'luigi-client-support-preload',
     component: LuigiPreloadComponent,
     data: { luigiRoute: '/home/reload' }
   },
-  /** here an example if you want to reuse the component and not recreating every time you navigate to it (a singleton Component) It requires in your module to redefine  */
+  /**
+   * here an example if you want to reuse the component and not recreating every time you navigate to it (a singleton Component)
+   * it requires in your module to redefine
+   */
   {
     path: 'luigi-client-support-preload=component',
     component: LuigiPreloadComponent,
     data: { reuse: true }
   },
-  /** here an example if you want to update modalPathParam on internal navigation  */
+  // here an example if you want to update modalPathParam on internal navigation
   {
     path: 'luigi-client-support-preload',
     component: LuigiPreloadComponent,
