ec8ab1dc0e87863568e9529f122c8a6ef76e5747
hardl <philipp.pracht@sap.com>
2024-11-22 13:56:08 +0100

    prettier

--- core-modular/src/communicaton.ts
+++ core-modular/src/communicaton.ts
@@ -1,14 +1,13 @@
 import Events from '@luigi-project/container';
 import type { Luigi } from './luigi';
 
-
 export const Communication = {
-    init: (luigi: Luigi) => {
-        console.log('Init communication...');
-    },
-    addListeners: (containerElement: any, luigi: Luigi) => {        
-        containerElement.addEventListener(Events.NAVIGATION_REQUEST, (event: any) => {
-            luigi.navigation().navigate((event as any).detail.link);
-        });
-    }
-}
+  init: (luigi: Luigi) => {
+    console.log('Init communication...');
+  },
+  addListeners: (containerElement: any, luigi: Luigi) => {
+    containerElement.addEventListener(Events.NAVIGATION_REQUEST, (event: any) => {
+      luigi.navigation().navigate((event as any).detail.link);
+    });
+  }
+};
--- core-modular/src/luigi.ts
+++ core-modular/src/luigi.ts
@@ -1,46 +1,45 @@
-import { mount } from "svelte";
-import App from "./App.svelte";
-import { Navigation } from "./navigation";
-import { Routing } from "./routing";
-import type { LuigiConnector } from "./types/connector";
+import { mount } from 'svelte';
+import App from './App.svelte';
+import { Navigation } from './navigation';
+import { Routing } from './routing';
+import type { LuigiConnector } from './types/connector';
 import { UI } from './ui';
-import { Communication } from "./communicaton";
+import { Communication } from './communicaton';
 
 const _init = () => {
-    const luigi = (window as any).Luigi;
-    Routing.init(luigi);
-    UI.init(luigi);
-    Communication.init(luigi);
-}
+  const luigi = (window as any).Luigi;
+  Routing.init(luigi);
+  UI.init(luigi);
+  Communication.init(luigi);
+};
 
 export class Luigi {
-    config: any;
+  config: any;
 
-    _connector: LuigiConnector | undefined;
-    _app: any;
-    _ui = UI;
-    _comm = Communication;
+  _connector: LuigiConnector | undefined;
+  _app: any;
+  _ui = UI;
+  _comm = Communication;
 
+  bootstrap(connector: LuigiConnector): void {
+    this._app = mount(App, {
+      target: document.body
+    });
+    this._connector = connector;
+  }
 
-    bootstrap(connector: LuigiConnector): void {
-        this._app = mount(App,{
-            target: document.body
-        });
-        this._connector = connector;
-    }
+  // NOTE: using arrow style functions to have "code completion" in browser dev tools
+  setConfig = (cfg: any) => {
+    this.config = cfg;
+    _init();
+  };
 
-    // NOTE: using arrow style functions to have "code completion" in browser dev tools
-    setConfig = (cfg: any) => {
-        this.config = cfg;
-        _init();
-    };
+  getConfig = (): any => {
+    return this.config;
+  };
 
-    getConfig = (): any => {
-        return this.config;
-    };
-
-    navigation = (): Navigation => {
-        return new Navigation(this);
-    };
-    // ...
+  navigation = (): Navigation => {
+    return new Navigation(this);
+  };
+  // ...
 }
