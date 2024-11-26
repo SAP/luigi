ae36d3e29bf579fd04320217cddfe96c0ce66465
Waldemar Mazurek <waldemar.mazurek@accenture.com>
2024-11-26 10:40:59 +0100

    Bumps deps in website docs

--- website/docs/vite.config.ts
+++ website/docs/vite.config.ts
@@ -3,6 +3,13 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [sveltekit()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler'
+      }
+    }
+  },
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}']
   },
