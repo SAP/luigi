650a61ba736bfb20633b1fcbe0f64a01abd28643
Waldemar Mazurek <waldemar.mazurek@accenture.com>
2024-12-18 09:57:30 +0100

    Fixes broken spec

--- container/test/utilities/helpers.spec.ts
+++ container/test/utilities/helpers.spec.ts
@@ -108,11 +108,11 @@ describe('GenericHelpers', () => {
       consoleErrorSpy.mockRestore();
     });
 
-    it('should return undefined for unsupported input types', () => {
+    it('should return false for unsupported input types', () => {
       const unsupportedValue = 42; // Number
       // @ts-ignore
       const result = GenericHelperFunctions.checkWebcomponentValue(unsupportedValue);
-      expect(result).toStrictEqual(undefined);
+      expect(result).toStrictEqual(false);
     });
   });
 
