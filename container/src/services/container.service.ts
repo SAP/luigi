584175c4fd4c6e6ce200453d16a8e7a00fd396f5
JohannesDoberer <johannes.doberer@sap.com>
2025-01-17 09:59:56 +0100

    runtime error handling

--- container/src/services/container.service.ts
+++ container/src/services/container.service.ts
@@ -24,7 +24,7 @@ export class ContainerService {
   sendCustomMessageToIframe(iframeHandle: IframeHandle, msg: object, msgName?: string) {
     const messageName = msgName || 'custom';
 
-    if (iframeHandle.iframe.contentWindow) {
+    if (iframeHandle?.iframe?.contentWindow) {
       const iframeUrl = new URL(iframeHandle.iframe.src);
       if (messageName === 'custom') {
         iframeHandle.iframe.contentWindow.postMessage({ msg: messageName, data: msg }, iframeUrl.origin);
