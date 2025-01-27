9230d02c0bae6f4871076d5008c639e593363fed
Waldemar Mazurek <waldemar.mazurek@accenture.com>
2025-01-27 11:43:32 +0100

    Adds missing type declaration on luigi-element

--- container/test-app/compound/luigi-client-init-wc/luigi-client-init-wc.js
+++ container/test-app/compound/luigi-client-init-wc/luigi-client-init-wc.js
@@ -7,8 +7,18 @@ export default class extends LuigiElement {
     //remove buttons after review/AC
     template.innerHTML = `
         <section style="display: flex; height: 100%; flex-direction: column">
-          <button id="addNodeParams">addNodeParams</button>
-          <button id="getNodeParams">getNodeParams</button>
+          <div style="display: flex; gap: 1rem; flex-wrap: wrap; margin-bottom: 1rem">
+            <button id="addNodeParams">addNodeParams</button>
+            <button id="getNodeParams">getNodeParams</button>
+            <button id="getCurrentTheme">getCurrentTheme</button>
+            <button id="getDirtyStatus">getDirtyStatus</button>
+            <button id="removeBackdrop">removeBackdrop</button>
+            <button id="getAnchor">getAnchor</button>
+            <button id="getUserSettings">getUserSettings</button>
+            <button id="setViewGroupData">setViewGroupData</button>
+            <button id="navigateToIntent">navigateToIntent</button>
+            <button id="fromParent">fromParent</button>
+          </div>
           <header><slot name="header">header</slot></header>
           <main style="flex: auto; overflow: auto">
             <slot name="content">content</slot>
@@ -25,6 +35,42 @@ export default class extends LuigiElement {
     this.$button2 = this._shadowRoot.querySelector('#getNodeParams');
     this.$button2.addEventListener('click', () => {
       console.log('getNodeParams', this.LuigiClient.getNodeParams());
+    });
+    this.$button3 = this._shadowRoot.querySelector('#getCurrentTheme');
+    this.$button3.addEventListener('click', () => {
+      console.log('getCurrentTheme', this.LuigiClient.uxManager().getCurrentTheme());
+    });
+    this.$button4 = this._shadowRoot.querySelector('#getDirtyStatus');
+    this.$button4.addEventListener('click', () => {
+      console.log('getDirtyStatus', this.LuigiClient.uxManager().getDirtyStatus());
+    });
+    this.$button5 = this._shadowRoot.querySelector('#removeBackdrop');
+    this.$button5.addEventListener('click', () => {
+      this.LuigiClient.uxManager().removeBackdrop();
+      console.log('removeBackdrop has been called');
+    });
+    this.$button6 = this._shadowRoot.querySelector('#getAnchor');
+    this.$button6.addEventListener('click', () => {
+      console.log('getAnchor', this.LuigiClient.getAnchor());
+    });
+    this.$button7 = this._shadowRoot.querySelector('#getUserSettings');
+    this.$button7.addEventListener('click', () => {
+      console.log('getUserSettings', this.LuigiClient.getUserSettings());
+    });
+    this.$button8 = this._shadowRoot.querySelector('#setViewGroupData');
+    this.$button8.addEventListener('click', () => {
+      this.LuigiClient.setViewGroupData({ vg: 'some data' });
+      console.log('setViewGroupData has been called');
+    });
+    this.$button9 = this._shadowRoot.querySelector('#navigateToIntent');
+    this.$button9.addEventListener('click', () => {
+      this.LuigiClient.linkManager().navigateToIntent('Sales-setting');
+      console.log('navigateToIntent has been called');
+    });
+    this.$button10 = this._shadowRoot.querySelector('#fromParent');
+    this.$button10.addEventListener('click', () => {
+      this.LuigiClient.linkManager().fromParent().getCurrentRoute();
+      console.log('fromParent has been called');
     });
   }
 
