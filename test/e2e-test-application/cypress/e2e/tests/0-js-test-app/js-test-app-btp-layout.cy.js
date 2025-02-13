e0c4153d1d847762c2aa4f885d2aacb4b5283357
JohannesDoberer <johannes.doberer@sap.com>
2025-02-13 15:04:48 +0100

    prettier

--- test/e2e-test-application/cypress/e2e/tests/0-js-test-app/js-test-app-btp-layout.cy.js
+++ test/e2e-test-application/cypress/e2e/tests/0-js-test-app/js-test-app-btp-layout.cy.js
@@ -20,21 +20,19 @@ describe('JS-TEST-APP with BTP layout', () => {
       it('renders the split view separator and dragger with the correct width', () => {
         cy.visitTestApp('/home', newConfig);
         cy.get('#app[configversion="split-view"]');
-        cy.getIframeBody().then($iframeBody => {
-          cy.wrap($iframeBody)
-            .find('button.openSplitView')
-            .click();
+        cy.getIframeBody().then(($iframeBody) => {
+          cy.wrap($iframeBody).find('button.openSplitView').click();
         });
 
         cy.get('div.fd-page.iframeContainer')
           .invoke('width')
-          .then(iframeContainerWidth => {
+          .then((iframeContainerWidth) => {
             cy.get('div#splitViewContainer')
               .invoke('width')
-              .then(splitViewContainerWidth => {
+              .then((splitViewContainerWidth) => {
                 cy.get('div#splitViewDragger')
                   .invoke('width')
-                  .then(splitViewDraggerWidth => {
+                  .then((splitViewDraggerWidth) => {
                     expect(iframeContainerWidth).to.equal(splitViewContainerWidth);
                     expect(iframeContainerWidth).to.equal(splitViewDraggerWidth);
                   });
@@ -44,41 +42,35 @@ describe('JS-TEST-APP with BTP layout', () => {
     });
   });
 
-  describe('addBackdrop, removeBackdrop btpLayout', ()=>{
+  describe('addBackdrop, removeBackdrop btpLayout', () => {
     beforeEach(() => {
       newConfig.tag = 'addBackdrop';
     });
-    it('addBackDrop, removeBackdrop', ()=>{
+    it('addBackDrop, removeBackdrop', () => {
       let $iframeBody;
       cy.visitTestApp('/home/one', newConfig);
       cy.get('#app[configversion="addBackdrop"]');
       cy.get('.lui-backdrop').should('not.exist');
-      cy.getIframeBody({}, 0, '.iframeContainer').then(result => {
+      cy.getIframeBody({}, 0, '.iframeContainer').then((result) => {
         $iframeBody = result;
-        cy.wrap($iframeBody)
-          .find('.addBackdrop')
-          .contains('add backdrop')
-          .click();
+        cy.wrap($iframeBody).find('.addBackdrop').contains('add backdrop').click();
       });
       cy.get('.lui-backdrop').should('exist');
       cy.get('[data-testid="home_home"]').then(($btn) => {
         cy.get('.lui-backdrop').then(($overlay) => {
           const btnRect = $btn[0].getBoundingClientRect();
           const overlayRect = $overlay[0].getBoundingClientRect();
-      
+
           expect(overlayRect.top).to.be.at.most(btnRect.top);
           expect(overlayRect.left).to.be.at.most(btnRect.left);
           expect(overlayRect.bottom).to.be.at.least(btnRect.bottom);
           expect(overlayRect.right).to.be.at.least(btnRect.right);
         });
       });
-      
-      cy.getIframeBody({}, 0, '.iframeContainer').then(result => {
+
+      cy.getIframeBody({}, 0, '.iframeContainer').then((result) => {
         $iframeBody = result;
-        cy.wrap($iframeBody)
-          .find('.removeBackdrop')
-          .contains('remove backdrop')
-          .click();
+        cy.wrap($iframeBody).find('.removeBackdrop').contains('remove backdrop').click();
       });
       cy.get('.lui-backdrop').should('not.exist');
     });
