290bc69c5fb3f352e6370f222aa1edba07432ff3
Waldemar Mazurek <waldemar.mazurek@accenture.com>
2025-02-20 11:25:57 +0100

    Fixes flaky e2e test in container

--- container/cypress/e2e/test-app/compound/wc-compound-container.cy.js
+++ container/cypress/e2e/test-app/compound/wc-compound-container.cy.js
@@ -291,11 +291,15 @@ describe('Compound Container Tests', () => {
     it('LuigiClient API publishEvent', () => {
       cy.on('window:alert', stub);
 
-      cy.get(containerSelector).shadow().contains('Publish event').click();
-
-      cy.should(() => {
-        expect(consoleInfo).to.equal('dataConverter(): Received Custom Message from "input1" MF My own event data');
-      });
+      cy.get(containerSelector)
+        .shadow()
+        .contains('Publish event')
+        .click()
+        .then(() => {
+          expect(consoleInfo).to.equal('dataConverter(): Received Custom Message from "input1" MF My own event data');
+          expect(stub.getCall(0)).to.be.calledWith('custom-message: sendInput');
+          cy.get('#titleCmp').should('include.text', 'new text: My own event data');
+        });
     });
 
     it('LuigiClient API uxManagerChainRequests', () => {
