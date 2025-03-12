50c0f1f1d191e00c647a69d64014ca2eb098f2ce
JohannesDoberer <johannes.doberer@sap.com>
2025-03-12 12:27:34 +0100

    test fix

--- container/test/services/webcomponents.service.spec.ts
+++ container/test/services/webcomponents.service.spec.ts
@@ -607,7 +607,7 @@ describe('createClientAPI', () => {
           expect.any(Function),
           'callback'
         );
-        expect(result).toEqual(mockEventData);
+        expect(result).toEqual(undefined);
       });
     });
 
@@ -616,7 +616,7 @@ describe('createClientAPI', () => {
       const settings = { confirmationSettings: 'settings' };
 
       service.containerService.dispatch = jest.fn((eventType, target, eventData, callback, callbackName) => {
-        callback(null);
+        callback(false);
       });
 
       // act
@@ -624,7 +624,7 @@ describe('createClientAPI', () => {
       const confirmationModalPromise = clientAPI.uxManager().showConfirmationModal(settings);
 
       // assert
-      expect(confirmationModalPromise).rejects.toThrow();
+      expect(confirmationModalPromise).rejects.toBeUndefined();
     });
 
     it('test uxManager closeUserSettings', () => {
