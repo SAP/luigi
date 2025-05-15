171b49efa2863063ac73fca6c085870b54ea5840
Ndricim Rrapi <ndricim.rrapi@sap.com>
2025-05-15 13:45:19 +0200

    add module chage

--- client-frameworks-support/client-support-angular/projects/client-support-angular/src/lib/luigi.angular.support.module.ts
+++ client-frameworks-support/client-support-angular/projects/client-support-angular/src/lib/luigi.angular.support.module.ts
@@ -44,7 +44,7 @@ export const staticRoutes: Routes = [
       provide: RouteReuseStrategy,
       useClass: LuigiRouteStrategy
     }
-  ],
+  ]
 })
 export class LuigiAngularSupportModule {
   constructor(navigation: LuigiAutoRoutingService, context: LuigiContextService) {}
