d90420778636e7dda68804b233e2d95b792695aa
Vincent Tomoaki Öller <85555471+VincentUllal@users.noreply.github.com>
2025-06-06 10:09:05 +0200

    Add Vega User Menu (#4294)

--- test/e2e-test-application/src/luigi-config/extended/settings.js
+++ test/e2e-test-application/src/luigi-config/extended/settings.js
@@ -12,7 +12,7 @@ class Settings {
   appLoadingIndicator = {
     hideAutomatically: false
   };
-  profileType = ''; // Options: simple | Fiori3
+  profileType = ''; // Options: simple | Fiori3 | Vega
   responsiveNavigation = 'Fiori3'; // Options: simple | simpleMobileOnly | semiCollapsible | Fiori3
   sideNavFooterText = `Luigi Client: ${version || 'unknown'}`;
   thirdPartyCookieCheck = {
@@ -70,7 +70,8 @@ class Settings {
   btpToolLayout = false;
   experimental = {
     btpToolLayout: true,
-    profileMenuFiori3: true
+    profileMenuFiori3: true,
+    profileMenuVega: true
   };
 }
 
